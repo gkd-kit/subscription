9ec422d70a1f1193a5a27e54d4af41a7e6b8273b
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-09-04 15:15:42 +0800

    fix: 携程-开屏广告，补充快照

--- src/apps/ctrip.android.view.ts
+++ src/apps/ctrip.android.view.ts
@@ -8,10 +8,12 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'ctrip.android.publicproduct.home.view.CtripHomeActivity',
-      snapshotUrls:
-        'https://gkd-kit.gitee.io/import/38517192/104f3807-7613-46ff-9eb2-3c8bcb6ee3b1',
       rules: [
         'LinearLayout[childCount=2] > TextView + TextView[text="跳过"||text="跳过广告"]',
+      ],
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/38517192/104f3807-7613-46ff-9eb2-3c8bcb6ee3b1',
+        'https://gkd-kit.songe.li/import/12511071',
       ],
     },
   ],
