aa5bda6adca594e0865468ad00646adb0efc13b8
zhangyusheng211 <1175886294@qq.com>
2023-09-13 18:46:16 +0800

    添加一刻相册广告跳过规则

---
+++ src/apps/com.baidu.youavideo.ts
@@ -0,0 +1,37 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+// 订阅使用 JSON5 语法, 根节点可以是数组也可以是对象
+  id: 'com.baidu.youavideo',
+  name: '一刻相册',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      activityIds: ['com.baidu.youavideo.app.ui.SplashActivity'],
+      rules: [
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/snapshot/1694594083094',
+          ],
+        },
+      ],
+    },
+   {
+      key: 2,
+      name: '软件升级提醒',
+      activityIds: ['com.baidu.youavideo.home.view.HomeActivity'],
+      rules: [
+        {
+          matches: '[id="com.baidu.youavideo:id/img_close"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/snapshot/1694599941204',
+          ],
+        },
+      ],
+    },
+  ],
+});
+
+
