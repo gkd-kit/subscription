313b9ea62abf7eace80394c16b83435041970610
lisonge <i@songe.li>
2023-09-15 10:36:52 +0800

    fix(宝宝树孕育): 规则优化

--- src/apps/com.babytree.apps.pregnancy.ts
+++ src/apps/com.babytree.apps.pregnancy.ts
@@ -14,7 +14,7 @@ export default defineAppConfig({
       rules: [
         {
           matches:
-            '[id="com.babytree.apps.pregnancy:id/ilr"||id="com.babytree.apps.pregnancy:id/dla"&&text*="跳过"]',
+            'ImageView[id!=null] + TextView[clickable=true][text^="跳过"][text.length<=4][id!=null]',
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/12614755',
             'https://gkd-kit.gitee.io/import/12614757',
@@ -25,9 +25,11 @@ export default defineAppConfig({
     {
       key: 2,
       name: '弹出广告(偶发)',
+      activityIds: 'com.babytree.apps.pregnancy.activity.SailfishActivity',
       rules: [
         {
-          matches: '[id="com.babytree.apps.pregnancy:id/bzm"]',
+          matches:
+            'ImageView[id="com.babytree.apps.pregnancy:id/bb_referenced_active_dialog_img_2"] + ImageView[id!=null][clickable=true]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12614834',
         },
       ],
@@ -35,9 +37,11 @@ export default defineAppConfig({
     {
       key: 3,
       name: '首页右侧悬浮广告(偶发)',
+      activityIds: 'com.babytree.apps.pregnancy.activity.SailfishActivity',
       rules: [
         {
-          matches: '[id="com.babytree.apps.pregnancy:id/cx5"]',
+          matches:
+            'ImageView[id!=null][clickable=false] + @ImageView[id!=null][clickable=true] + FrameLayout[id=null] > TextView[text=null][id!=null]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12614838',
         },
       ],
