8508ae5fd2b0dec324cf6ceb0cb4b79d3672c2a1
Kun Jinkao <wwt18689249843@gmail.com>
2023-09-16 10:56:48 +0800

    调整代码风格

--- src/apps/com.sankuai.meituan.ts
+++ src/apps/com.sankuai.meituan.ts
@@ -23,24 +23,21 @@ export default defineAppConfig({
       key: 2,
       name: '美团买菜活动弹窗',
       activityIds: 'com.meituan.retail.c.android.mrn.mrn.MallMrnModal',
-      rules:
-        'ViewGroup > ViewGroup +(5) ViewGroup > ImageView',
+      rules: 'ViewGroup > ViewGroup +(5) ViewGroup > ImageView',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12639717',
     },
     {
       key: 3,
       name: '小调查弹窗',
       activityIds: 'com.meituan.retail.c.android.mrn.mrn.MallMrnModal',
-      rules:
-        '[text="小调查"] + ViewGroup > ViewGroup > ImageView',
+      rules: '[text="小调查"] + ViewGroup > ViewGroup > ImageView',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12639723',
     },
     {
       key: 4,
       name: '首页右侧抽奖小广告',
       activityIds: 'com.miui.home.launcher.Launcher',
-      rules:
-        'FrameLayout > @ImageView + ImageView[desc="资质与规则"]',
+      rules: 'FrameLayout > @ImageView + ImageView[desc="资质与规则"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12639734',
     },
   ],
