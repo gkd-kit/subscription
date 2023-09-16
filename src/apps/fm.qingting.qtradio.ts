792c407e0af9cd57c6936105adebe1efe9938c35
Kun Jinkao <wwt18689249843@gmail.com>
2023-09-16 23:10:05 +0800

    新增一个开屏广告，修改规则

--- src/apps/fm.qingting.qtradio.ts
+++ src/apps/fm.qingting.qtradio.ts
@@ -8,16 +8,20 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: ['fm.qingting.qtradio.QTRadioActivity'],
-      rules:
-        '@TextView[text="跳过广告"] - TextView < LinearLayout - FrameLayout >(2) ImageView',
-      snapshotUrls: ['https://gkd-kit.gitee.io/import/12640433'],
+      rules: [
+        '[id="fm.qingting.qtradio:id/third_skip_layout"]',
+        '[id="fm.qingting.qtradio:id/skip_ad"]',
+      ],
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12640433',
+        'https://gkd-kit.gitee.io/import/12640971',
+      ],
     },
     {
       key: 1,
       name: '播放界面的广告',
       activityIds: ['fm.qingting.qtradio.QTRadioActivity'],
-      rules:
-        '@[id="fm.qingting.qtradio:id/view_ad_close"] + LinearLayout > TextView[text="广告"]',
+      rules: '[id="fm.qingting.qtradio:id/view_ad_close"]',
       snapshotUrls: ['https://gkd-kit.gitee.io/import/12640438'],
     },
   ],
