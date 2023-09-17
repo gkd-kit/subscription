776ffd51182a22ddbff25ece1b3e1aa7d1151053
lisonge <i@songe.li>
2023-09-17 22:38:22 +0800

    feat(全球网测): 更新弹窗-简化规则

--- src/apps/com.cnspeedtest.globalspeed.ts
+++ src/apps/com.cnspeedtest.globalspeed.ts
@@ -9,7 +9,7 @@ export default defineAppConfig({
       name: '更新弹窗',
       activityIds: 'com.ruili.android.taierspeed.main.MainActivity',
       rules: [
-        '[text="更新消息"] - ImageView < LinearLayout +(4) LinearLayout > Button[text="取消"]',
+        '[id="com.cnspeedtest.globalspeed:id/vision_btn_ok"][text="更新"] +2 [id="com.cnspeedtest.globalspeed:id/vision_btn_cancel"]',
       ],
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12642345',
     },
