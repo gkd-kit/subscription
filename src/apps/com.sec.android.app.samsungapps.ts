567868b14f36203fbff55aeda3ec352937bc6b70
gujixian <gujixian@gmail.com>
2023-09-20 22:57:25 +0800

    feat(三星应用商店): 开屏广告

---
+++ src/apps/com.sec.android.app.samsungapps.ts
@@ -0,0 +1,27 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+    id: 'com.sec.android.app.samsungapps',
+    name: '三星应用商店',
+    groups: [
+        {
+            key: 1,
+            name: '首页广告',
+            activityIds: [
+                'com.sec.android.app.samsungapps.notipopup.WebViewPopup'
+            ],
+            rules: [
+                {
+                    matches: '[text="关闭当前广告"]',
+                    snapshotUrls: 'https://gkd-kit.gitee.io/import/12505365',
+                },
+                {
+                    matches: '[id="com.sec.android.app.samsungapps:id/skip"]',
+                    snapshotUrls: [
+                        'https://gkd-kit.songe.li/import/12674484',
+                    ],
+                },
+            ],
+        },
+    ],
+});
