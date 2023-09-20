2f05de67f7e53e28b65670184a700d2a5fc44d6e
CallMeBill <callmebill@billw.cn>
2023-09-20 09:49:49 +0800

    feat(华为运动健康): “我的”页会员广告

--- src/apps/com.huawei.health.ts
+++ src/apps/com.huawei.health.ts
@@ -5,7 +5,7 @@ export default defineAppConfig({
   name: '华为运动健康',
   groups: [
     {
-      key: 1,
+      key: 0,
       name: '开屏广告',
       activityIds: ['com.huawei.health.MainActivity'],
       rules: [
@@ -13,6 +13,17 @@ export default defineAppConfig({
           matches: '[id="com.huawei.hms.ads.uiengine:id/hiad_skip_text"]',
           action: 'clickCenter',
           snapshotUrls: ['https://gkd-kit.gitee.io/import/12667766'],
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '“我的”页会员广告',
+      activityIds: ['com.huawei.health.MainActivity'],
+      rules: [
+        {
+          matches: '[id="com.huawei.health:id/item_banner_ad_close_icon"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12667814'],
         },
       ],
     },
