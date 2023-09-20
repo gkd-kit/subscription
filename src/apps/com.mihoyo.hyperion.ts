4d2bb82211343fae2a0d5f002cdcdc7d8abd7198
Kun Jinkao <wwt18689249843@gmail.com>
2023-09-21 00:36:00 +0800

    feat(米游社)

--- src/apps/com.mihoyo.hyperion.ts
+++ src/apps/com.mihoyo.hyperion.ts
@@ -10,21 +10,26 @@ export default defineAppConfig({
       activityIds: [
         'com.mihoyo.hyperion.ui.SplashActivity',
         'com.mihoyo.hyperion.splash.SplashActivity',
+        'com.miui.home.launcher.Launcher',
       ],
-      rules: '[id=`com.mihoyo.hyperion:id/mSplashBtJump`]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12482738',
+      rules: '[id="com.mihoyo.hyperion:id/mSplashBtJump"]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12482738',
+        'https://gkd-kit.gitee.io/import/12675530', // activityId: 'com.miui.home.launcher.Launcher'
+      ],
     },
     {
       key: 1,
       name: '青少年模式',
       desc: '关闭青少年模式提醒弹窗',
-      rules: 'TextView[id=`com.mihoyo.hyperion:id/tv_dialog_i_know`]',
+      rules: 'TextView[id="com.mihoyo.hyperion:id/tv_dialog_i_know"]',
     },
     {
       key: 2,
       name: '版本更新提示',
       desc: '关闭版本更新提示弹窗',
-      rules: 'TextView[id=`com.mihoyo.hyperion:id/mUpgradeDialogCancel`]',
+      rules: 'TextView[id="com.mihoyo.hyperion:id/mUpgradeDialogCancel"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12675513',
     },
   ],
 });
