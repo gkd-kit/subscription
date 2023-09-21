4b394813fe8e71a70c828c358d339aba261ee4bf
lisonge <i@songe.li>
2023-09-21 21:04:22 +0800

    chore: 优化

--- src/apps/com.sec.android.app.samsungapps.ts
+++ src/apps/com.sec.android.app.samsungapps.ts
@@ -1,27 +1,19 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-    id: 'com.sec.android.app.samsungapps',
-    name: '三星应用商店',
-    groups: [
+  id: 'com.sec.android.app.samsungapps',
+  name: '三星应用商店',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      activityIds: ['com.sec.android.app.samsungapps.notipopup.WebViewPopup'],
+      rules: [
         {
-            key: 1,
-            name: '开屏广告',
-            activityIds: [
-                'com.sec.android.app.samsungapps.notipopup.WebViewPopup'
-            ],
-            rules: [
-                {
-                    matches: '[text="关闭当前广告"]',
-                    snapshotUrls: 'https://gkd-kit.gitee.io/import/12505365',
-                },
-                {
-                    matches: '[id="com.sec.android.app.samsungapps:id/skip"]',
-                    snapshotUrls: [
-                        'https://gkd-kit.songe.li/import/12674484',
-                    ],
-                },
-            ],
+          matches: '[id="com.sec.android.app.samsungapps:id/skip"]',
+          snapshotUrls: ['https://gkd-kit.songe.li/import/12674484'],
         },
-    ],
+      ],
+    },
+  ],
 });
