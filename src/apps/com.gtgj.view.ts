f9e881336a013d860a0c0bb3d989b8c0b8d5db0b
CallMeBill <callmebill@billw.cn>
2023-09-21 20:39:11 +0800

    feat(高铁管家): 开屏

---
+++ src/apps/com.gtgj.view.ts
@@ -0,0 +1,14 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.gtgj.view',
+  name: '高铁管家',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.gtgj.view.LaunchActivity',
+      rules: 'FrameLayout > TextView[text=`跳过`]',
+    },
+  ],
+});
