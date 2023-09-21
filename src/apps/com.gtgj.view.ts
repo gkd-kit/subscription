f04c1f37f5494b6cb9968fe813b4ec3448aa222d
lisonge <i@songe.li>
2023-09-21 22:00:53 +0800

    chore: 规则优化

--- src/apps/com.gtgj.view.ts
+++ src/apps/com.gtgj.view.ts
@@ -8,7 +8,8 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.gtgj.view.LaunchActivity',
-      rules: 'FrameLayout > TextView[text=`跳过`]',
+      rules:
+        '[id="com.gtgj.view:id/logo_img_container"] >2 FrameLayout[clickable=true] > TextView[text=`跳过`]',
       snapshotUrls: ['https://gkd-kit.gitee.io/import/12684583'],
     },
   ],
