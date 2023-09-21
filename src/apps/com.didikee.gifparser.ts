22bbe56fb354309b17e5f68f7713e778ed98e650
lisonge <i@songe.li>
2023-09-21 21:00:47 +0800

    chore: 移除注释

--- src/apps/com.didikee.gifparser.ts
+++ src/apps/com.didikee.gifparser.ts
@@ -16,44 +16,5 @@ export default defineAppConfig({
         },
       ],
     },
-    /*
-    {
-      enable: false, // app 关闭按钮无效，点击不感兴趣仍然无法关闭广告
-      key: 999,
-      name: '处理进度-底部广告',
-      activityIds: 'com.gif.process.ProcessingActivity',
-      rules: [
-        {
-          key: 0,
-          matches: 'Image[id=null] < @View +(2) View >(3) [text*="广告"]',
-          snapshotUrls: [
-            'https://gkd-kit.gitee.io/import/12674424',
-            'https://gkd-kit.gitee.io/import/12674435',
-          ],
-        },
-        {
-          key: 1,
-          matches: 'Image[id=null] < @View + View >(2) [text="广告"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12674464'],
-        },
-        {
-          key: 2,
-          matches: 'Image[id=null] < @View - View >(2) [text="广告"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12674473'],
-        },
-        {
-          key: 3,
-          matches: 'Image[id=null] < @View -(3) View >(2) [text="广告"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12674604'],
-        },
-        {
-          preKeys: [0, 1, 2, 3],
-          key: 10,
-          matches: '[id="com.byted.pangle:id/tt_item_tv"][text="不感兴趣"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12674540'],
-        },
-      ],
-    },
-    */
   ],
 });
