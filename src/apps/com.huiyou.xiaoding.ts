29e0a1c5dc609c4966ef1e4d9bc904b32b9decd8
sishuikaki <2088799046@qq.com>
2023-09-22 20:10:24 +0800

    feat(校钉): 跳过开屏广告

--- src/apps/com.huiyou.xiaoding.ts
+++ src/apps/com.huiyou.xiaoding.ts
@@ -10,38 +10,12 @@ export default defineAppConfig({
       activityIds: ['com.huiyou.xiaoding.launcher.LauncherActivity'],
       rules: [
         {
-          matches: '[text="跳过"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12699825',
-        },
-        {
           matches: '[id$="ksad_splash_circle_skip_view"]',
           snapshotUrls: ['https://gkd-kit.gitee.io/import/12699825'],
-        },
-      ],
-    },
-    {
-      key: 0,
-      name: '开屏广告2',
-      activityIds: ['com.huiyou.xiaoding.launcher.LauncherActivity'],
-      rules: [
-        {
-          matches: '[text="Skip"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12699822',
         },
         {
           matches: '[id$="skip_parent_view"]',
           snapshotUrls: ['https://gkd-kit.gitee.io/import/12699822'],
-        },
-      ],
-    },
-    {
-      key: 0,
-      name: '开屏广告3',
-      activityIds: ['com.huiyou.xiaoding.launcher.LauncherActivity'],
-      rules: [
-        {
-          matches: '[text="跳过 "]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12699790',
         },
         {
           matches: '[name="android.widget.TextView"]',
