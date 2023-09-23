52d050cef86d1a4cd97764624ba9424121579033
Kun Jinkao <wwt18689249843@gmail.com>
2023-09-23 09:28:26 +0800

    feat(哔哩哔哩)

--- src/apps/tv.danmaku.bili.ts
+++ src/apps/tv.danmaku.bili.ts
@@ -8,7 +8,9 @@ export default defineAppConfig({
       key: -1,
       name: '开屏广告',
       desc: '开屏广告,切回APP开屏广告',
-      rules: 'TextView[id=`tv.danmaku.bili:id/count_down`]',
+      activityIds: 'tv.danmaku.bili.MainActivityV2',
+      rules: '[id="tv.danmaku.bili:id/count_down"][text^="跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12705270',
     },
     {
       key: 0,
@@ -59,20 +61,24 @@ export default defineAppConfig({
       activityIds: [
         'com.bilibili.video.videodetail.VideoDetailsActivity',
         'com.bilibili.ship.theseus.all.UnitedBizDetailsActivity',
+        'tv.danmaku.bili.MainActivityV2',
       ],
       rules: [
-        '[id=`tv.danmaku.bili:id/reason1_layout`] > [id=`tv.danmaku.bili:id/reason1`][text*=`广告`]',
+        '[id=`tv.danmaku.bili:id/reason1_layout`] > [id=`tv.danmaku.bili:id/reason1`][text*=`广告`]', // 缺少快照
         {
           name: '点击屏蔽广告原因',
           matches:
-            '[id="tv.danmaku.bili:id/menu_text"][text="屏蔽广告"] < [id="tv.danmaku.bili:id/title"] + [id="tv.danmaku.bili:id/dislike_reasons"] >2 [id="tv.danmaku.bili:id/reason1_layout"]',
+            '[id="tv.danmaku.bili:id/dislike_reasons"] >2 [id="tv.danmaku.bili:id/reason1_layout"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12642261',
         },
         {
           name: '点击广告卡片右侧菜单图标',
           matches:
-            '[id=`tv.danmaku.bili:id/ad_tint_frame`][desc^=`UP主推荐广告`] @[id=`tv.danmaku.bili:id/more`] > ImageView',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12642260',
+            '[id="tv.danmaku.bili:id/ad_tint_frame"] >(n) [id="tv.danmaku.bili:id/more"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12642260', // n = 2
+            'https://gkd-kit.gitee.io/import/12705266', // n = 3
+          ],
         },
       ],
     },
