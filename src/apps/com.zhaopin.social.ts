d5bc6cc87b747690c3cfb214cd198025af12e876
Kun Jinkao <wwt18689249843@gmail.com>
2023-09-23 18:20:56 +0800

    feat(智联招聘)

---
+++ src/apps/com.zhaopin.social.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.zhaopin.social',
+  name: '智联招聘',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.zhaopin.social.SplashActivity',
+      rules:
+        '[id="com.zhaopin.social:id/tv_ad"] + [id="com.zhaopin.social:id/cdp_view"][text^="跳过"]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12706183'],
+    },
+    {
+      key: 1,
+      name: '我的页面-banner广告',
+      activityIds: 'com.zhaopin.social.homepage.ZSC_MainTabActivity',
+      rules: '[id="com.zhaopin.social:id/rl_banner_close"][clickable=true]',
+      snapshotUrls: ['https://gkd-kit.gitee.io/import/12706181'],
+    },
+  ],
+});
