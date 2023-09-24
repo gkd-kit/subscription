d3c49c2d0a55ca5129085f2bb140984757c98aba
Kun Jinkao <wwt18689249843@gmail.com>
2023-09-24 21:31:21 +0800

    feat(快手)

--- src/apps/com.smile.gifmaker.ts
+++ src/apps/com.smile.gifmaker.ts
@@ -5,6 +5,13 @@ export default defineAppConfig({
   name: '快手',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      activityIds: 'com.yxcorp.gifshow.HomeActivity',
+      rules: '[id="com.smile.gifmaker:id/splash_skip_text"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12708602',
+    },
+    {
       key: 0,
       name: '关闭青少年弹窗',
       activityIds: 'com.yxcorp.gifshow.HomeActivity',
