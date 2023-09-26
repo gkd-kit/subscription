130acc47749a3a53a850be1c53905b977eac674b
lisonge <i@songe.li>
2023-09-26 23:47:20 +0800

    feat(大众点评): 规则优化

--- src/apps/com.dianping.v1.ts
+++ src/apps/com.dianping.v1.ts
@@ -19,38 +19,30 @@ export default defineAppConfig({
     {
       key: 1,
       name: '主页推荐关注弹窗',
-      activityIds: [
-        'com.dianping.main.guide.SplashActivity',
-        'com.dianping.v1.NovaMainActivity',
-      ],
-      rules: '[id=null][desc="关闭"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727011', // 对应第二条activityIds
-      // 缺第一条activityIds对应的快照
+      activityIds: ['com.dianping.main.guide.SplashActivity'],
+      rules:
+        'FrameLayout[id="android:id/content"] >2 FrameLayout > ImageView + FrameLayout[id=null][desc="关闭"][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727011',
     },
     {
       key: 2,
       name: '消息页上方开启系统通知提醒',
-      activityIds: [
-        'com.dianping.v1.NovaMainActivity',
-        'com.dianping.main.guide.SplashActivity',
-      ],
+      activityIds: ['com.dianping.main.guide.SplashActivity'],
       rules: {
-        matches: '[text="去开启"] +2 FrameLayout[clickable=true]',
+        matches:
+          '[text="去开启"][clickable=true] +2 @FrameLayout[clickable=true] > ImageView',
         action: 'clickCenter', // clickNode无响应
       },
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727070', // 对应第二条activityIds
-      // 缺第一条activityIds对应的快照
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727070',
     },
     {
       key: 3,
-      name: '签到领红包悬浮窗',
-      activityIds: [
-        'com.dianping.v1.NovaMainActivity',
-        'com.dianping.main.guide.SplashActivity',
-      ],
-      rules: '[id="com.dianping.v1:id/close"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727366', // 对应第一条activityIds
-      // 缺第二条activityIds对应的快照
+      name: '关闭[签到开红包]',
+      desc: '点击右下角悬浮图片右上角的x图标',
+      activityIds: ['com.dianping.v1.NovaMainActivity'],
+      rules:
+        '[id="com.dianping.v1:id/float_image"] + [id="com.dianping.v1:id/close"][clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727366',
     },
   ],
 });
