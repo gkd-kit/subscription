e4ba83258f3fb9a16da61d8e52da6dd046181763
longm21 <2692312972@qq.com>
2023-09-26 20:04:28 +0800

    feat(大众点评): 补充事件

--- src/apps/com.dianping.v1.ts
+++ src/apps/com.dianping.v1.ts
@@ -7,8 +7,27 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.dianping.v1.NovaMainActivity',
+      activityIds: [
+        'com.dianping.v1.NovaMainActivity',
+        'com.dianping.main.guide.SplashActivity',
+      ],
       rules: '[id=`com.dianping.v1:id/new_skip`]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12726928', // 对应第二条activityIds
+      // 缺第一条activityIds对应的快照
+    },
+    {
+      key: 1,
+      name: '主页推荐关注弹窗',
+      activityIds: 'com.dianping.main.guide.SplashActivity',
+      rules: '[id=null][desc="关闭"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727011',
+    },
+    {
+      key: 2,
+      name: '消息页上方开启系统通知提醒',
+      activityIds: 'com.dianping.main.guide.SplashActivity',
+      rules: '[text="去开启"] +2 FrameLayout[clickable=true]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727070',
     },
   ],
 });
