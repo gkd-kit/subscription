f9e16241c54f1996bc938e6db81a7abb9f3d82c9
longm21 <2692312972@qq.com>
2023-09-26 20:28:55 +0800

    feat(大众点评): 补充一些场景

--- src/apps/com.dianping.v1.ts
+++ src/apps/com.dianping.v1.ts
@@ -14,20 +14,43 @@ export default defineAppConfig({
       rules: '[id=`com.dianping.v1:id/new_skip`]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12726928', // 对应第二条activityIds
       // 缺第一条activityIds对应的快照
+      // 上面两条activityIds，在截快照时交替出现，故都放在判定条件里
     },
     {
       key: 1,
       name: '主页推荐关注弹窗',
-      activityIds: 'com.dianping.main.guide.SplashActivity',
+      activityIds: [
+        'com.dianping.main.guide.SplashActivity',
+        'com.dianping.v1.NovaMainActivity',
+      ],
       rules: '[id=null][desc="关闭"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727011',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727011', // 对应第二条activityIds
+      // 缺第一条activityIds对应的快照
     },
     {
       key: 2,
       name: '消息页上方开启系统通知提醒',
-      activityIds: 'com.dianping.main.guide.SplashActivity',
-      rules: '[text="去开启"] +2 FrameLayout[clickable=true]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727070',
+      activityIds: [
+        'com.dianping.v1.NovaMainActivity',
+        'com.dianping.main.guide.SplashActivity',
+      ],
+      rules: {
+        matches: '[text="去开启"] +2 FrameLayout[clickable=true]',
+        action: 'clickCenter', // clickNode无响应
+      },
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727070', // 对应第二条activityIds
+      // 缺第一条activityIds对应的快照
+    },
+    {
+      key: 3,
+      name: '签到领红包悬浮窗',
+      activityIds: [
+        'com.dianping.v1.NovaMainActivity',
+        'com.dianping.main.guide.SplashActivity',
+      ],
+      rules: '[id="com.dianping.v1:id/close"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12727366', // 对应第一条activityIds
+      // 缺第二条activityIds对应的快照
     },
   ],
 });
