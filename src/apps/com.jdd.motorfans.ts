77858d7688d860f8c488303c88c98027b349771c
lisonge <i@songe.li>
2023-09-27 18:12:41 +0800

    feat(摩托范): 弹窗广告-添加示例

--- src/apps/com.jdd.motorfans.ts
+++ src/apps/com.jdd.motorfans.ts
@@ -13,6 +13,8 @@ export default defineAppConfig({
           matches:
             'RelativeLayout > ImageView[id="com.jdd.motorfans:id/iv_close"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12733646',
+          exampleUrls:
+            'https://user-images.githubusercontent.com/44717382/270852019-b0296eaa-a378-49b3-877b-acefca2a7d58.gif',
         },
       ],
     },
