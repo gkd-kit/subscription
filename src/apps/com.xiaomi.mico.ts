5196c8112fb521d2fca9af6dd5363f0d2684dc93
Kun Jinkao <wwt18689249843@gmail.com>
2023-09-28 12:44:31 +0800

    feat(小爱音箱)

---
+++ src/apps/com.xiaomi.mico.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xiaomi.mico',
+  name: '小爱音箱',
+  groups: [
+    {
+      key: 1,
+      name: '首页广告弹窗',
+      activityIds: 'com.xiaomi.mico.main.MainActivity',
+      rules:
+        '[id="com.xiaomi.mico:id/adImage"] + [id="com.xiaomi.mico:id/ivCloseAd"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12745621',
+    },
+  ],
+});
