d4eb86eb93a28d9d4c5b9fe7c3eecd4c4e6660ec
shanlan <hi@gobyte.cn>
2023-09-29 09:04:09 +0800

    feat(小熊油耗): 升级弹窗广告

--- src/apps/com.firebear.androil.ts
+++ src/apps/com.firebear.androil.ts
@@ -11,5 +11,12 @@ export default defineAppConfig({
       rules: '[id=`com.byted.pangle.m:id/tt_splash_skip_btn`]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12712742',
     },
+    {
+      key: 1,
+      name: '升级弹窗',
+      activityIds: 'com.firebear.androil.app.home.MainActivity',
+      rules: '[id=`com.firebear.androil.app.home.MainActivity`]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12755032',
+    },
   ],
 });
