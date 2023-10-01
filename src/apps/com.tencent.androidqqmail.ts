3136060c5688ca9430ba68497943c4faa578c2fd
Kun Jinkao <wwt18689249843@gmail.com>
2023-10-01 09:31:47 +0800

    新增 【开屏广告】

--- src/apps/com.tencent.androidqqmail.ts
+++ src/apps/com.tencent.androidqqmail.ts
@@ -5,6 +5,13 @@ export default defineAppConfig({
   name: 'qq邮箱',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      activityIds: 'com.tencent.qqmail.launcher.desktop.LauncherActivity',
+      rules: 'RelativeLayout > ImageView[id!=null] + ImageView[id!=null]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12775855',
+    },
+    {
       key: 0,
       name: '广告邮件-列表卡片广告',
       activityIds: 'com.tencent.qqmail.fragment.base.MailFragmentActivity',
