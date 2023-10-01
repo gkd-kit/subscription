7e94ef7edc005c64513db3529f8295edf7719980
Kun Jinkao <wwt18689249843@gmail.com>
2023-10-01 09:40:18 +0800

    优化【广告邮件-列表卡片广告】

--- src/apps/com.tencent.androidqqmail.ts
+++ src/apps/com.tencent.androidqqmail.ts
@@ -16,8 +16,23 @@ export default defineAppConfig({
       name: '广告邮件-列表卡片广告',
       activityIds: 'com.tencent.qqmail.fragment.base.MailFragmentActivity',
       rules: [
-        'TextView[text=`赞助商提供的广告`] <n FrameLayout <n ListView[id=`com.tencent.androidqqmail:id/pop_up_list`] TextView[text=`不感兴趣`]',
-        '[id=`com.tencent.androidqqmail:id/advertise_view_ad`]',
+        {
+          key: 0,
+          matches: '[id="com.tencent.androidqqmail:id/advertise_view_ad"]', // 需要补充快照
+        },
+        {
+          key: 1,
+          matches:
+            'LinearLayout > TextView + @LinearLayout[id!=null][clickable=true] > [text="广告"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12775857',
+        },
+        {
+          preKeys: [1],
+          key: 9,
+          matches:
+            '[text="赞助商提供的广告"] < FrameLayout + @FrameLayout > [text="不感兴趣"]',
+          snapshotUrls: 'https://gkd-kit.songe.li/import/12775862',
+        },
       ],
     },
   ],
