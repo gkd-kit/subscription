fe681ef934d7dbf128b373fc24642d6fc3e1bdbc
shjdgwj <1018996814@qq.com>
2023-10-01 17:25:34 +0800

    feat(OmoFun): 开屏广告，首页通知，插屏广告
    合并规则

--- src/apps/com.ksf.yyx.ts
+++ src/apps/com.ksf.yyx.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: shjdgwj 1018996814@qq.com
+ * @Date: 2023-09-30 23:00:38
+ * @LastEditors: shjdgwj 1018996814@qq.com
+ * @LastEditTime: 2023-10-01 17:24:13
+ * @FilePath: \gkd_subscription\src\apps\com.ksf.yyx.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
