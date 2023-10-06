36363824d9a5de3674782a0e7e8803dec4b799f2
lisonge <i@songe.li>
2023-10-06 13:39:20 +0800

    feat(掌上公交): 规则优化

--- src/apps/com.mygolbs.mybus.ts
+++ src/apps/com.mygolbs.mybus.ts
@@ -8,28 +8,111 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.mygolbs.mybus.LoginActivity',
-      rules: '[id="com.mygolbs.mybus:id/splash_container"] >n [text^="跳过"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12745634',
+      rules: [
+        {
+          matches:
+            '[id="com.mygolbs.mybus:id/splash_container"||id="com.mygolbs.mybus:id/adsRl"] [text^="跳过"]',
+          snapshotUrls: [
+            'https://gkd-kit.songe.li/import/12745634',
+            'https://gkd-kit.vercel.app/import/12790377',
+          ],
+        },
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790485',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '广告卡片',
+      activityIds: [
+        'com.mygolbs.mybus.RTimeActivity',
+        'com.mygolbs.mybus.NewHomePageActivity',
+        'com.mygolbs.mybus.StationsResultActivityNew',
+        'com.mygolbs.mybus.mapsearch.poisearch.PoiSearchActivity',
+      ],
+      rules: [
+        {
+          name: '点击底部中间x关闭图标',
+          matches:
+            '[id="com.mygolbs.mybus:id/ad_frameLayout"] RelativeLayout[childCount=4] > TextView[text.length>0] + ImageView + ImageView[clickable=true][id=null]',
+          snapshotUrls: [
+            'https://gkd-kit.vercel.app/import/12790521',
+            'https://gkd-kit.vercel.app/import/12790706',
+            'https://gkd-kit.vercel.app/import/12790841',
+            'https://gkd-kit.vercel.app/import/12790887',
+          ],
+        },
+        {
+          name: '点击右上/下角x关闭图标',
+          matches:
+            '[id="com.mygolbs.mybus:id/ad_frameLayout"] View[childCount=2||childCount=3] > View[childCount=4] > View[childCount=1] > Image[text=""]',
+          snapshotUrls: [
+            'https://gkd-kit.vercel.app/import/12790656',
+            'https://gkd-kit.vercel.app/import/12790903',
+          ],
+        },
+        {
+          name: '点击右上角x关闭图标-2',
+          matches:
+            '[id="com.mygolbs.mybus:id/ad_frameLayout"] View[childCount=5] > View[childCount=2][index=4] > View[childCount=1] > Image[text=""]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790610',
+        },
+        {
+          name: '点击右上角x关闭图标-3',
+          matches:
+            '[id="com.mygolbs.mybus:id/ad_frameLayout"] View[childCount=2] > View[childCount=6][index=0] > View[childCount=1][index=3] > Image[text=""]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790941',
+        },
+        {
+          name: '点击右下角x关闭按钮',
+          matches:
+            '[text="广告"] <3 View + @View[clickable=true] >2 [text="×"][visibleToUser=true]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791122',
+        },
+        {
+          name: '点击不感兴趣',
+          matches:
+            '@LinearLayout[clickable=true][childCount<=2] > [text="不感兴趣"]',
+          snapshotUrls: [
+            'https://gkd-kit.vercel.app/import/12790671',
+            'https://gkd-kit.vercel.app/import/12790551',
+            'https://gkd-kit.vercel.app/import/12790616',
+            'https://gkd-kit.vercel.app/import/12790707',
+            'https://gkd-kit.vercel.app/import/12790717',
+          ],
+        },
+        {
+          name: '点击暂不领取',
+          matches:
+            '[id="com.mygolbs.mybus:id/ll_ad"] [id="com.mygolbs.mybus:id/btn_cancel"]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',
+        },
+      ],
     },
     {
       key: 2,
-      name: '开屏广告_2', //text为跳过
-      activityIds: ['com.mygolbs.mybus.LoginActivity'],
+      name: '广告弹窗',
+      activityIds: ['com.mygolbs.mybus.mapsearch.poisearch.PoiSearchActivity'],
       rules: [
         {
-          matches: '[text="跳过"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790377',
+          name: '点击右上角x关闭图标',
+          matches:
+            '[id="android:id/content"] >3 FrameLayout[childCount=6][index=0] > FrameLayout[childCount=1][index=1] > ImageView[id=null]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790762',
         },
       ],
     },
     {
       key: 3,
-      name: '开屏广告_3', //text为null
-      activityIds: ['com.mygolbs.mybus.LoginActivity'],
+      name: '顶栏小广告',
+      activityIds: ['com.mygolbs.mybus.RTimeActivity'],
       rules: [
         {
-          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790485',
+          name: '点击右侧x关闭按钮',
+          matches: '[id="com.mygolbs.mybus:id/ad_close2"]',
+          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790841',
         },
       ],
     },
@@ -42,8 +125,8 @@ export default defineAppConfig({
       rules: [
         {
           matches: [
-            '[text$="开启通知权限"]',
-            '@[id="com.mygolbs.mybus:id/view_neg"] > [text="取消"]',
+            '[id="com.mygolbs.mybus:id/message"][text$="请开启通知权限"]',
+            '[id="com.mygolbs.mybus:id/view_neg"] > [id="com.mygolbs.mybus:id/negativeButton"][text="取消"]',
           ],
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12715980',
         },
@@ -58,209 +141,6 @@ export default defineAppConfig({
       rules:
         '@[id="com.mygolbs.mybus:id/iv_close"] < RelativeLayout - LinearLayout >n [text="签到成功"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12716035',
-    },
-    {
-      key: 21,
-      name: '首页广告_1，兼容收藏', //百毒联盟
-      activityIds: ['com.mygolbs.mybus.NewHomePageActivity'],
-      rules: [
-        {
-          matches: '[name="android.widget.ImageView"][text=null][index=2][width=44]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790521',
-        },
-        {
-          matches: '[text="虚假欺诈"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790551',
-        },
-        {
-          matches: '[text="暂不领取"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',//暂不领取新版本每个大广告都会有，链接就不改了
-        },
-      ],
-    },
-    {
-      key: 22,
-      name: '首页广告_2', //13b字节穿山甲
-      activityIds: ['com.mygolbs.mybus.NewHomePageActivity'],
-      rules: [
-        {
-          matches: '[name="android.widget.Image"][text=""][width=41]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790656',
-        },
-        {
-          matches: '[text="诱导点击"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790671',
-        },
-      ],
-    },
-    {
-      key: 31,
-      name: '搜索广告_1', //13b字节穿山甲
-      activityIds: ['com.mygolbs.mybus.mapsearch.poisearch.PoiSearchActivity'],
-      rules: [
-        {
-          matches: '[name="android.widget.Image"][text=""][width=33]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790610',
-        },
-        {
-          matches: '[text="虚假欺诈"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790616',
-        },
-        {
-          matches: '[text="暂不领取"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',//暂不领取新版本每个大广告都会有，链接就不改了
-        },
-      ],
-    },
-    {
-      key: 32,
-      name: '搜索广告_2', //百毒联盟
-      activityIds: ['com.mygolbs.mybus.mapsearch.poisearch.PoiSearchActivity', 'com.mygolbs.mybus.NewHomePageActivity'],
-      rules: [
-        {
-          matches: '[name="android.widget.ImageView"][index=2][width=44]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790706',
-        },
-        {
-          matches: '[text="虚假欺诈"]',
-          snapshotUrls: [
-            'https://gkd-kit.vercel.app/import/12790707',
-            'https://gkd-kit.vercel.app/import/12790717',
-          ],
-        },
-        {
-          matches: '[text="暂不领取"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',//暂不领取新版本每个大广告都会有，链接就不改了
-        },
-      ],
-    },
-    {
-      key: 41,
-      name: '随机弹窗_1', //腾讯广告
-      activityIds: ['com.mygolbs.mybus.mapsearch.poisearch.PoiSearchActivity', 'com.mygolbs.mybus.NewHomePageActivity'],
-      rules: [
-        {
-          matches: '[name="android.widget.ImageView"][width=58]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790762',
-        },
-      ],
-    },
-    {
-      key: 51,
-      name: '定位权限', //权限请求
-      activityIds: ['com.mygolbs.mybus.defines.CustomDialog'],
-      rules: [
-        {
-          matches: '[id="com.mygolbs.mybus:id/negativeButton"][text="取消"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790762',
-        },
-      ],
-    },
-    {
-      key: 52,
-      name: '实时车辆底部百度推广', //实时车辆底部百度推广自动回
-      activityIds: ['com.mygolbs.mybus.RTimeActivity'],
-      rules: [
-        {
-          matches: '[id="com.mygolbs.mybus:id/img_turnup"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791122',
-        },
-        {
-          matches: '[text="暂不领取"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',//暂不领取新版本每个大广告都会有，链接就不改了
-        },
-      ],
-    },
-    {
-      key: 61,
-      name: '实时车辆_1', //未知,顶栏小广告
-      activityIds: ['com.mygolbs.mybus.RTimeActivity'],
-      rules: [
-        {
-          matches: '[id="com.mygolbs.mybus:id/ad_close2"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790841',
-        },
-        {
-          matches: '[text="暂不领取"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',//暂不领取新版本每个大广告都会有，链接就不改了
-        },
-      ],
-    },
-    {
-      key: 62,
-      name: '实时车辆_2', //底部,百毒联盟
-      activityIds: ['com.mygolbs.mybus.RTimeActivity'],
-      rules: [
-        {
-          matches: '[name="android.widget.ImageView"][index=2][width=44][height=33]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790841',
-        },
-        {
-          matches: '[text="虚假欺诈"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790857',
-        },
-        {
-          matches: '[text="暂不领取"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',//暂不领取新版本每个大广告都会有，链接就不改了
-        },
-      ],
-    },
-    {
-      key: 63,
-      name: '实时车辆_3', //底部,13b字节穿山甲
-      activityIds: ['com.mygolbs.mybus.RTimeActivity'],
-      rules: [
-        {
-          matches: '[name="android.widget.Image"][text=""][width=41]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790941',
-        },
-        {
-          matches: '[text="虚假欺诈"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790972',
-        },
-        {
-          matches: '[text="暂不领取"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',//暂不领取新版本每个大广告都会有，链接就不改了
-        },
-      ],
-    },
-    {
-      key: 71,
-      name: '站台页面_1', //底部,百毒联盟
-      activityIds: ['com.mygolbs.mybus.StationsResultActivityNew'],
-      rules: [
-        {
-          matches: '[name="android.widget.ImageView"][index=2][width=44][height=33]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790887',
-        },
-        {
-          matches: '[text="虚假欺诈"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790913',
-        },
-        {
-          matches: '[text="暂不领取"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',//暂不领取新版本每个大广告都会有，链接就不改了
-        },
-      ],
-    },
-    {
-      key: 72,
-      name: '站台页面_2', //底部,13b字节穿山甲
-      activityIds: ['com.mygolbs.mybus.StationsResultActivityNew'],
-      rules: [
-        {
-          matches: '[name="android.widget.Image"][text=""][width=31]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790903',
-        },
-        {
-          matches: '[text="虚假欺诈"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12790926',
-        },
-        {
-          matches: '[text="暂不领取"]',
-          snapshotUrls: 'https://gkd-kit.vercel.app/import/12791579',//暂不领取新版本每个大广告都会有，链接就不改了
-        },
-      ],
     },
   ],
 });
