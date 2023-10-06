9afd2c7c60122cb3fcca61c7c55847f35b3bb36f
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-07 00:52:05 +0800

    feat(Soul)
    - 开屏广告

---
+++ src/apps/cn.soulapp.android.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig(
+{
+  id: 'cn.soulapp.android',
+  name: 'Soul',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['cn.soulapp.android.ad.ui.HotAdActivity'],
+      rules: [
+        {
+          matches: '[id="cn.soulapp.android:id/c_ad_skip_view_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12833280'
+        }
+      ],
+    },
+  ],
+});
+
