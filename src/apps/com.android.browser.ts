ab92d683c86219178efb31603b684ecb51be1275
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-07 00:53:17 +0800

    feat(小米浏览器)
    - 开屏广告

---
+++ src/apps/com.android.browser.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.android.browser',
+  name: '小米浏览器',
+  groups: [
+    {
+      key: 0,
+      name: '个性推荐获得更丰富内容',
+      activityIds: ['com.android.browser.BrowserActivity'],
+      rules: [
+        {
+          matches: '[id="com.android.browser:id/apv"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12829403',
+          }          
+      ],
+    },
+  ],
+});
+
