6ca3c37d0c3cbe6fa60e5e98973dd6343513747e
二刺螈 <i@songe.li>
2023-10-08 00:04:12 +0800

    feat(小米浏览器): 关闭[个性推荐开启提示]-规则优化

--- src/apps/com.android.browser.ts
+++ src/apps/com.android.browser.ts
@@ -6,11 +6,12 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '个性推荐获得更丰富内容',
+      name: '关闭[个性推荐开启提示]',
       activityIds: ['com.android.browser.BrowserActivity'],
       rules: [
         {
-          matches: '[id="com.android.browser:id/apv"]',
+          matches:
+            '@ImageView[id!=null][clickable=true] -2 [text="个性推荐获得更丰富内容"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12829403',
         },
       ],
