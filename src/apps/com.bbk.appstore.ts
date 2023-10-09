0d9405122f75bcb7ee2b9cccf79fedfa4faa17ee
二刺螈 <i@songe.li>
2023-10-10 00:38:02 +0800

    feat(vivo应用商店): 开屏广告-规则优化

--- src/apps/com.bbk.appstore.ts
+++ src/apps/com.bbk.appstore.ts
@@ -7,8 +7,10 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       activityIds: ['com.bbk.appstore.ui.AppStore'],
-      rules: '[text ^= "跳过" ]',
+      rules:
+        '[id="com.bbk.appstore:id/launch_activity_root_layout"] >6 [text^="跳过"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12847395',
     },
   ],
