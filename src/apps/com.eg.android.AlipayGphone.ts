41a0799e01513a8e90d206174f6f0d9802f1a5af
Kun Jinkao <wwt18689249843@gmail.com>
2023-10-09 20:45:59 +0800

    feat 【关闭花呗升级弹窗】完善规则

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -8,21 +8,28 @@ export default defineAppConfig({
       enable: false,
       key: 0,
       name: '关闭花呗升级弹窗',
-      activityIds:
-        'com.alipay.mobile.nebulax.integration.mpaas.activity.NebulaActivity$Main',
       rules: [
         {
           key: 0,
+          activityIds:
+            'com.alipay.mobile.nebulax.integration.mpaas.activity.NebulaActivity$Main',
           matches: 'View[childCount=3] > @Image - View[text="花呗服务升级"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12737055',
         },
         {
           key: 1,
+          activityIds: [
+            'com.alipay.android.msp.ui.views.MspUniRenderActivity',
+            'com.alipay.android.msp.ui.views.MspContainerActivity',
+          ],
           matches: [
             '[text="根据相关法律法规要求，请尽快完成花呗升级"]',
             'FrameLayout > FrameLayout  > FrameLayout > [text="关闭"]',
           ],
-          snapshotUrls: 'https://gkd-kit.songe.li/import/12826077',
+          snapshotUrls: [
+            'https://gkd-kit.songe.li/import/12826077', // com.alipay.android.msp.ui.views.MspUniRenderActivity
+            'https://gkd-kit.songe.li/import/12846198', // com.alipay.android.msp.ui.views.MspContainerActivity
+          ],
         },
       ],
     },
