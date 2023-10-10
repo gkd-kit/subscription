17f838e1e15b3260b4a024d02782974310b399b8
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-11 00:30:33 +0800

    Update com.jincheng.supercaculator.ts

--- src/apps/com.jincheng.supercaculator.ts
+++ src/apps/com.jincheng.supercaculator.ts
@@ -8,8 +8,11 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.jincheng.supercaculator.activity.MainActivity',
-      rules: '[id = "com.byted.pangle:id/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12859492',
+      rules: '@Image < View < View TextView[text = "反馈"]',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12859523',
+        'https://gkd-kit.songe.li/import/12859545',
+       ],
     },
   ],
 });
