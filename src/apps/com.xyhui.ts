1cf4a0501dc9b00ef4d88b61ad3cc5bb375b26d3
aoguai <1340219674@qq.com>
2023-10-11 18:30:48 +0800

    feat(PU口袋校园): 一些规则 (gkd-kit#926) - 开屏广告 - 首页-优量汇弹窗广告 - 首页-浮窗广告

--- src/apps/com.xyhui.ts
+++ src/apps/com.xyhui.ts
@@ -8,19 +8,21 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.xyhui.start.LoadingActivity',
-      rules: 'LinearLayout > [text^="跳过"]',
+      rules: '[name$="ViewGroup"||name$="LinearLayout"] > [text^="跳过"]',
       snapshotUrls: [
         'https://gkd-kit.gitee.io/import/12642486',
         'https://gkd-kit.songe.li/import/12846496',
+        'https://gkd-kit.songe.li/import/12868232',
       ],
     },
     {
       key: 1,
-      name: '优量汇广告',
+      name: '首页-优量汇弹窗广告',
       activityIds: [
         'com.xyhui.start.PUMainActivity',
         'com.huawei.android.launcher.unihome.UniHomeLauncher',
         'com.huawei.permissioncontroller.hwcust.appjump.AppJumpActivity',
+        'com.xyhui.start.LoadingActivity',
       ],
       rules: [
         {
@@ -33,6 +35,7 @@ export default defineAppConfig({
             'https://gkd-kit.gitee.io/import/12646519', // 立即抢购
             'https://gkd-kit.songe.li/import/12793157', // 立即安装
             'https://gkd-kit.songe.li/import/12793177', // 立即下载
+            'https://gkd-kit.songe.li/import/12868157', // 立即下载, activityId = 'com.xyhui.start.LoadingActivity'
             'https://gkd-kit.gitee.io/import/12646541', // 华为手机的快照，activityId = 'com.huawei.permissioncontroller.hwcust.appjump.AppJumpActivity'
           ],
         },
@@ -51,9 +54,15 @@ export default defineAppConfig({
     {
       key: 2,
       name: '首页-浮窗广告',
-      activityIds: 'com.xyhui.start.PUMainActivity',
+      activityIds: [
+        'com.xyhui.start.PUMainActivity',
+        'com.xyhui.start.LoadingActivity',
+      ],
       rules: 'ImageView[id="com.xyhui:id/deleteIv"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12846543',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12846543',
+        'https://gkd-kit.songe.li/import/12868119',
+      ],
     },
   ],
 });
