aea5e2ac14a89951778180a316c216f99c342b03
dlh <du__th@163.com>
2023-10-11 14:04:04 +0800

    feat( 天翼云盘 ): 开屏广告，首页弹窗

---
+++ src/apps/com.cn21.ecloud.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.cn21.ecloud',
+  name: '天翼云盘',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: ['com.cn21.ecloud.activity.login.StartActivity'],
+      rules: '[id="com.cn21.ecloud:id/tv_ad_skip"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12865476',
+    },
+    {
+      key: 1,
+      name: '首页弹窗',
+      activityIds: ['com.cn21.ecloud.activity.MainPageActivity'],
+      rules:
+        '[id="com.cn21.ecloud:id/content_frame"] >n FrameLayout >n @ImageView[desc=null] - View < View[childCount=2]',
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/12865481',
+        'https://gkd-kit.gitee.io/import/12865488',
+      ],
+    },
+  ],
+});
