e244f0ff03700d4989036fadc50ed696ff18b930
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-12 23:45:47 +0800

    Update com.lovebizhi.wallpaper.ts

--- src/apps/com.lovebizhi.wallpaper.ts
+++ src/apps/com.lovebizhi.wallpaper.ts
@@ -8,7 +8,8 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.adesk.picasso.view.MainActivity',
-      //来自 https://github.com/gkd-kit/subscription/issues/979 的优化建议，反馈会匹配到桌面活动ID
+      
+      // 来自 #979 的优化建议，反馈会匹配到桌面活动ID
       matchLauncher: true,
       rules: '[id = "com.byted.pangle:id/tt_splash_skip_btn"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12859492',
