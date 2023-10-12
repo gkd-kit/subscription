fe7e4c3dc7b18d64b9501417eedac3809668457c
二刺螈 <i@songe.li>
2023-10-13 01:25:20 +0800

    feat(四川航空): 开屏广告-规则优化

--- src/apps/com.bw30.zsch.ts
+++ src/apps/com.bw30.zsch.ts
@@ -7,12 +7,9 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: [
-        'com.bw.zsch.home.PrivacyPolicyActivity',
-        'com.miui.privacy.common.PermissionAlertActivity',
-        'com.bw.zsch.FirstDataLoadActivity',
-      ],
-      rules: 'TextView[text^="跳过"]',
+      matchLauncher: true,
+      quickFind: true,
+      rules: 'TextView[id="com.bw30.zsch:id/timer_text"]',
       snapshotUrls: [
         'https://gkd-kit.songe.li/import/12882959',
         'https://gkd-kit.songe.li/import/12882967',
