2a66e42fbdc885860370f5592932f7309378490e
walklty <walklty@hotmail.com>
2023-10-12 13:44:43 +0800

    Create com.glyh.mbank.pro.ts

---
+++ src/apps/com.glyh.mbank.pro.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.glyh.mbank.pro',
+  name: '桂林银行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.miui.home.launcher.Launcher',
+      matchLauncher: True, 
+      rules:
+        'TextView[id="com.glyh.mbank.pro:id/tv_countdown" && text*="跳过"]', 
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12876201',
+    },
+  ],
+});
