79c10ac9992da2aaebda91e5cc61b8c116f8412b
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-14 00:04:24 +0800

    feat(追漫大全): 广告弹窗-规则优化

--- src/apps/com.mj.mjyd.ts
+++ src/apps/com.mj.mjyd.ts
@@ -8,9 +8,13 @@ export default defineAppConfig({
       key: 1,
       name: '广告弹窗',
       quickFind: true,
+      activityIds: 'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Activity_T',
       rules:
-        '@ImageView < FrameLayout[childCount=1] <2 FrameLayout[childCount=6] >3 [text="立即下载"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/12877729',
+        '@ImageView < FrameLayout[childCount=1] <2 FrameLayout >3 [text="立即下载"]',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/12877729',
+        'https://gkd-kit.songe.li/import/12895086',
+      ],
     },
   ],
 });
