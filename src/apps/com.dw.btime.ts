5f49fca3a32ddd4dc85cfc8ef0b1a88d50378ca7
lisonge <i@songe.li>
2023-10-14 01:17:47 +0800

    feat(亲宝宝): 规则优化

--- src/apps/com.dw.btime.ts
+++ src/apps/com.dw.btime.ts
@@ -7,25 +7,25 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       activityIds: 'com.dw.btime.MainHomeTabActivity',
-      rules:
-        'TextView[text="向上滑动"] <2 LinearLayout + FrameLayout > Button[text^="跳过"]',
+      rules: '[id="com.dw.btime:id/ad_screen_skip_btn_v1"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12889257',
     },
     {
       key: 1,
       name: '应用内弹窗',
+      quickFind: true,
       activityIds: 'com.dw.btime.MainHomeTabActivity',
-      rules:
-        'ImageView[desc="亲宝宝"] < FrameLayout -2 ImageView[clickable=true]',
+      rules: '[id="com.dw.btime:id/iv_overlay_close"][clickable=true]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12889448',
     },
     {
       key: 2,
       name: '应用内横幅',
+      quickFind: true,
       activityIds: 'com.dw.btime.MainHomeTabActivity',
-      rules:
-        'RelativeLayout[childCount=4] > RelativeLayout[childCount=2] + ImageView[id="com.dw.btime:id/old_baby_close"]',
+      rules: '[id="com.dw.btime:id/old_baby_close"][clickable=true]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12889450',
     },
   ],
