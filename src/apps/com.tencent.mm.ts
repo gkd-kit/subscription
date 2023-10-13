60f0622b3ca3c8e842456a8876d95cff2c09f3e5
lisonge <i@songe.li>
2023-10-14 01:00:27 +0800

    feat(微信): 朋友圈广告-规则优化

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -15,7 +15,8 @@ export default defineAppConfig({
       rules: [
         {
           name: '点击广告卡片右上角',
-          matches: 'ImageView - TextView[text="广告"][id!=null]',
+          matches:
+            'ImageView - TextView[text="广告"][clickable=true][id!=null]',
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/12642588',
             'https://gkd-kit.gitee.io/import/12888129', //ImageView - TextView[text="广告"][id!=null][index=0]这个规则无法匹配该广告，需要删除[index=0]
