db5660dd9611796067d8751b32e37eae1f029d0a
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-14 00:29:58 +0800

    Update com.mj.mjyd.ts

--- src/apps/com.mj.mjyd.ts
+++ src/apps/com.mj.mjyd.ts
@@ -10,8 +10,10 @@ export default defineAppConfig({
       quickFind: true,
       activityIds:
         'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Activity_T',
-      rules:
+      rules:[
         '@ImageView < FrameLayout[childCount=1] <2 FrameLayout >3 [text="立即下载"]',
+        'ImageView -2 FrameLayout ImageView[depth=8 && index =0]',
+      ],
       snapshotUrls: [
         'https://gkd-kit.songe.li/import/12877729',
         'https://gkd-kit.songe.li/import/12895086',
