e2dc0562ae106892c6efe8a9a486ba1cee035ada
walklty <walklty@hotmail.com>
2023-10-13 10:18:56 +0800

    Update com.viva.note.ts

--- src/apps/com.viva.note.ts
+++ src/apps/com.viva.note.ts
@@ -14,6 +14,14 @@ export default defineAppConfig({
           matches: 'TextView[text="滑动屏幕"] <3 ViewGroup <2 ViewGroup < ViewGroup +3 ViewGroup > ViewGroup > @TextView[text="跳过"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12888429',
         },
+        {
+          matches: 'TextView[text^="点击跳转"] < LinearLayout < RelativeLayout -3 @TextView[text="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12888730',
+        },
+        {
+            matches: 'FrameLayout[childCount=4] > TextView[text^="跳过"]',
+            snapshotUrls: 'https://gkd-kit.gitee.io/import/12888762',
+          },
       ],
     },
     {
@@ -23,7 +31,7 @@ export default defineAppConfig({
             'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
             'com.maoyun.guoguo.MainActivity', 
         ],
-        matchLauncher: true, 
+        matchLauncher: true,
         rules: [
           {
             matches: '@Button[desc="关闭"] + Button[desc="不再显示"]',
@@ -38,6 +46,7 @@ export default defineAppConfig({
         key: 2,
         name: '应用内弹窗',
         activityIds: ['com.sigmob.sdk.base.common.TransparentAdActivity'],
+        matchLauncher: true,
         rules: [
           {
             matches: '@TextView[text="跳过"] < View <4 View +3 TextView[text="广告"]',
@@ -46,6 +55,14 @@ export default defineAppConfig({
           {
             matches: 'TextView[text="广告"] + View > TextView[text="反馈"] + @TextView',
             snapshotUrls: 'https://gkd-kit.gitee.io/import/12888419',
+          },
+          {
+            matches: 'TextView[text="广告"] <3 View -2 View > @TextView[text="跳过"]',
+            snapshotUrls: 'https://gkd-kit.gitee.io/import/12888578',
+          },
+          {
+            matches: 'TextView[text="反馈"] + @TextView +3 TextView[text="广告"]',
+            snapshotUrls: 'https://gkd-kit.gitee.io/import/12888647',
           },
         ],
       },
