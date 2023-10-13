dffcd0e9e5c0e2e99df2ed9d959e7bb981018c81
walklty <walklty@hotmail.com>
2023-10-13 17:25:45 +0800

    Create com.wudaokou.hippo.ts

---
+++ src/apps/com.wudaokou.hippo.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.wudaokou.hippo',
+  name: '盒马',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.wudaokou.hippo.launcher.splash.SplashActivity',
+      matchLauncher: true,
+      rules: [
+        {
+          matches: 'ImageView + TextView[id="com.wudaokou.hippo:id/splash_skip"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12891918',
+        },
+      ],
+    },
+  ],
+});
