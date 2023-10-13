39f8f4c59962ec49385af63a8b36736c4d960927
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-13 22:05:11 +0800

    feat(小米浏览器): 关闭[卡片广告]

--- src/apps/com.android.browser.ts
+++ src/apps/com.android.browser.ts
@@ -16,5 +16,37 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      enable: false,
+      key: 2,
+      name: '关闭[卡片广告]',
+      desc: '点击右侧X-弹出原因-点击【不感兴趣】',
+      activityIds: [
+        'com.android.browser.BrowserActivity',
+        'com.android.browser.flow.view.FeedbackDialog',
+        'com.android.browser.homepage.infoflow.InfoFlowVideoActivity',
+      ],
+      rules: [
+        {
+          key: 0,
+          matches:
+            '[id = "com.android.browser:id/ee" || id = "com.android.browser:id/ad_feed_back" ]',
+          snapshotUrls: [
+          'https://gkd-kit.songe.li/import/12893622',
+          'https://gkd-kit.songe.li/import/12893649',
+          'https://gkd-kit.songe.li/import/12893677',
+          ],
+        },
+        {
+          prekey: [0,1],
+          matches:
+            '@TextView[text="不感兴趣"] < ViewGroup < FrameLayout + FrameLayout [text="出现过于频繁"]',
+          snapshotUrls: [
+            'https://gkd-kit.songe.li/import/12893693',
+            'https://gkd-kit.songe.li/import/12893637',
+          ],
+        },
+      ],
+    },
   ],
 });
