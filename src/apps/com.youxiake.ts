127e81eb53b0da93ba7e227d5ef5344f00b01fda
walklty <walklty@hotmail.com>
2023-10-13 17:31:09 +0800

    Create com.youxiake.ts

---
+++ src/apps/com.youxiake.ts
@@ -0,0 +1,20 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.youxiake',
+  name: '游侠客旅行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.youxiake.ui.main.MainActivity',
+      matchLauncher: true,
+      rules: [
+        {
+          matches: 'LinearLayout > TextView[id="com.youxiake:id/start_skip_txt"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12891979',
+        },
+      ],
+    },
+  ],
+});
