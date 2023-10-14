9292222b398e58cca5ea3147b5930534a1e4ea71
lisonge <i@songe.li>
2023-10-15 02:29:24 +0800

    feat(菜鸟): 开屏广告-规则优化

--- src/apps/com.cainiao.wireless.ts
+++ src/apps/com.cainiao.wireless.ts
@@ -7,15 +7,15 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       activityIds: 'com.cainiao.wireless.homepage.view.activity.AdsActivity',
       rules: [
         {
-          matches: '[id="com.cainiao.wireless:id/homesplash_close_fullscreen"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12724823'],
-        },
-        {
-          matches: '@LinearLayout > [text*="跳过"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12901758'],
+          matches: '[text^="跳过"][text.length<=4]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12901758',
+            'https://gkd-kit.gitee.io/import/12724823',
+          ],
         },
       ],
     },
