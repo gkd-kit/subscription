d30ca80d10fa1bf0e451a470bf6fa9b86a649450
shanlan <hi@gobyte.cn>
2023-10-14 14:42:44 +0800

    feat(菜鸟): 开屏广告-新增规则

--- src/apps/com.cainiao.wireless.ts
+++ src/apps/com.cainiao.wireless.ts
@@ -8,8 +8,16 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.cainiao.wireless.homepage.view.activity.AdsActivity',
-      rules: '[id="com.cainiao.wireless:id/homesplash_close_fullscreen"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/12724823',
+      rules: [
+        {
+          matches: '[id="com.cainiao.wireless:id/homesplash_close_fullscreen"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12724823'],
+        },
+        {
+          matches: '@LinearLayout > [text*="跳过"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/12901758'],
+        },
+      ],
     },
   ],
 });
