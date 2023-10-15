7ba880e45c22e8f1ba00ea775f84d6c7b53977f4
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-15 14:24:43 +0800

    feat 小说阅读页面广告

--- src/apps/com.tencent.mtt.ts
+++ src/apps/com.tencent.mtt.ts
@@ -16,5 +16,38 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 10,
+      name: '小说阅读页面广告',
+      activityIds: 'com.tencent.mtt.MainActivity',
+      rules: [
+        {
+          key: 0,
+          matches: '@ViewGroup[visibleToUser=true] > ViewGroup > [desc="广告"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12907446',
+            'https://gkd-kit.gitee.io/import/12907445', // 限定 visibleToUser=true，防止在节点不可见时触发规则
+          ],
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches: '@ViewGroup > [desc="屏蔽此广告"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12907654',
+            'https://gkd-kit.gitee.io/import/12907651',
+          ],
+        },
+        {
+          preKeys: 1,
+          key: 2,
+          matches: '@ViewGroup > [desc="不感兴趣"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12907655',
+            'https://gkd-kit.gitee.io/import/12907653',
+          ],
+        },
+      ],
+    },
   ],
 });
