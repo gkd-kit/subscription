1c9630338df941fde3b605e778d76791777d47d1
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-17 00:21:54 +0800

    feat(用药参考): 开屏广告

--- src/apps/com.ky.medical.reference.ts
+++ src/apps/com.ky.medical.reference.ts
@@ -6,14 +6,10 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '弹窗广告',
-      activityIds: 'com.ky.medical.reference.promotion.AdDialogActivity',
-      rules: [
-        {
-          matches: '[id="com.ky.medical.reference:id/icClose"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12840924',
-        },
-      ],
+      name: '开屏广告',
+      activityIds: 'com.ky.medical.reference.activity.InitActivity',
+      rules: '[id= "com.ky.medical.reference:id/btn_go"][text ^= "关闭"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/12918049',
     },
   ],
 });
