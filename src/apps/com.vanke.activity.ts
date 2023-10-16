d65958e944a9c953eff4145e62dc07f62deec20e
二刺螈 <i@songe.li>
2023-10-17 00:21:26 +0800

    feat(住这儿): 规则优化

--- src/apps/com.vanke.activity.ts
+++ src/apps/com.vanke.activity.ts
@@ -7,8 +7,9 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      quickFind: true,
       activityIds: 'com.vanke.activity.module.common.SplashNewAct',
-      rules: 'Button[text ^= "跳过"]',
+      rules: 'Button[text^="跳过"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12917007',
     },
   ],
