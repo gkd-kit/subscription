34a9df274bc90c05c68a06fb30d63753d15f0a7f
walklty <walklty@hotmail.com>
2023-10-16 11:20:59 +0800

    Update com.tencent.qqlive.ts

--- src/apps/com.tencent.qqlive.ts
+++ src/apps/com.tencent.qqlive.ts
@@ -15,15 +15,18 @@ export default defineAppConfig({
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12700227',
         },
         {
-          matches: 'TextView[text="广告"] LinearLayout + TextView[text="跳过"]',
+          matches: 'TextView[text^="广告"] LinearLayout + TextView[text="跳过"]',
         },
         {
-          matches: '@[text="跳过"] - LinearLayout > [text="广告"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/12700122',
+          matches: '@[text="跳过"] - LinearLayout > [text^="广告"]',
+          snapshotUrls: [
+          'https://gkd-kit.gitee.io/import/12700122',
+          'https://gkd-kit.gitee.io/import/12910953',
+        ]
         },
         {
           matches:
-            '@[text="跳过"] < FrameLayout - FrameLayout >(2) [text="广告"]',
+            '@[text="跳过"] < FrameLayout - FrameLayout >(2) [text^="广告"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12700541',
         },
       ],
