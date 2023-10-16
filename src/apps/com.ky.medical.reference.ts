7feaf7035a3c4a87d8512180ba277924c9dc4a6e
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-17 00:28:47 +0800

    Update com.ky.medical.reference.ts

--- src/apps/com.ky.medical.reference.ts
+++ src/apps/com.ky.medical.reference.ts
@@ -5,11 +5,22 @@ export default defineAppConfig({
   name: '用药参考',
   groups: [
     {
-      key: 0,
+      key: -1,
       name: '开屏广告',
       activityIds: 'com.ky.medical.reference.activity.InitActivity',
-      rules: '[id= "com.ky.medical.reference:id/btn_go"][text ^= "关闭"]',
+      rules: '[id="com.ky.medical.reference:id/btn_go"][text^="关闭"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/12918049',
+    },
+    {
+      key: 0,
+      name: '弹窗广告',
+      activityIds: 'com.ky.medical.reference.promotion.AdDialogActivity',
+      rules: [
+        {
+          matches: '[id="com.ky.medical.reference:id/icClose"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12840924',
+        },
+      ],
     },
   ],
 });
