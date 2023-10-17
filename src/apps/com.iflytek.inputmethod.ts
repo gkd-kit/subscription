9bf3408ee57a63662817f9fe1dbad7b00837a68e
walklty <walklty@hotmail.com>
2023-10-17 16:19:08 +0800

    feat(讯飞输入法): 开屏广告

---
+++ src/apps/com.iflytek.inputmethod.ts
@@ -0,0 +1,24 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.iflytek.inputmethod',
+  name: '讯飞输入法',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      desc: '点击右上方跳过按钮',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds:
+        'com.iflytek.inputmethod.settingsnew.splash.activity.NewSplashActivity',
+      rules: [
+        {
+          matches:
+            '[text="跳过广告"] < [id="com.iflytek.inputmethod.settingsnew:id/ll_skip"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/12906597',
+        },
+      ],
+    },
+  ],
+});
