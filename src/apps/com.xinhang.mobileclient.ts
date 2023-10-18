82eec5cd4a3881bb8ce768ae021d38b1da877ded
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-18 20:47:57 +0800

    feat: 应用内广告弹窗

--- src/apps/com.xinhang.mobileclient.ts
+++ src/apps/com.xinhang.mobileclient.ts
@@ -13,5 +13,22 @@ export default defineAppConfig({
       rules: '[id="com.xinhang.mobileclient:id/cdp_skip"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/13024858',
     },
+    {
+      key: 1,
+      name: '应用内广告弹窗',
+      activityIds: 'com.xinhang.mobileclient.home.activity.HomeActivity',
+      rules: [
+        {
+          key: 0,
+          matches: 'View[desc^="close"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13024867',
+        },
+        {
+          key: 1,
+          matches: 'View > View > TextView + TextView[text=""]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13024869',
+        },
+      ],
+    },
   ],
 });
