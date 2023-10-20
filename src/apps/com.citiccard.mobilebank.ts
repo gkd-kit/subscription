caad062fca683f6639e6fede1079fc8dfb023836
walklty <walklty@hotmail.com>
2023-10-20 09:31:12 +0800

    feat(动卡空间): 开屏广告

--- src/apps/com.citiccard.mobilebank.ts
+++ src/apps/com.citiccard.mobilebank.ts
@@ -7,12 +7,20 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: ['com.citiccard.mobilebankapp.WelcomeActivity'],
+      activityIds: [
+        'com.citiccard.mobilebankapp.WelcomeActivity',
+        'com.android.packageinstaller.permission.ui.GrantPermissionsActivity',
+      ],
+      quickFind: true,
       rules: [
         {
           matches:
             '[id="com.citiccard.mobilebank:id/iv_popAdv"] + [id="com.citiccard.mobilebank:id/btn_skip"][text$="跳过"]',
           snapshotUrls: ['https://gkd-kit.gitee.io/import/12684908'],
+        },
+        {
+          matches: '[id="com.citiccard.mobilebank:id/jump"]',
+          snapshotUrls: ['https://gkd-kit.gitee.io/import/13049013'],
         },
       ],
     },
