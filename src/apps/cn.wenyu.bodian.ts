767e0c8a4859533a7496aacf33fd210a9c3cd3e6
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-22 16:13:42 +0800

    feat(波点音乐): 开屏广告

---
+++ src/apps/cn.wenyu.bodian.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.wenyu.bodian',
+  name: '波点音乐',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'cn.wenyu.bodian.MainActivity',
+      rules:
+        '[id="cn.wenyu.bodian:id/splash_container"] +n TextView[text="跳过广告"]',
+      snapshotUrls: 'https://gkd-kit.songe.li/import/13062529',
+    },
+  ],
+});
