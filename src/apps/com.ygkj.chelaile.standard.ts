f10f29d8c119cafb9bfb3b9f6edc6081ef37fd17
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-22 18:46:01 +0800

    feat: 应用内广告卡片

--- src/apps/com.ygkj.chelaile.standard.ts
+++ src/apps/com.ygkj.chelaile.standard.ts
@@ -12,5 +12,16 @@ export default defineAppConfig({
       rules: '[id="com.ygkj.chelaile.standard:id/cll_splash_ad_skip"]',
       snapshotUrls: 'https://gkd-kit.songe.li/import/13062992',
     },
+    {
+      key: 1,
+      name: '应用内广告卡片',
+      activityIds: 'dev.xesam.chelaile.app.module.func.SplashActivity',
+      rules:
+        'ImageView[id^="com.ygkj.chelaile.standard:id/cll_all_pic_close"][visibleToUser=true]',
+      snapshotUrls: [
+        'https://gkd-kit.songe.li/import/13062991',
+        'https://gkd-kit.songe.li/import/13062984',
+      ],
+    },
   ],
 });
