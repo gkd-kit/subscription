89eda8282a0b7091bb86c0b2c2dc935a88e58133
lisonge <i@songe.li>
2023-10-23 00:32:46 +0800

    feat(微音乐): 规则优化

--- src/apps/com.zhouyu.music.ts
+++ src/apps/com.zhouyu.music.ts
@@ -6,11 +6,13 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '应用内广告弹窗',
+      name: '广告弹窗',
       activityIds: 'com.zhouyu.music.activities.MainActivity',
       rules:
         '@TextView[id=null][clickable=true] + View > View > View [text="广告"]',
-      snapshotUrls: 'https://gkd-kit.songe.li/import/13062330',
+      snapshotUrls: 'https://i.gkd.li/import/13062330',
+      exampleUrls:
+        'https://github.com/gkd-kit/inspect/assets/38517192/798afc53-4aaf-481f-acb1-7c193bd8e6d6',
     },
   ],
 });
