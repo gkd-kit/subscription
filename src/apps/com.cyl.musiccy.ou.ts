0a97616f62a94ce468770e01073596cf0fa2067f
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-23 22:29:37 +0800

    格式更正

--- src/apps/com.cyl.musiccy.ou.ts
+++ src/apps/com.cyl.musiccy.ou.ts
@@ -33,7 +33,7 @@ export default defineAppConfig({
           snapshotUrls: 'https://gkd-kit.gitee.io/import/13063246',
         },
         {
-          mathes: '[id="com.cyl.musiccy.ou:id/ksad_splash_root_container"] [text="跳过"]',
+          matches: '[id="com.cyl.musiccy.ou:id/ksad_splash_root_container"] [text="跳过"]',
           snapshotUrls: 'https://gkd-kit.gitee.io/import/13071599',
         },
       ],
