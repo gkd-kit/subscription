f3fff5cc652badfb1fbdf52afa9c92e88f9e68cd
Nebulafalls <1924383963@qq.com>
2023-10-23 21:15:14 +0800

    添加fiil+规则

---
+++ src/apps/com.fengeek.f002.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.fengeek.f002',
+  name: 'fiil+',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      activityIds: ['com.fengeek.f002.WelcomeActivity'],
+      rules: [
+        {
+          matches: '[text$=	"跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13068924',
+        },
+      ],
+    },
+  ],
+});
