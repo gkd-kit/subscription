d14257e0093ee558fa759af40315cc6ab1ec6b5c
TOM <volatile@126.com>
2023-10-23 11:27:39 +0800

    feat:新增美柚开屏去广告

---
+++ src/apps/com.lingan.seeyou.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig(
+    {
+      id: 'com.lingan.seeyou',
+      name: '美柚',
+      groups: [
+        {
+          key: 1,
+          name: '开屏广告',
+          activityIds: ['com.meetyou.crsdk.CROpenScreenNewActivity'],
+          rules: [
+            {
+              matches: '[text="跳过"]',
+              snapshotUrls: 'https://gkd-kit.gitee.io/import/13065769',
+            },
+          ],
+        },
+      ],
+    }
+);
