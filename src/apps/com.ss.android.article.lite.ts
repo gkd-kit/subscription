cc47905afeed4f53efaa184e50b8fb2cbce9103a
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-24 15:18:21 +0800

    格式更正

--- src/apps/com.ss.android.article.lite.ts
+++ src/apps/com.ss.android.article.lite.ts
@@ -7,7 +7,7 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      quickfind: true,
+      quickFind: true,
       matchLauncher: true,
       activityIds: 'com.ss.android.article.lite.activity.SplashActivity',
       rules: '@FrameLayout[desc= "跳过广告"] > TextView[text="跳过广告"]',
