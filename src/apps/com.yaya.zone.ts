69bc66f04b90da7bb334cf4ed439ba13c99f6ae3
w12vgs <49601536+w12vgs@users.noreply.github.com>
2023-10-24 21:08:45 +0800

    叮咚买菜开屏广告

--- src/apps/com.yaya.zone.ts
+++ src/apps/com.yaya.zone.ts
@@ -3,21 +3,17 @@
 export default defineAppConfig({ 
   id: 'com.yaya.zone', 
   name: '叮咚买菜', 
-  groups: [{
-  id: 'com.yaya.zone',//应用ID
-  name: '叮咚买菜',//应用名称
   groups: [
     {
       key: 1,
-      name: '开屏广告',//规则名称
-      activityIds: ['cn.me.android.splash.activity.SplashActivity'],//ActivityId
+      name: '开屏广告',
+      activityIds: ['cn.me.android.splash.activity.SplashActivity'],
       rules: [
         {
-          matches: '[id="com.yaya.zone:id/tv_skip"]',//找点击元素
-          snapshotUrls: 'https://gkd-kit.gitee.io/snapshot/1698151049770',//快照地址
+          matches: '[id="com.yaya.zone:id/tv_skip"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/snapshot/1698151049770',
         },
       ],
     },
   ],
-}], 
 });
