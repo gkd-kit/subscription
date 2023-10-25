62015296b4550f49f7a10a38498d8afcee85b8a5
Nebulafalls <1924383963@qq.com>
2023-10-25 12:40:34 +0800

    添加自动连接耳机按钮

--- src/apps/com.fengeek.f002.ts
+++ src/apps/com.fengeek.f002.ts
@@ -15,5 +15,29 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '自动连接耳机',
+      activityIds: ['com.fengeek.f002.MainActivity'],
+      rules: [
+        {
+          key: 0,
+          name: '点击连接耳机按钮',
+          quickFind: true,
+          action: 'clickCenter',
+          matches: '[clickable=true][id="com.fengeek.f002:id/av_elc"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13161277',
+        },
+        {
+          preKeys: [0],
+          key: 1,
+          name: '点击扫描到的设备',
+          quickFind: true,
+          action: 'clickNode',
+          matches: '[clickable=true][id="com.fengeek.f002:id/device_container"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13161365',
+        }
+      ],
+    },
   ],
 });
