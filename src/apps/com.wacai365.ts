d76626d4549f78df5eb1df19dccc8641848025d7
lisonge <i@songe.li>
2023-10-26 23:30:16 +0800

    feat(挖财记账): 规则优化

--- src/apps/com.wacai365.ts
+++ src/apps/com.wacai365.ts
@@ -9,9 +9,20 @@ export default defineAppConfig({
       name: '开屏广告',
       quickFind: true,
       matchLauncher: true,
-      activityIds: 'com.wacai365.HomeActivity',
-      rules: '[id="com.wacai365:id/ksad_splash_circle_skip_view"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/13162861',
+      activityIds: [
+        'com.wacai365.HomeActivity',
+        'com.wacai.android.wind.splash.WindDialogActivity',
+      ],
+      rules: [
+        {
+          matches: '[id="com.wacai365:id/ksad_splash_circle_skip_view"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13162861',
+        },
+        {
+          matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13177538',
+        },
+      ],
     },
   ],
 });
