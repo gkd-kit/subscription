4158903f3adaf1d6d263d7ed56a0f9be8f9b9f2d
lisonge <i@songe.li>
2023-10-26 23:24:51 +0800

    feat(图图影视): 规则优化

--- src/apps/com.jsguohua.youquanmall.tt.ts
+++ src/apps/com.jsguohua.youquanmall.tt.ts
@@ -1,28 +1,6 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: 'xxxxx',
-  name: 'xxxx',
-  groups: [
-    {
-      key: 0,
-      name: 'xxxx',
-      desc: 'xxxx',
-      matchLauncher: true,
-      quickFind: true, //选择器的focus=true
-      activityIds: 'xxxx',
-          rules: [
-            {
-              matches: 'xxxxx', //[id="xx" || id="xx"], [text="xx"] +n * > * > [text="xx"] *指代全部
-              snapshotUrls: 'xxxxxx',
-            },
-          ],
-    },
-  ],
-});
-import { defineAppConfig } from '../types';
-
-export default defineAppConfig({
   id: 'com.jsguohua.youquanmall.tt',
   name: '图图影视',
   groups: [
@@ -32,23 +10,24 @@ export default defineAppConfig({
       matchLauncher: true,
       quickFind: true,
       activityIds: 'com.zero.flutter_qq_ads.page.AdSplashActivity',
-          rules: [
-            {
-              matches: '[text="跳过 "]',
-              snapshotUrls: 'https://gkd-kit.gitee.io/import/13163305',
-            },
-          ],
+      rules: [
+        {
+          matches: '[text^="跳过"][text.length<=3]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13163305',
+        },
+      ],
     },
     {
-        key: 1,
-        name: '应用内弹窗',
-        activityIds: 'com.zero.flutter_qq_ads.page.AdSplashActivity',
-            rules: [
-              {
-                matches: '[text="去微信看看"] < FrameLayout <n FrameLayout[childCount=2] -n FrameLayout > ImageView',
-                snapshotUrls: 'https://gkd-kit.gitee.io/import/13163314',
-              },
-            ],
-      },
+      key: 1,
+      name: '应用内弹窗',
+      activityIds: 'com.zero.flutter_qq_ads.page.AdSplashActivity',
+      rules: [
+        {
+          matches:
+            '[text="去微信看看"] < FrameLayout <n FrameLayout[childCount=2] -n FrameLayout > ImageView',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13163314',
+        },
+      ],
+    },
   ],
 });
