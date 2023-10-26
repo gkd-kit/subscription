1d6b407a8725dc3c4ff36b4335f429a76d65676e
Ran <2357752497@qq.com>
2023-10-26 17:07:09 +0800

    得间免费小说：开屏广告

--- src/apps/com.chaozh.iReader.dj.ts
+++ src/apps/com.chaozh.iReader.dj.ts
@@ -1,14 +1,19 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: 'org.daai.netcheck',
-  name: '网络万用表',
+  id: 'com.chaozh.iReader.dj',
+  name: '得间免费小说',
   groups: [
     {
       key: 0,
       name: '开屏广告',
-      rules: 'TextView[text*="点击跳过"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/13174128',
+      activityIds: ['com.chaozh.iReader.ui.activity.WelcomeActivity'],
+      rules: [
+        {
+          matches: '[id$=""com.byted.pangle:id/tt_splash_skip_btn""]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/snapshot/1698220670268',
+        },
+      ],
     },
   ],
 });
