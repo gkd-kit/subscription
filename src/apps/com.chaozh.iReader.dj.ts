0ca65bc79733e3a92d5758e6c3918afd4a09325b
Ran <2357752497@qq.com>
2023-10-26 16:55:24 +0800

    测试

--- src/apps/com.chaozh.iReader.dj.ts
+++ src/apps/com.chaozh.iReader.dj.ts
@@ -1,19 +1,14 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: 'com.chaozh.iReader.dj',
-  name: '得间免费小说',
+  id: 'org.daai.netcheck',
+  name: '网络万用表',
   groups: [
     {
-      key: 1,
+      key: 0,
       name: '开屏广告',
-      activityIds: ['com.chaozh.iReader.ui.activity.WelcomeActivity'],
-      rules: [
-        {
-          matches: '[id$="com.byted.pangle:id/tt_splash_skip_btn"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/snapshot/1698220670268',
-        },
-      ],
+      rules: 'TextView[text*="点击跳过"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13174128',
     },
   ],
 });
