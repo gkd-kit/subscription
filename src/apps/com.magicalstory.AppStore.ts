9728976990e6b2914fb9d388dfecfcd1db77e57e
walklty <47232102+walklty@users.noreply.github.com>
2023-10-27 16:23:49 +0800

    Create com.magicalstory.AppStore.ts

---
+++ src/apps/com.magicalstory.AppStore.ts
@@ -0,0 +1,35 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.magicalstory.AppStore',
+  name: '奇妙应用',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'com.hihonor.android.launcher.unihome.UniHomeLauncher',
+          rules: [
+            {
+              matches: '[text="跳过"]',
+              snapshotUrls: 'https://gkd-kit.gitee.io/import/13185745',
+            },
+          ],
+    },
+    {
+        key: 1,
+        name: '下方横幅',
+        activityIds: 'com.magicalstory.AppStore.search.searchActivity',
+            rules: [
+              {
+                matches: [
+                    '[id="com.magicalstory.AppStore:id/banner"]',
+                    'FrameLayout[childCount=1] > ImageView',
+                ],
+                snapshotUrls: 'https://gkd-kit.gitee.io/import/13185746',
+              },
+            ],
+      },
+  ],
+});
