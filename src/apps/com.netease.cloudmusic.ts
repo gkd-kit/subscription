9f088402a37b66e71e0b13c62fc11fd4d292c519
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-27 20:18:17 +0800

    feat 广告弹窗

--- src/apps/com.netease.cloudmusic.ts
+++ src/apps/com.netease.cloudmusic.ts
@@ -57,5 +57,18 @@ export default defineAppConfig({
       rules: ['@ImageView + ViewGroup > TextView[text="VIP歌曲免费听30分钟"]'],
       snapshotUrls: ['https://i.gkd.li/import/12843383'],
     },
+    {
+      key: 4,
+      name: '广告弹窗',
+      activityIds: 'com.netease.cloudmusic.activity.MainActivity',
+      rules: [
+        {
+          key: 0,
+          name: '音乐专辑售卖广告弹窗',
+          matches: 'WebView >n View > TextView + TextView + TextView',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13188737',
+        },
+      ],
+    },
   ],
 });
