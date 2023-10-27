9e617916842d04656d96041dc3166708bb03455f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-27 18:18:52 +0800

    feat 广告弹窗

--- src/apps/com.ping.cimoc.ts
+++ src/apps/com.ping.cimoc.ts
@@ -12,5 +12,13 @@ export default defineAppConfig({
       rules: '[id="com.ping.cimoc:id/splash_container"] >n [text="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/13187773',
     },
+    {
+      key: 1,
+      name: '广告弹窗',
+      activityIds: 'com.ping.cimoc.ui.activity.MainActivity',
+      rules:
+        'ImageView < FrameLayout + FrameLayout > FrameLayout[childCount=1] > ImageView',
+      snapshotUrls: 'https://i.gkd.li/import/13187751',
+    },
   ],
 });
