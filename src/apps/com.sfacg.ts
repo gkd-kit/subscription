e78f0ba5c4adfc61463115117d8f6726dc56dbc9
walklty <47232102+walklty@users.noreply.github.com>
2023-10-27 16:17:38 +0800

    Create com.sfacg.ts

---
+++ src/apps/com.sfacg.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.sfacg',
+  name: '菠萝包轻小说',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'com.sf.ui.launcher.LauncherActivity',
+          rules: [
+            {
+              matches: '[id="com.sfacg:id/tv_jump"]',
+              snapshotUrls: 'https://gkd-kit.gitee.io/import/13185579',
+            },
+          ],
+    },
+  ],
+});
