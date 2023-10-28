ec8bac7045078b811a3420d208114d07f0a7f433
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-29 00:05:20 +0800

    feat(应用汇): 开屏广告

---
+++ src/apps/com.yingyonghui.market.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.yingyonghui.market',
+  name: '应用汇',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchLauncher: true,
+      rules: [id="com.yingyonghui.market:id/splashAdvert_skipText"],
+      snapshotUrls: 'https://i.gkd.li/import/13194979',
+    },
+  ],
+});
