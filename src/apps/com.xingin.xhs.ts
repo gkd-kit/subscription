c8509c039a9720fee3d8e073375a04009090732e
lisonge <i@songe.li>
2023-10-29 02:03:34 +0800

    feat(小红书): 关闭开启通知弹窗

--- src/apps/com.xingin.xhs.ts
+++ src/apps/com.xingin.xhs.ts
@@ -14,9 +14,10 @@ export default defineAppConfig({
     },
     {
       key: 1,
-      name: '申请开启通知',
+      name: '关闭开启通知弹窗',
+      quickFind: true,
       activityIds: 'com.xingin.xhs.index.v2.IndexActivityV2',
-      rules: '[text ^="打开通知"] <2 LinearLayout + ImageView[clickable=true]',
+      rules: '@ImageView <2 FrameLayout >2 [text^="打开通知"]',
       snapshotUrls: 'https://i.gkd.li/import/13195753',
     },
   ],
