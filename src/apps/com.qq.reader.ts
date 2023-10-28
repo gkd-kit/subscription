36035148347babbe8bd226977547a30d11a9c6dc
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-28 15:56:58 +0800

    feat(QQ阅读): 青少年弹窗

--- src/apps/com.qq.reader.ts
+++ src/apps/com.qq.reader.ts
@@ -11,5 +11,12 @@ export default defineAppConfig({
       rules: '[id="com.qq.reader:id/splash_container"] + [text^="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/12811930',
     },
+    {
+      key: 2,
+      name: '青少年弹窗',
+      matchLauncher: true,
+      rules: 'TextView[text="QQ阅读青少年模式"] +3 TextView[text="我知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/13194867',
+    },
   ],
 });
