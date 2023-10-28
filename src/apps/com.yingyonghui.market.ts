e962f6fbb9c3090dcd103fe2ff56f8ad418c8605
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-29 00:22:25 +0800

    格式更正

--- src/apps/com.yingyonghui.market.ts
+++ src/apps/com.yingyonghui.market.ts
@@ -9,7 +9,7 @@ export default defineAppConfig({
       name: '开屏广告',
       quickFind: true,
       matchLauncher: true,
-      rules: [id="com.yingyonghui.market:id/splashAdvert_skipText"],
+      rules: [id = "com.yingyonghui.market:id/splashAdvert_skipText"],
       snapshotUrls: 'https://i.gkd.li/import/13194979',
     },
   ],
