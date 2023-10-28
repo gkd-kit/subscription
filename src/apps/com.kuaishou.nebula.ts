ed8b83a678c2bfbb64e898910245544d37e98593
Otis0227 <106000651+Otis0227@users.noreply.github.com>
2023-10-29 00:45:22 +0800

    feat(快手极速版):青少年弹窗跳过
    快手极速版，青少年弹窗跳过

--- src/apps/com.kuaishou.nebula.ts
+++ src/apps/com.kuaishou.nebula.ts
@@ -11,5 +11,12 @@ export default defineAppConfig({
       rules: '[id="com.kuaishou.nebula:id/splash_skip_text"]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/12519389',
     },
+    {
+      key: 1,
+      name: '青少年弹窗',
+      activityIds: 'com.yxcorp.gifshow.HomeActivity',
+      rules: '[id="com.kuaishou.nebula:id/positive"][text^="我知道了"]',
+      snapshotUrls: 'https://i.gkd.li/import/13196316',
+    },
   ],
 });
