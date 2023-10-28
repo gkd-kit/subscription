5d38e87988147c1a446489a24a3100df7066206f
lisonge <i@songe.li>
2023-10-28 23:28:45 +0800

    feat(LOFTER): 开屏广告

--- src/apps/com.lofter.android.ts
+++ src/apps/com.lofter.android.ts
@@ -8,7 +8,8 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.lofter.android.global.splash.HomeActivity',
-      rules: '[text^="跳过"]',
+      rules: '[text^="跳过"][text.length<=10]',
+      quickFind: true,
       snapshotUrls: [
         'https://i.gkd.li/import/13189846',
         'https://i.gkd.li/import/13189906',
