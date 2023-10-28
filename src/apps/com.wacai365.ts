02ebc956da23fa0a9645e32ee1993c3b4cbef671
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-28 18:51:59 +0800

    feat(OPPO手机管家): 版本更新

--- src/apps/com.wacai365.ts
+++ src/apps/com.wacai365.ts
@@ -1,28 +1,25 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: 'com.wacai365',
-  name: '挖财记账',
+  id: 'com.coloros.phonemanager',
+  name: 'OPPO手机管家',
   groups: [
     {
-      key: 0,
-      name: '开屏广告',
+      key: 3,
+      name: '版本更新',
       quickFind: true,
       matchLauncher: true,
-      activityIds: [
-        'com.wacai365.HomeActivity',
-        'com.wacai.android.wind.splash.WindDialogActivity',
-      ],
+      rules: '@Button[text="取消"] < LinearLayout -2 LinearLayout [text="检测到新版本"]',
       rules: [
         {
-          matches: '[id="com.wacai365:id/ksad_splash_circle_skip_view"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/13162861',
-        },
-        {
-          matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/13177538',
+          matches:[
+            '[text="检测到新版本"]'，
+            '[text="取消"]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13194979',
         },
       ],
+      snapshotUrls: 'https://i.gkd.li/import/13194979',
     },
   ],
 });
