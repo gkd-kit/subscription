4f7061dc1fff7f88d25dd4721863f0a83f3056bf
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-28 23:15:45 +0800

    Fix(Soul): 开屏广告

--- src/apps/cn.soulapp.android.ts
+++ src/apps/cn.soulapp.android.ts
@@ -12,11 +12,16 @@ export default defineAppConfig({
         'cn.soulapp.android.ad.ui.HotAdActivity',
         'cn.soulapp.android.component.startup.main.MainActivity',
       ],
-      rules: '[id="cn.soulapp.android:id/c_ad_skip_view_btn"]',
-      snapshotUrls: [
-        'https://gkd-kit.gitee.io/import/12833280',
-        'https://i.gkd.li/import/12850094',
+      rules: [{
+          action: 'clickCenter', //action=clickNode会造成点击失败，疑似GKD v1.41前版本问题
+          matches: '[id="cn.soulapp.android:id/c_ad_skip_view_btn"]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12833280',
+            'https://gkd-kit.gitee.io/import/12850094',
+          ],
+        },       
       ],
+     
     },
     {
       key: 1,
