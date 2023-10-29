55825827c31b13f7379697fa176f9cb3f05c2460
walklty <walklty@hotmail.com>
2023-10-30 05:15:06 +0800

    Create com.kkeji.news.client.ts

---
+++ src/apps/com.kkeji.news.client.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.kkeji.news.client',
+  name: '快科技',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchLauncher: true,
+      quickFind: true,
+      activityIds: 'com.kkeji.news.client.start.ActivitySplashMy',
+          rules: [
+            {
+              matches: '[id="com.kkeji.news.client:id/tv_counter"]',
+              snapshotUrls: 'https://i.gkd.li/import/13197536',
+            },
+          ],
+    },
+  ],
+});
