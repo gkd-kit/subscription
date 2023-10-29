64dec917a6e405a7b0a04b12cdd808e2ad6fae76
aoguai <1340219674@qq.com>
2023-10-29 16:07:22 +0800

    feat(易校园): 开屏广告规则优化(gkd-kit#1442)

--- src/apps/cn.com.yunma.school.app.ts
+++ src/apps/cn.com.yunma.school.app.ts
@@ -10,8 +10,15 @@ export default defineAppConfig({
       quickFind: true,
       matchLauncher: true,
       activityIds: 'com.yunma.app.ui.start.SplashActivity',
-      rules: '[id="cn.com.yunma.school.app:id/skip"]',
-      snapshotUrls: 'https://gkd-kit.gitee.io/import/13175275',
+      rules: [
+        '[text^="跳过"]',
+        '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+      ],
+      snapshotUrls: [
+        'https://gkd-kit.gitee.io/import/13175275',
+        'https://gkd-kit.gitee.io/import/13177064',
+        'https://gkd-kit.gitee.io/import/13179898',
+      ],
     },
   ],
 });
