0dc6ac73eac51bccd70fc34e5ac3dcf847d1f331
yanyu2023 <130368710+yanyu2023@users.noreply.github.com>
2023-10-29 12:15:34 +0800

    Create com.ikang.official.ts

---
+++ src/apps/com.ikang.official.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.ikang.official',
+  name: '爱康约体检查报告',
+  groups: [ {
+      key: 1,
+      name: '开屏广告',
+      activityIds: ['com.ikang.official.ui.AdvertActivity'],
+      rules: [
+        {
+          matches: '[id="com.ikang.official:id/llAdvertCountdown"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13197061',
+        },
+      ],
+    },],
+});
