aef8880e64a4c4e9077ddcbf113697013fd9b804
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-10-29 20:53:11 +0800

    feat 更新弹窗

--- src/apps/com.tencent.docs.ts
+++ src/apps/com.tencent.docs.ts
@@ -12,5 +12,12 @@ export default defineAppConfig({
       rules: '@LinearLayout > [id="com.tencent.docs:id/tv_gdt_ad_text"]',
       snapshotUrls: 'https://i.gkd.li/import/13198082',
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      activityIds: 'com.tencent.docs.DocsFlutterActivity',
+      rules: '@[desc="暂不更新"] + [desc="现在更新"]',
+      snapshotUrls: 'https://i.gkd.li/import/13198091',
+    },
   ],
 });
