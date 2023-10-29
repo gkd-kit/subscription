3d1e6604965f7811e46336ac3fb6ffc09fdf81fe
lisonge <i@songe.li>
2023-10-29 23:30:29 +0800

    feat(爱康约体检查报告): 开屏广告

--- src/apps/com.ikang.official.ts
+++ src/apps/com.ikang.official.ts
@@ -7,7 +7,10 @@ export default defineAppConfig({
     {
       key: 1,
       name: '开屏广告',
-      activityIds: ['com.ikang.official.ui.AdvertActivity'],
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
           matches: '[id="com.ikang.official:id/llAdvertCountdown"]',
