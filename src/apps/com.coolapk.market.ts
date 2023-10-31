fb08681a8ebb78337ad1b975b6630489812f1757
OldCoding <85047508+OldCoding@users.noreply.github.com>
2023-10-31 11:30:27 +0800

    Update com.coolapk.market.ts

--- src/apps/com.coolapk.market.ts
+++ src/apps/com.coolapk.market.ts
@@ -20,9 +20,12 @@ export default defineAppConfig({
         },
         {
           key: 1,
-          matches:
-            '[id="com.coolapk.market:id/ad_container"] >n [text^="跳过"]',
+          matches: [
+                '[id="com.coolapk.market:id/ad_container"]',
+                '[id="com.byted.pangle:id/tt_splash_skip_btn" || text^="跳过"]',
+        ], 
           snapshotUrls: 'https://gkd-kit.gitee.io/import/12917990',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13211392',
         },
       ],
     },
