1c9292dc87d513d8c17b7e52e2f32d5d78c08162
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-10-31 18:21:23 +0800

    feat(华为运动健康):开屏广告

--- src/apps/com.huawei.health.ts
+++ src/apps/com.huawei.health.ts
@@ -7,12 +7,17 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: ['com.huawei.health.MainActivity'],
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          matches: '[id="com.huawei.hms.ads.uiengine:id/hiad_skip_text"]',
+          matches: '[id$="id/hiad_skip_text"]',
           action: 'clickCenter',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12667766'],
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12667766'
+            'https://gkd-kit.gitee.io/import/13215012',
+          ],
         },
       ],
     },
