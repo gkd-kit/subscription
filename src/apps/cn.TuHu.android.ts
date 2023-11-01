d7028d235d30829210de0885918c55c71f71d9b2
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-01 23:34:51 +0800

    feat 请求通知权限弹窗

--- src/apps/cn.TuHu.android.ts
+++ src/apps/cn.TuHu.android.ts
@@ -14,5 +14,12 @@ export default defineAppConfig({
       rules: '[id="cn.TuHu.android:id/rl_activity_welcome_jump"]',
       snapshotUrls: 'https://i.gkd.li/import/12846408',
     },
+    {
+      key: 10,
+      name: '请求通知权限弹窗',
+      activityIds: 'cn.TuHu.Activity.tuhutab.TuHuTabActivity',
+      rules: '[text="开启推送提醒"] <2 * + [id="cn.TuHu.android:id/imgClose"]',
+      snapshotUrls: 'https://i.gkd.li/import/13228796',
+    },
   ],
 });
