a331632e24deef7a2354528761c48ab802e7f34f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-01 23:09:55 +0800

    feat 广告卡片

---
+++ src/apps/youqu.android.todesk.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'youqu.android.todesk',
+  name: 'ToDesk',
+  groups: [
+    {
+      key: 1,
+      name: '广告卡片',
+      activityIds: 'youqu.android.todesk.activity.WelcomeActivity',
+      rules:
+        '[id="youqu.android.todesk:id/sivDeviceBanner"] + [id="youqu.android.todesk:id/llClose"]',
+      snapshotUrls: 'https://i.gkd.li/import/13228546',
+    },
+  ],
+});
