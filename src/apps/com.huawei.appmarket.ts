939de96ee8daddfeb8fc18db2c14086888c1331c
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-01 23:07:22 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.huawei.appmarket.ts
+++ src/apps/com.huawei.appmarket.ts
@@ -7,13 +7,13 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: ['com.huawei.appmarket.MainActivity'],
-      rules: [
-        {
-          matches: '[id="com.huawei.appmarket:id/skip_textview"]',
-          snapshotUrls: ['https://gkd-kit.gitee.io/import/12683196'],
-        },
-      ],
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.huawei.appmarket:id/skip_textview"]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/12683196',
+    },
     {
       key: 1,
       name: '更新弹窗',
