cab8a84308ca14a7cfc1246265d1bbe9790d7e0f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-01 22:56:31 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.ygkj.chelaile.standard.ts
+++ src/apps/com.ygkj.chelaile.standard.ts
@@ -7,10 +7,23 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'dev.xesam.chelaile.app.module.func.SplashActivity',
-      matchLauncher: true,
-      rules: '[id="com.ygkj.chelaile.standard:id/cll_splash_ad_skip"]',
-      snapshotUrls: 'https://i.gkd.li/import/13062992',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches: '[id="com.ygkj.chelaile.standard:id/cll_splash_ad_skip"]',
+          snapshotUrls: 'https://i.gkd.li/import/13062992',
+        },
+        {
+          key: 1,
+          matches:
+            '[id="com.ygkj.chelaile.standard:id/cll_ad_container"] >n [text^="跳过"]',
+          snapshotUrls: 'https://i.gkd.li/import/13228351',
+        },
+      ],
     },
     {
       key: 1,
@@ -21,17 +34,6 @@ export default defineAppConfig({
       snapshotUrls: [
         'https://i.gkd.li/import/13062991',
         'https://i.gkd.li/import/13062984',
-      ],
-    },
-    {
-      key: 1,
-      name: '应用内广告卡片',
-      activityIds: 'dev.xesam.chelaile.app.module.func.SplashActivity',
-      rules:
-        'ImageView[id^="com.ygkj.chelaile.standard:id/cll_all_pic_close"][visibleToUser=true]',
-      snapshotUrls: [
-        'https://gkd-kit.songe.li/import/13062991',
-        'https://gkd-kit.songe.li/import/13062984',
       ],
     },
   ],
