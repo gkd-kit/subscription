bf6f08adc10060837a63dd35729ed337e56250ae
walklty <47232102+walklty@users.noreply.github.com>
2023-11-01 16:45:14 +0800

    Create com.oppo.community.ts

---
+++ src/apps/com.oppo.community.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.oppo.community',
+  name: 'OPPO 社区',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+          rules: [
+            {
+              matches: '[text^="跳过"][text.length=4]', 
+              snapshotUrls: 'https://gkd-kit.gitee.io/import/13224994',
+            },
+          ],
+    },
+  ],
+});
