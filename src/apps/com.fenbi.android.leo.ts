34f5ec5bcf89b573bfe160533fed150074f20c09
lisonge <i@songe.li>
2023-11-02 20:37:45 +0800

    feat(小猿口算): 开屏广告

--- src/apps/com.fenbi.android.leo.ts
+++ src/apps/com.fenbi.android.leo.ts
@@ -11,7 +11,7 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[text="跳过"]',
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://gkd-kit.gitee.io/import/13226136',
     },
     {
