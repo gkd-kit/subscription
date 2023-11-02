e244fb338c882cb42fe13163ed20b7b303f1a352
lisonge <i@songe.li>
2023-11-02 20:53:06 +0800

    feat(返利): 开屏广告

--- src/apps/com.fanli.android.apps.ts
+++ src/apps/com.fanli.android.apps.ts
@@ -11,7 +11,7 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: '[id="com.fanli.android.apps:id/splash_skip"] [text="跳过"]',
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13227495',
     },
   ],
