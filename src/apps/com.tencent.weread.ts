47041b2912f443247a34d5912410dea531e00fa5
lisonge <i@songe.li>
2023-11-02 22:01:36 +0800

    feat(微信读书): 规则优化

--- src/apps/com.tencent.weread.ts
+++ src/apps/com.tencent.weread.ts
@@ -8,14 +8,17 @@ export default defineAppConfig({
       key: 1,
       name: '系统推送弹窗',
       desc: '系统推送弹窗-点击取消',
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 30000,
+      resetMatch: 'app',
       activityIds: [
         'com.tencent.weread.module.bottomSheet.BottomSheetForFragment',
         'com.tencent.weread.WeReadFragmentActivity',
       ],
       rules: [
         {
-          matches:
-            '[id="com.tencent.weread:id/open_notification_title"] +(n) [id="com.tencent.weread:id/open_notification_close"]',
+          matches: '[id="com.tencent.weread:id/open_notification_close"]',
           snapshotUrls: [
             'https://gkd-kit.gitee.io/import/12642247',
             'https://i.gkd.li/import/13233735',
@@ -26,6 +29,10 @@ export default defineAppConfig({
     {
       key: 2,
       name: '青少年模式弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 30000,
+      resetMatch: 'app',
       activityIds: 'com.tencent.weread.WeReadFragmentActivity',
       rules: '[text="设置青少年模式"] - LinearLayout >2 [text="我知道了"]',
       snapshotUrls: 'https://i.gkd.li/import/13233668',
