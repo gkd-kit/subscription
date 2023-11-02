11d8c8df9960d4b24c30b60f215673a484101d49
lisonge <i@songe.li>
2023-11-02 22:07:59 +0800

    feat(丰巢): 开屏广告

--- src/apps/com.fcbox.hiveconsumer.ts
+++ src/apps/com.fcbox.hiveconsumer.ts
@@ -7,19 +7,15 @@ export default defineAppConfig({
     {
       key: 1,
       name: '开屏广告',
-      activityIds: [
-        'kt.com.fcbox.hiveconsumer.app.business.advert.XWSplashActivity',
-      ],
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
       rules: [
         {
-          matches:
-            '[id="com.fcbox.hiveconsumer:id/ksad_splash_circle_skip_view"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/13226664', //获取链接的方法在上一步有说明
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13226664',
         },
-        // {
-        //   matches: '[text*="跳过"][text.length<=5]',
-        //   snapshotUrls: 'https://gkd-kit.gitee.io/import/13234598',
-        // }
       ],
     },
   ],
