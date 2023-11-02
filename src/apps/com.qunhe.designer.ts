117d6177c4c0990453b0c4c64176dd48ea65af0d
lisonge <i@songe.li>
2023-11-02 22:10:12 +0800

    feat(酷家乐设计师): 开屏广告

--- src/apps/com.qunhe.designer.ts
+++ src/apps/com.qunhe.designer.ts
@@ -7,15 +7,14 @@ export default defineAppConfig({
     {
       key: 1,
       name: '开屏广告',
-      activityIds: ['com.qunhe.designer.ui.main.MainActivity'],
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
       rules: [
         {
-          matches: '[id="com.qunhe.designer:id/skip"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/13226570', //获取链接的方法在上一步有说明
-        },
-        {
-          matches: '[text*="跳过"][text.length<=5]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/13234598',
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13226570',
         },
       ],
     },
