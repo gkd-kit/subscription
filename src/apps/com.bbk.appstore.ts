1f1c023c0333cce5f45a13a55e77e3d5d60a43aa
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-03 14:02:50 +0800

    fix(vivo应用商店): 关闭消息通知新增控件id，优化代码

--- src/apps/com.bbk.appstore.ts
+++ src/apps/com.bbk.appstore.ts
@@ -20,17 +20,16 @@ export default defineAppConfig({
       quickFind: true,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: [
-        {
-          activityIds: 'com.bbk.appstore.upgrade.UpgradeNecessaryActivity',
-          matches: '[id="com.bbk.appstore:id/upgrade_necessary_btn_high_new"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/13198101',
-        },
-        {
-          activityIds: 'com.bbk.appstore.ui.AppStoreTabActivity',
-          matches: '[id="com.bbk.appstore:id/common_dialog_netgtive_btn"]',
-          snapshotUrls: 'https://gkd-kit.gitee.io/import/13198234',
-        },
+      activityIds: [
+        'com.bbk.appstore.upgrade.UpgradeNecessaryActivity',
+        'com.bbk.appstore.ui.AppStoreTabActivity',
+      ],
+      rules: 
+        '[id="com.bbk.appstore:id/upgrade_necessary_btn_high_new" || id="com.bbk.appstore:id/common_dialog_netgtive_btn" || "com.bbk.appstore:id/quit_button"]',
+      snapshotUrls: [
+      'https://gkd-kit.gitee.io/import/13198101', //com.bbk.appstore.upgrade.UpgradeNecessaryActivity
+      'https://gkd-kit.gitee.io/import/13198234', //com.bbk.appstore.ui.AppStoreTabActivity
+      'https://gkd-kit.gitee.io/import/13246971', //com.bbk.appstore.ui.AppStoreTabActivity
       ],
     },
   ],
