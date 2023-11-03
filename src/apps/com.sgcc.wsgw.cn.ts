a9030928983c9616609dc285ebdd228dffd2e4ed
walklty <47232102+walklty@users.noreply.github.com>
2023-11-03 16:26:50 +0800

    Update com.sgcc.wsgw.cn.ts

--- src/apps/com.sgcc.wsgw.cn.ts
+++ src/apps/com.sgcc.wsgw.cn.ts
@@ -11,10 +11,8 @@ export default defineAppConfig({
       actionMaximum: 1,
       resetMatch: 'app',
       quickFind: true,
-      rules: [
-        matches: '[text^="跳过"][text.length<10]',
-        snapshotUrls: 'https://gkd-kit.gitee.io/import/13247957',
-      ],
+      rules: '[text^="跳过"][text.length<10]',
+      snapshotUrls: 'https://gkd-kit.gitee.io/import/13247957',
     },
     {
       key: 1,
