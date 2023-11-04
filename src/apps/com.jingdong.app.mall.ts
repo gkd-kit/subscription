eb9195ce87b89ac0eabe468dc4d86c656fb2dcfe
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-04 13:21:11 +0800

    fix(京东): 新增开屏广告类型

--- src/apps/com.jingdong.app.mall.ts
+++ src/apps/com.jingdong.app.mall.ts
@@ -7,12 +7,24 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.jingdong.app.mall.MainFrameActivity',
-      rules:
-        '@RelativeLayout[desc="跳过"][id!=null] > TextView[text="跳过"][id!=null]',
-      snapshotUrls: [
-        'https://gkd-kit.gitee.io/import/12668289',
-        'https://gkd-kit.gitee.io/import/12519430',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '@RelativeLayout[desc="跳过"][id!=null] > TextView[text="跳过"][id!=null]',
+          snapshotUrls: [
+            'https://gkd-kit.gitee.io/import/12668289',
+            'https://gkd-kit.gitee.io/import/12519430',
+          ],
+        },
+        {
+          key: 1,
+          matches: 'ImageView[desc="闪屏广告"] + * >2 TextView[text="跳过"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13255802',
+        },
       ],
     },
     {
