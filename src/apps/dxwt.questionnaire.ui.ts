42a06a80aba37ecf684d0db7f8e0fe8a8142401b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-04 08:46:54 +0800

    feat 开屏广告

---
+++ src/apps/dxwt.questionnaire.ui.ts
@@ -0,0 +1,17 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'dxwt.questionnaire.ui',
+  name: '10000社区',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[desc^="关闭"][desc.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13255491',
+    },
+  ],
+});
