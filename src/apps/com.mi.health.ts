cb090dc43672e1bbb28aa282b6e9e772a1b8dfc6
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-05 14:54:11 +0800

    feat 好评弹窗

---
+++ src/apps/com.mi.health.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.mi.health',
+  name: '小米运动健康',
+  groups: [
+    {
+      key: 1,
+      name: '好评弹窗',
+      activityIds: 'com.xiaomi.fitness.baseui.common.CommonBaseActivity',
+      rules: '[id="com.mi.health:id/nextView"][text="下次再说"]',
+      snapshotUrls: 'https://i.gkd.li/import/13258813',
+    },
+  ],
+});
