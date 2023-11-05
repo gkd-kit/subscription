8a312bad36b6e76565a877c7f1c02ed84527db6b
lisonge <i@songe.li>
2023-11-05 21:40:04 +0800

    feat(虎扑): 推荐流广告-规则优化

--- src/apps/com.hupu.games.ts
+++ src/apps/com.hupu.games.ts
@@ -22,33 +22,31 @@ export default defineAppConfig({
       activityIds: [
         'com.hupu.games.main.MainActivity',
         'com.hupu.android.bbs.detail.PostDetailActivity',
+        'com.hupu.topic.TopicActivity',
       ],
+      quickFind: true,
       name: '推荐流广告',
       desc: '点击卡片右上角广告文字,出现广告反馈,点击屏蔽该广告',
       rules: [
         {
           key: 0,
-          quickFind: true,
-          activityIds: 'com.hupu.topic.TopicActivity',
           matches:
             '@[id="com.hupu.games:id/shield_view"] >2 [id="com.hupu.games:id/tv_tag"][text="广告"]',
           snapshotUrls: [
-            'https://gkd-kit.gitee.io/import/12511005', // com.hupu.games.main.MainActivity
-            'https://i.gkd.li/import/13258026', // com.hupu.topic.TopicActivity
-            'https://i.gkd.li/import/13259692', // com.hupu.android.bbs.detail.PostDetailActivity
+            'https://gkd-kit.gitee.io/import/12511005',
+            'https://i.gkd.li/import/13258026',
+            'https://i.gkd.li/import/13259692',
           ],
         },
         {
           preKeys: 0,
           key: 1,
-          activityIds:
-            'com.google.android.material.bottomsheet.BottomSheetDialog',
           matches:
             '@[clickable=true] > [id="com.hupu.games:id/tv_title"][text="屏蔽该广告"]',
           snapshotUrls: [
-            'https://gkd-kit.gitee.io/import/12511010', // com.hupu.games.main.MainActivity
-            'https://gkd-kit.gitee.io/import/12534848', // com.google.android.material.bottomsheet.BottomSheetDialog
-            'https://i.gkd.li/import/13259699', // com.hupu.android.bbs.detail.PostDetailActivity
+            'https://gkd-kit.gitee.io/import/12511010',
+            'https://gkd-kit.gitee.io/import/12534848',
+            'https://i.gkd.li/import/13259699',
           ],
         },
       ],
