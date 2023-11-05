7b91d1163bac73e7a011304eeec57dcc87c223e3
lisonge <i@songe.li>
2023-11-05 23:23:35 +0800

    feat(有道翻译官): 翻译界面-结果下方广告

--- src/apps/com.youdao.translator.ts
+++ src/apps/com.youdao.translator.ts
@@ -7,10 +7,13 @@ export default defineAppConfig({
     {
       key: 1,
       name: '翻译界面-结果下方广告',
+      desc: '点击广告卡片右上角x',
+      quickFind: true,
       activityIds: ['com.youdao.translator.activity.trans.TransResultActivity'],
       rules: [
         {
-          matches: '@ImageView[id=`com.youdao.translator:id/close_iv`]',
+          matches:
+            '@ImageView[id=`com.youdao.translator:id/close_iv`] + [id="com.youdao.translator:id/native_ad_flag_tv"]',
           snapshotUrls: 'https://i.gkd.li/import/13259910',
         },
       ],
