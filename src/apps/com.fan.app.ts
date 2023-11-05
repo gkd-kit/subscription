03d1100ed3dc6c678bea793fce08fa47d90008b5
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-05 08:35:37 +0800

    feat 广告卡片

--- src/apps/com.fan.app.ts
+++ src/apps/com.fan.app.ts
@@ -23,6 +23,28 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '广告卡片',
+      activityIds: 'com.mvvm.view.LazyMapStrategyActivity_TengXun',
+      rules: [
+        {
+          key: 0,
+          matches: '[desc^="dislike"] > View',
+          snapshotUrls: 'https://i.gkd.li/import/13258021',
+        },
+        {
+          preKeys: 0,
+          key: 1,
+          matches: '@LinearLayout > [text="不感兴趣"]',
+          snapshotUrls: 'https://i.gkd.li/import/13258015',
+        },
+        {
+          key: 2,
+          matches: '@Image < View -2 View >2 View > TextView[text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/13258018',
+        },
+      ],
     },
   ],
 });
