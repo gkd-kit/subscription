0f81238b1c807353d17c6c8aadb49c04adddc98d
Mr-BeanSir <113022853+Mr-BeanSir@users.noreply.github.com>
2023-11-06 16:34:52 +0800

    更新酷狗音乐概念版规则

--- src/apps/com.kugou.android.lite.ts
+++ src/apps/com.kugou.android.lite.ts
@@ -1,17 +1,20 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: 'com.kugou.android.lite',
-  name: '酷狗概念版',
-  groups: [
-    {
-      key: 0,
-      name: '开屏广告',
-      matchLauncher: true,
-      activityIds:
-        'com.kugou.android.app.splash.adcontainer.AdContainerActivity',
-      rules: '@[desc="跳过"] - FrameLayout >n [text="互动广告"]',
-      snapshotUrls: 'https://i.gkd.li/import/12919282',
-    },
-  ],
+    "id": "com.kugou.android.lite",
+    "name": "酷狗概念版",
+    "groups": [{
+            "name": "开屏广告",
+            "key": 0,
+            "matchLauncher": true,
+            "activityIds": ["com.kugou.android.app.splash.adcontainer.AdContainerActivity"],
+            "rules": [{
+                    "preKeys": [],
+                    "matches": ["LinearLayout > @TextView[text=`跳过`]"],
+                    "excludeMatches": []
+                }
+            ],
+            "snapshotUrls": ["https://i.gkd.li/snapshot/1699258882134"]
+        }
+    ]
 });
