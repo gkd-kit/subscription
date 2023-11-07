6ab96f49d8183badad994a62aff8b3c044f7693f
二刺螈 <i@songe.li>
2023-11-07 23:50:05 +0800

    feat(最会买): 开屏广告

--- src/apps/com.ruanmei.zuihuimai.ts
+++ src/apps/com.ruanmei.zuihuimai.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '开屏弹窗',
+      name: '开屏广告',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
