71f36e159c8b3c39dbb2e6578c7e92d26c130747
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-08 20:05:14 +0800

    feat 开屏广告（优化规则）

--- src/apps/run.xbud.android.ts
+++ src/apps/run.xbud.android.ts
@@ -7,8 +7,11 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'run.xbud.android.mvp.ui.other.SplashActivity',
-      rules: '[id="run.xbud.android:id/bodyLayout"] >n [text^="跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12777125',
     },
     {
