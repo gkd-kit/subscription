ae3fe8f605f17350d6e59b69ef3c7565ca36d5ed
walklty <47232102+walklty@users.noreply.github.com>
2023-11-08 10:37:32 +0800

    Create com.dingda.app.ts

---
+++ src/apps/com.dingda.app.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.dingda.app',
+  name: '叮嗒出行',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13290327',
+        },
+      ],
+    },
+  ],
+});
