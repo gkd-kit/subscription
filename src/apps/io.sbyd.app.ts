108f17acca792c6fc50f8b40a009117b685948e3
walklty <47232102+walklty@users.noreply.github.com>
2023-11-10 14:04:40 +0800

    Update io.sbyd.app.ts

--- src/apps/io.sbyd.app.ts
+++ src/apps/io.sbyd.app.ts
@@ -24,14 +24,23 @@ export default defineAppConfig({
       activityIds: 'io.sbyd.app.ui.book.read.ReadBookActivity',
       rules: [
         {
+          key: 1,
           matches:
             'TextView < LinearLayout[childCount=2] < FrameLayout[childCount=2] > FrameLayout[childCount=1] > ImageView',
-          snapshotUrls: 'https://i.gkd.li/import/import/13274336',
+          snapshotUrls: 'https://i.gkd.li/import/13274336',
         },
         {
-          matches:
-            'FrameLayout[childCount=6] > FrameLayout[childCount=1] > ImageView',
-          snapshotUrls: 'https://i.gkd.li/import/import/13302326',
+          key: 2,
+          matches: [
+            '[text^="立即" || text^="查看" || text^="领取"][text.length=4]', // 立即抢购,立即下载,立即申请,查看详情,领取优惠
+            '[depth=6][childCount=1] > ImageView[text=null][visibleToUser=true]', // 非广告页面下没有这种节点,不会误触
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13302326',
+            'https://i.gkd.li/import/13313576',
+            'https://i.gkd.li/import/13313624',
+            'https://i.gkd.li/import/13313576',
+          ],
         },
       ],
     },
