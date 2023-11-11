cd460a33612a92179cddf90b3228b512770a9f41
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-11-11 18:13:28 +0800

    feat(钉钉): 消息列表上方广告+文件即将过期提示

--- src/apps/com.alibaba.android.rimet.ts
+++ src/apps/com.alibaba.android.rimet.ts
@@ -26,5 +26,43 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      enable: false,
+      key: 1,
+      name: '消息列表上方广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: "app",
+      activityIds: 'com.bbk.launcher2.Launcher',
+      rules: [
+        {
+          key: 0,
+          matches:
+            'ImageView < FrameLayout + FrameLayout > [id="com.alibaba.android.rimet:id/icft_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13325125',
+          action: "clickCenter",
+        },
+      ],
+    },
+    {
+      enable: false,
+      key: 2,
+      name: '文件即将过期提示',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: "app",
+      activityIds: 'com.bbk.launcher2.Launcher',
+      rules: [
+        {
+          key: 0,
+          matches:
+            '@FrameLayout[id="com.alibaba.android.rimet:id/layout_close"] -2 TextView[text="文件即将过期请备份"]',
+          action: "clickCenter",
+          snapshotUrls: 'https://i.gkd.li/import/13325125',
+        },
+      ],
+    },
   ],
 });
