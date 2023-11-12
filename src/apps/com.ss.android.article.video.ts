a73e5c8346c5f4251bc6e2122858ea948f914e44
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-12 23:32:10 +0800

    feat 更新弹窗

--- src/apps/com.ss.android.article.video.ts
+++ src/apps/com.ss.android.article.video.ts
@@ -10,6 +10,14 @@ export default defineAppConfig({
       activityIds: 'com.ixigua.commonui.uikit.dialog.XGAlertDialog',
       snapshotUrls: ['https://i.gkd.li/import/12472628'],
       rules: ['TextView[text="我知道了"][clickable=true]'],
+    {
+      key: 1,
+      name: '更新弹窗',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@[desc="关闭"] - LinearLayout > [text="立即升级"]',
+      snapshotUrls: 'https://i.gkd.li/import/13328430',
     },
   ],
 });
