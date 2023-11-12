2e68e4457e4db85b6ac100d7a6ce65fc42793951
lisonge <i@songe.li>
2023-11-13 01:37:39 +0800

    feat(网易邮箱): 开屏广告

--- src/apps/com.netease.mobimail.ts
+++ src/apps/com.netease.mobimail.ts
@@ -14,10 +14,13 @@ export default defineAppConfig({
         {
           key: 0,
           name: '开屏广告-0',
-          matches: '[id="com.netease.mobimail:id/ad_skip"][clickable=true]',
+          quickFind: true,
+          matches: '[text*="跳过"][text.length<=10][clickable=true]',
           snapshotUrls: [
             'https://i.gkd.li/import/12667519',
             'https://i.gkd.li/import/13328425', // 限定 [clickable=true] 防止误触假的跳过按钮
+            'https://i.gkd.li/import/12686132',
+            'https://i.gkd.li/import/13328441',
           ],
         },
         {
@@ -25,21 +28,6 @@ export default defineAppConfig({
           name: '开屏广告-1',
           matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
           snapshotUrls: 'https://i.gkd.li/import/12686093',
-        },
-        {
-          key: 2,
-          name: '开屏广告-2',
-          matches:
-            '[id="com.netease.mobimail:id/ad_placeholder"] >5 TextView[text^="跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/12686132',
-        },
-        {
-          key: 3,
-          name: '开屏广告-3',
-          quickFind: true,
-          matches:
-            '[id="com.netease.mobimail:id/anythink_myoffer_splash_skip"]',
-          snapshotUrls: 'https://i.gkd.li/import/13328441',
         },
       ],
     },
