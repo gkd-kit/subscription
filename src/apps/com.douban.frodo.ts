048d3fe5821b70470325ab0dd7b85e00d69cacb0
lisonge <i@songe.li>
2023-11-13 00:04:23 +0800

    feat(豆瓣): 开屏广告

--- src/apps/com.douban.frodo.ts
+++ src/apps/com.douban.frodo.ts
@@ -7,11 +7,11 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: [
-        'com.douban.frodo.activity.SplashActivity',
-        'com.douban.frodo.splash.SplashActivityHot',
-      ],
-      rules: '[id="com.douban.frodo:id/skip"||text^="跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: [
         'https://i.gkd.li/import/12505151',
         'https://i.gkd.li/import/12505152',
