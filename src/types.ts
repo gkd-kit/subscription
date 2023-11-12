82960dd0ccfd593f302666cd62539506d8487759
lisonge <i@songe.li>
2023-11-13 01:54:00 +0800

    chore: quickFind 说明

--- src/types.ts
+++ src/types.ts
@@ -53,7 +53,6 @@ type CommonProps = {
   delay?: number;
 
   /**
-   * 当节点支持qucikFind时，节点属性列表顶部将出现'可快速查找'标识,例：[id="com.ct.client:id/tvNo"](https://i.gkd.li/import/13316168)
    *
    * 如果开启, 此规则下的所有 `末尾属性选择器`的`第一个属性选择表达式`符合下面的结构之一的选择器 将使用快速查找
    *
@@ -74,6 +73,12 @@ type CommonProps = {
    * 它是优点是快速, 因为遍历所有节点是一个耗时行为, 虽然多数情况下这种耗时较低
    *
    * 但是在某些软件比如 哔哩哔哩 的开屏广告在这种耗时下延迟可达 1-2s, 这也是导致 [gkd-kit/gkd#60](https://github.com/gkd-kit/gkd/issues/60) 的原因
+   *
+   * ---
+   *
+   * v1.4.1 版本后生成的快照时将标识每个节点是否可 quickFind, 网页审查工具属性面板顶部会注明这个标识
+   *
+   * [![image](https://github.com/gkd-kit/subscription/assets/38517192/80cdbad1-fa8d-4415-81a1-cecaa7d69e33)](https://i.gkd.li/import/13316168)
    *
    */
   quickFind?: boolean;
