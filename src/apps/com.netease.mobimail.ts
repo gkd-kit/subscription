7f7bb5e552de25ba220878f89d70ffd1697a0a52
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-12 23:16:48 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.netease.mobimail.ts
+++ src/apps/com.netease.mobimail.ts
@@ -7,20 +7,39 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.netease.mail.ad.launch.ui.SplashAdActivity',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          matches: '[id="com.netease.mobimail:id/ad_skip"]',
-          snapshotUrls: 'https://i.gkd.li/import/12667519',
+          key: 0,
+          name: '开屏广告-0',
+          matches: '[id="com.netease.mobimail:id/ad_skip"][clickable=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12667519',
+            'https://i.gkd.li/import/13328425', // 限定 [clickable=true] 防止误触假的跳过按钮
+          ],
         },
         {
+          key: 1,
+          name: '开屏广告-1',
           matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
           snapshotUrls: 'https://i.gkd.li/import/12686093',
         },
         {
+          key: 2,
+          name: '开屏广告-2',
           matches:
             '[id="com.netease.mobimail:id/ad_placeholder"] >5 TextView[text^="跳过"]',
           snapshotUrls: 'https://i.gkd.li/import/12686132',
+        },
+        {
+          key: 3,
+          name: '开屏广告-3',
+          quickFind: true,
+          matches:
+            '[id="com.netease.mobimail:id/anythink_myoffer_splash_skip"]',
+          snapshotUrls: 'https://i.gkd.li/import/13328441',
         },
       ],
     },
