2530beb787d1571cc88f323b4c9c4ca7b7b6c978
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-12 13:56:46 +0800

    feat(萤石云视频):开屏广告

--- src/apps/com.videogo.ts
+++ src/apps/com.videogo.ts
@@ -13,9 +13,17 @@ export default defineAppConfig({
       resetMatch: 'app',
       rules: [
         {
+          key: 0,
+          name: '【跳过+倒计时】型',
           matches: '[id="com.videogo:id/ad_skip_tv"]',
           snapshotUrls: 'https://i.gkd.li/import/12565356',
         },
+        {
+          key: 1,
+          name: '【圆圈跳过】型',
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13327309',
+        },  
       ],
     },
     {
