c175e0213e1b2b1e92e2cdec41d0963dbf6a15c0
xe5700 <xe5700@outlook.com>
2023-11-13 12:03:02 +0800

    feat(比亚迪王朝): 添加跳过自动更新支持

---
+++ src/apps/com.byd.aeri.caranywhere.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.byd.aeri.caranywhere',
+  name: '比亚迪王朝',
+  groups: [
+    {
+      key: 1,
+      enable: false,
+      name: '跳过自动更新',
+      activityIds: ['com.byd.aeri.splash.module_splash.activity.TabManager'],
+      rules: [
+        {
+          key: 1,
+          matches:
+            '[id="com.byd.aeri.caranywhere:id/tv_dialog_title"][text="版本更新提示"]',
+        },
+        {
+          matches: '[id="com.byd.aeri.caranywhere:id/tv_cancel"]',
+          snapshotUrls: 'https://gkd-kit.gitee.io/import/13242667', //获取链接的方法在上一步有说明
+          preKeys: [1],
+        },
+      ],
+    },
+  ],
+});
