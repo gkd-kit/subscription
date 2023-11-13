15d149453c796b648fd9efbe938315a1b2023812
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-13 20:41:30 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.github.eprendre.tingshu.ts
+++ src/apps/com.github.eprendre.tingshu.ts
@@ -7,9 +7,11 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.github.eprendre.tingshu.ui.SplashActivity',
-      rules:
-        '[id="com.github.eprendre.tingshu:id/frame_layout"] >n [text^="跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12783430',
     },
     {
