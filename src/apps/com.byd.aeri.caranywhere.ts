ac9bd70435c511f8df16261a3d2bdcbd773ef2d9
lisonge <i@songe.li>
2023-11-14 20:13:52 +0800

    feat(比亚迪王朝): 更新弹窗

--- src/apps/com.byd.aeri.caranywhere.ts
+++ src/apps/com.byd.aeri.caranywhere.ts
@@ -5,20 +5,20 @@ export default defineAppConfig({
   name: '比亚迪王朝',
   groups: [
     {
-      key: 1,
       enable: false,
-      name: '跳过自动更新',
+      key: 1,
+      name: '更新弹窗',
+      resetMatch: 'app',
+      actionMaximum: 1,
       activityIds: ['com.byd.aeri.splash.module_splash.activity.TabManager'],
       rules: [
         {
-          key: 1,
-          matches:
-            '[id="com.byd.aeri.caranywhere:id/tv_dialog_title"][text="版本更新提示"]',
-        },
-        {
-          matches: '[id="com.byd.aeri.caranywhere:id/tv_cancel"]',
-          snapshotUrls: 'https://i.gkd.li/import/13348383', //获取链接的方法在上一步有说明
-          preKeys: [1],
+          quickFind: true,
+          matches: [
+            '[id="com.byd.aeri.caranywhere:id/tv_determine"][text="立即安装"]',
+            '[id="com.byd.aeri.caranywhere:id/tv_cancel"]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13348383',
         },
       ],
     },
