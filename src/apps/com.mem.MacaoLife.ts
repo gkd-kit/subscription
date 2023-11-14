9f7a5c2bac601d18d17498ba15005b05b40f1a5a
JammyLin <jammylin123@outlook.com>
2023-11-15 00:10:53 +0800

    feat(澳覓): 开屏、浮窗广告及开启通知提示框

---
+++ src/apps/com.mem.MacaoLife.ts
@@ -0,0 +1,34 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.mem.MacaoLife',
+  name: '澳覓',
+  groups: [
+    {
+      key: 0,
+      name: '開屏廣告',
+      matchTime: 10000,
+      actionDelay: 800, // 剛啟動時開屏廣告點擊不到
+      quickFind: true,
+      activityIds: 'com.mem.life.ui.launch.LaunchActivity',
+      rules: '[id="com.mem.MacaoLife:id/skip_time_out_layout"]',
+      snapshotUrls: 'https://i.gkd.li/import/13350651',
+    },
+    {
+      key: 1,
+      name: '開啟通知提示',
+      quickFind: true,
+      activityIds: 'com.mem.life.ui.launch.LaunchActivity',
+      rules: '[id="com.mem.MacaoLife:id/cancel"]',
+      snapshotUrls: 'https://i.gkd.li/import/13350665',
+    },
+    {
+      key: 2,
+      name: '浮窗廣告',
+      quickFind: true,
+      activityIds: 'com.mem.life.ui.home.HomeActivity',
+      rules: '[id="com.mem.MacaoLife:id/close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13350685',
+    },
+  ],
+});
