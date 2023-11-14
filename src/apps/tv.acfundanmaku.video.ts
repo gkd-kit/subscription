9b38f90dbb6339ab8d49556be82969705312d7b8
lisonge <i@songe.li>
2023-11-14 21:11:31 +0800

    feat(AcFun): 开屏广告

--- src/apps/tv.acfundanmaku.video.ts
+++ src/apps/tv.acfundanmaku.video.ts
@@ -7,10 +7,13 @@ export default defineAppConfig({
     {
       key: 1,
       name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
       rules: [
         {
-          activityIds: 'tv.acfun.core.module.splash.SplashActivity',
-          matches: '[id=\'tv.acfundanmaku.video:id/splashSkipView\']',
+          matches: '[id="tv.acfundanmaku.video:id/splashSkipView"]',
           exampleUrls:
             'https://m.gkd.li/6328439/1a9541c5-fba7-4ecc-a276-f19a5641df1f',
           snapshotUrls: 'https://i.gkd.li/import/13348326',
