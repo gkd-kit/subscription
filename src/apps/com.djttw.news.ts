d5a291cd82db72ea044bfc526e6552a55d27049a
Zeng Weiyu <zerozwy@gmail.com>
2023-11-15 11:30:42 +0800

    feat(党建头条):开屏广告

---
+++ src/apps/com.djttw.news.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+    "id": "com.djttw.news",
+    "name": "党建头条",
+    "groups": [
+      {
+        "key": 1,
+        "name": "开屏广告",
+        "matchTime": 10000,
+        "actionMaximum": 1,
+        "resetMatch": 'app',
+        "rules": [
+          {
+            "activityIds": "io.dcloud.PandoraEntryActivity",
+            "matches": "[text='跳过']",
+            "exampleUrls": "https://m.gkd.li/6328439/3434673a-67eb-4c81-8fd3-c69dfa8d00db",
+            "snapshotUrls": "https://i.gkd.li/import/13359665"
+          }
+        ]
+      }
+    ]
+  });
