bcc24c5fa1d1150d77194491413c651ad891e29e
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-16 23:04:46 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.autonavi.minimap.ts
+++ src/apps/com.autonavi.minimap.ts
@@ -7,9 +7,11 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.autonavi.map.activity.NewMapActivity',
-      rules:
-        '[id="com.autonavi.minimap:id/fl_splash_container"] >n [text^="跳过"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@LinearLayout > [text^="跳过"][text.length<=10]',
       snapshotUrls: [
         'https://i.gkd.li/import/12667556',
         'https://i.gkd.li/import/12750045',
