2dadf416c44b67d9921710f39ee1fe2f33c39a77
Zeng Weiyu <zerozwy@gmail.com>
2023-11-16 17:35:19 +0800

    feat(oppo阅读):开屏广告

---
+++ src/apps/com.heytap.reader.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+    "id": "com.heytap.reader",
+    "name": "阅读",
+    "groups": [
+        {
+            "key": 1,
+            "name": "开屏广告",
+            matchTime: 10000,
+            actionMaximum: 1,
+            resetMatch: 'app',
+            "rules": [
+                {
+                    "activityIds": "com.chaozh.iReader.ui.activity.WelcomeActivity",
+                    "matches": "[text*='跳过']",
+                    "exampleUrls": "https://m.gkd.li/6328439/ee16eabc-470a-45b5-8d5f-398af1cae47e",
+                    "snapshotUrls": "https://i.gkd.li/import/13375516"
+                }
+            ]
+        }
+    ]
+});
