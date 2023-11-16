500b800ce357c5f3dd19a4178c6f266e70531f03
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-16 23:34:35 +0800

    feat 卡片式广告

--- src/apps/com.youdao.note.ts
+++ src/apps/com.youdao.note.ts
@@ -17,5 +17,20 @@ export default defineAppConfig({
         'https://i.gkd.li/import/13399002',
       ],
     },
+    {
+      key: 1,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 1,
+          name: '首页上方卡片广告',
+          activityIds: 'com.youdao.note.activity2.MainActivity',
+          quickFind: true,
+          matches:
+            '[id="com.youdao.note:id/recycle_view"] + [id="com.youdao.note:id/close_ad"]',
+          snapshotUrls: 'https://i.gkd.li/import/13379524',
+        },
+      ],
+    },
   ],
 });
