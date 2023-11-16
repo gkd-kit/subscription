3ab63e1122e08b9669a993a1d653a36ae04007e3
lisonge <i@songe.li>
2023-11-16 21:50:12 +0800

    feat(唱吧): 开屏广告

--- src/apps/com.changba.ts
+++ src/apps/com.changba.ts
@@ -1,20 +1,22 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: "com.changba",
-  name: "唱吧",
+  id: 'com.changba',
+  name: '唱吧',
   groups: [
     {
       key: 0,
-      name: "开屏广告",
-      desc: "跳过开屏广告",
+      name: '开屏广告',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      matchTime: 10000,
       rules: [
         {
-          activityIds: "com.changba.module.advertise.splash.AdSplashActivity",
-          matches: "[id$='com.changba:id/ad_timer']",
-          snapshotUrls: "https://i.gkd.li/import/13373946"
-        }
-      ]
+          quickFind: true,
+          matches: '[id="com.changba:id/ad_timer"]',
+          snapshotUrls: 'https://i.gkd.li/import/13373946',
+        },
+      ],
     },
   ],
 });
