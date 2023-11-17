f8252655703fce1a33ae8f096516d74a7e42230b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-17 21:09:55 +0800

    feat 自动识别解析链接

--- src/apps/com.wan.tools.ts
+++ src/apps/com.wan.tools.ts
@@ -14,5 +14,13 @@ export default defineAppConfig({
       rules: 'LinearLayout[childCount=2] > @[text="取消"] + [text="确认"]',
       snapshotUrls: 'https://i.gkd.li/import/13392027',
     },
+    {
+      enable: false,
+      key: 2,
+      name: '自动识别解析链接',
+      activityIds: 'com.one.downloadtools.ui.activity.HomeActivity',
+      rules: '@[text="识别解析"] -2 * > [text^="检测到你复制链接"]',
+      snapshotUrls: 'https://i.gkd.li/import/13392085',
+    },
   ],
 });
