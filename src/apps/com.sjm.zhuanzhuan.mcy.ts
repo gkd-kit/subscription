fecb39f4a46d328d28e3f4bb01c1f12fbaee3200
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-17 21:58:23 +0800

    feat 更新弹窗

--- src/apps/com.sjm.zhuanzhuan.mcy.ts
+++ src/apps/com.sjm.zhuanzhuan.mcy.ts
@@ -14,5 +14,21 @@ export default defineAppConfig({
       rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13391977',
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 1,
+          name: '更新弹窗',
+          matches:
+            '[id="com.sjm.zhuanzhuan.mcy:id/tv_new_version"] < LinearLayoutCompat +n [text="立即升级"]',
+          action: 'back',
+          snapshotUrls: 'https://i.gkd.li/import/13392514',
+        },
+      ],
+    },
   ],
 });
