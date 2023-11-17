6ba958ae76bc1a8022b2c86c7ef2740ddaeff009
bleach2012 <33366298+bleach2012@users.noreply.github.com>
2023-11-17 15:08:44 +0800

    雪球app首页广告及评论区广告

--- src/apps/com.xueqiu.android.ts
+++ src/apps/com.xueqiu.android.ts
@@ -11,5 +11,31 @@ export default defineAppConfig({
       rules: '[id="com.xueqiu.android:id/tv_skip"][text^="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/12776009',
     },
+    {
+        "key": 1,
+        "name": "首页广告",
+        "desc": "",
+        "rules": [
+          {
+            "activityIds": "com.xueqiu.android.mainnesting.view.MainNestingActivity",
+            "matches": "[id='com.xueqiu.android:id/ad_close']",
+            "exampleUrls": "https://m.gkd.li/33366298/d05d1eec-cda5-4d37-a494-8eef7b7871dd",
+            "snapshotUrls": "https://i.gkd.li/import/13388227"
+          }
+        ]
+      },
+    {
+      "key": 2,
+        "name": "评论区广告",
+        "desc": "",
+        "rules": [
+          {
+            "activityIds": "com.xueqiu.android.community.StatusDetailActivity",
+            "matches": "[id='com.xueqiu.android:id/ad_card_close']",
+            "exampleUrls": "https://m.gkd.li/33366298/b223cc74-fd4f-45a2-89ab-4a18ddffc01c",
+            "snapshotUrls": "https://i.gkd.li/import/13388361"
+          }
+        ]
+    }
   ],
 });
