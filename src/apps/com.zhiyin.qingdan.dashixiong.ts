244e3ffd6ef5c05b3e96cb7ff9dee77eeb4265d6
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-18 23:11:04 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.zhiyin.qingdan.dashixiong.ts
+++ src/apps/com.zhiyin.qingdan.dashixiong.ts
@@ -7,21 +7,14 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.zhiyin.qingdan.dashixiong.MainActivity',
-      matchLauncher: true,
       quickFind: true,
-      rules: [
-        {
-          key: 0,
-          matches: 'ImageView -n LinearLayout > [text^="跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/12843284',
-        },
-        {
-          key: 1,
-          matches:
-            '[id="com.zhiyin.qingdan.dashixiong:id/ksad_splash_circle_skip_view"] > [text="跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/12843283',
-        },
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12843284',
+        'https://i.gkd.li/import/12843283',
       ],
     },
     {
