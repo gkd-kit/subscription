d0bf22218a2214d85c8cb0824905d654e45bbf97
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-18 13:40:45 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.ecitic.bank.mobile.ts
+++ src/apps/com.ecitic.bank.mobile.ts
@@ -7,9 +7,12 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: ['com.ecitic.bank.mobile.ui.SplashActivity'],
-      rules: '[id="com.ecitic.bank.mobile:id/skip_adv_layout"]',
-      snapshotUrls: ['https://i.gkd.li/import/12701203'],
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@LinearLayout > [text="跳过"]',
+      snapshotUrls: 'https://i.gkd.li/import/12701203',
     },
     {
       key: 1,
