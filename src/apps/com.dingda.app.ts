1f3c10404f2cb9fdc0fd79a4fc89aa472d69a7a5
ran030929 <2357752497@qq.com>
2023-11-19 23:17:40 +0800

    叮嗒出行:开屏广告优化

--- src/apps/com.dingda.app.ts
+++ src/apps/com.dingda.app.ts
@@ -7,17 +7,19 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       quickFind: true,
       rules: [
         {
           matches: [
-            '[id="com.dingda.app:id/adContent"]',
             '[id!=null][id="com.byted.pangle:id/tt_splash_skip_btn"]', // 不能 quickFind
+            '[text="跳过 "&&name="android.widget.TextView"]',
           ],
-          snapshotUrls: 'https://i.gkd.li/import/13290327',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13290327',
+            'https://i.gkd.li/import/13403575',
+          ]
         },
       ],
     },
