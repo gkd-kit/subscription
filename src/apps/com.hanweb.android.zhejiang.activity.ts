46d5924dc4719c75fc477e5dd08a9aceef0e7aff
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-19 11:27:14 +0800

    feat 更新弹窗

--- src/apps/com.hanweb.android.zhejiang.activity.ts
+++ src/apps/com.hanweb.android.zhejiang.activity.ts
@@ -14,5 +14,15 @@ export default defineAppConfig({
       rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12913138',
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules:
+        '@[id="com.hanweb.android.zhejiang.activity:id/iv_cancel"] -2 * >2 [text="发现新版本"]',
+      snapshotUrls: 'https://i.gkd.li/import/13402048',
+    },
   ],
 });
