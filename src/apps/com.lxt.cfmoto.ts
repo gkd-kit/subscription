c87903c754a8063a493d142e2c76cc41f7f9bf64
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-19 10:39:25 +0800

    feat 弹窗广告

--- src/apps/com.lxt.cfmoto.ts
+++ src/apps/com.lxt.cfmoto.ts
@@ -14,5 +14,19 @@ export default defineAppConfig({
       rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13401968',
     },
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 1,
+          name: '首页弹窗广告',
+          activityIds: 'com.cfmoto.oilmoto.ui.MainCfActivity',
+          matches:
+            'RelativeLayout[childCount=2] > [id="com.lxt.cfmoto:id/image"] + [id="com.lxt.cfmoto:id/closeClick"]',
+          snapshotUrls: 'https://i.gkd.li/import/13401967',
+        },
+      ],
+    },
   ],
 });
