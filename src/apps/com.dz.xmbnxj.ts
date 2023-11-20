1976ddac510858395cb91604de69c5ebcee6c366
二刺螈 <i@songe.li>
2023-11-20 22:07:25 +0800

    Update com.dz.xmbnxj.ts

--- src/apps/com.dz.xmbnxj.ts
+++ src/apps/com.dz.xmbnxj.ts
@@ -1,20 +1,22 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  "id": "com.dz.xmbnxj",
-  "name": "小卖部逆袭记",
-  "groups": [
+  id: 'com.dz.xmbnxj',
+  name: '小卖部逆袭记',
+  groups: [
     {
-      "key": 1,
-      "name": "主页面广告",
-      "rules": [
+      key: 1,
+      name: '底部卡片广告',
+      rules: [
         {
-          "activityIds": "com.lynx.boot.LynxActivity",
-          "matches": "[id='com.dz.xmbnxj:id/mix_img_close']",
-          "exampleUrls": "https://m.gkd.li/99116490/40d3c3c9-9443-4c71-9a98-69efa38f8bfd",
-          "snapshotUrls": "https://i.gkd.li/import/13399232"
-        }
-      ]
-    }
-  ]
+          activityIds: 'com.lynx.boot.LynxActivity',
+          quickFind: true,
+          matches: '[id="com.dz.xmbnxj:id/mix_img_close"]',
+          exampleUrls:
+            'https://m.gkd.li/99116490/40d3c3c9-9443-4c71-9a98-69efa38f8bfd',
+          snapshotUrls: 'https://i.gkd.li/import/13399232',
+        },
+      ],
+    },
+  ],
 });
