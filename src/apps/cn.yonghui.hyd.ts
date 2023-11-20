75f931705fb64612f68038d37a7ffdeebbcf446b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-20 20:34:23 +0800

    feat 开屏广告

--- src/apps/cn.yonghui.hyd.ts
+++ src/apps/cn.yonghui.hyd.ts
@@ -5,6 +5,16 @@ export default defineAppConfig({
   name: '永辉生活',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text^="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13414432',
+    },
+    {
       key: 1,
       name: '红包弹窗',
       rules: [
