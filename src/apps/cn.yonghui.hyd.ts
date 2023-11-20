d4154ed56ece4180b6cd3e17a05ca33bad257986
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-20 20:30:43 +0800

    feat 红包弹窗

---
+++ src/apps/cn.yonghui.hyd.ts
@@ -0,0 +1,32 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.yonghui.hyd',
+  name: '永辉生活',
+  groups: [
+    {
+      key: 1,
+      name: '红包弹窗',
+      rules: [
+        {
+          key: 0,
+          name: '领券中心-红包弹窗',
+          activityIds:
+            'cn.yonghui.hyd.coupon.couponfactory.CouponFactoryActivity',
+          matches:
+            '[id="cn.yonghui.hyd:id/advance"] +n [id="cn.yonghui.hyd:id/close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13414401',
+        },
+        {
+          key: 1,
+          name: '好券即将过期弹窗',
+          activityIds:
+            'cn.yonghui.hyd.coupon.couponfactory.CouponFactoryActivity',
+          matches:
+            '[id="cn.yonghui.hyd:id/couponsWillGoneHeaderIv"] +n [id="cn.yonghui.hyd:id/close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13414444',
+        },
+      ],
+    },
+  ],
+});
