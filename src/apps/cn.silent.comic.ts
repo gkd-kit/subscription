db363e1db9ab843e866663ab734b4104c45c4826
lisonge <i@songe.li>
2023-11-20 21:57:59 +0800

    feat(免漫): 开屏广告

--- src/apps/cn.silent.comic.ts
+++ src/apps/cn.silent.comic.ts
@@ -5,31 +5,22 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '首页-开屏广告',
-      desc: '[ChangeMe]本规则由GKD网页端审查工具生成',
-      activityIds: ['io.dcloud.PandoraEntryActivity', 'io.dcloud.PandoraEntry'],
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          activityIds: 'io.dcloud.PandoraEntryActivity',
-          matches: '[id=\'cn.silent.comic:id/ksad_splash_circle_skip_view\']',
-          exampleUrls:
+          matches: '[id="cn.silent.comic:id/ksad_splash_circle_skip_view"]',
+          exampleUrls: [
             'https://m.gkd.li/83610194/f7c54df0-30d8-405a-b7eb-dd06c8db11cf',
-          snapshotUrls: 'https://i.gkd.li/import/13405442',
-        },
-        {
-          activityIds: 'io.dcloud.PandoraEntry',
-          matches: '[id=\'cn.silent.comic:id/ksad_splash_circle_skip_view\']',
-          exampleUrls:
             'https://m.gkd.li/83610194/94a8ecb5-1124-4090-a1ce-11340a4a76f2',
-          snapshotUrls: 'https://i.gkd.li/import/13405483',
-        },
-        {
-          activityIds: 'io.dcloud.PandoraEntryActivity',
-          matches: '[id=\'cn.silent.comic:id/ad_dcloud_main_skip\']',
-        },
-        {
-          activityIds: 'io.dcloud.PandoraEntry',
-          matches: '[id=\'cn.silent.comic:id/ad_dcloud_main_skip\']',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/import/13405442',
+            'https://i.gkd.li/import/13405483',
+          ],
         },
       ],
     },
