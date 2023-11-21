98b82fe27eb232a4237e9102b224a416d22848e7
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-21 18:55:56 +0800

    feat(当当):开屏广告

---
+++ src/apps/com.dangdang.buy2.ts
@@ -0,0 +1,27 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.dangdang.buy2',
+  name: '当当',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          name: '【跳过】',
+          matches: '[text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13424651',
+        },
+        {
+          name: '【穿山甲SDK】',
+          matches: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+        },
+      ],
+    },
+  ],
+});
