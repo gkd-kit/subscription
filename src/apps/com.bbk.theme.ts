d607cbe282481070e34ea964eb5caf4458f5b96b
adproqwq <2757917031@qq.com>
2023-11-22 19:28:08 +0800

    feat(i主题): 开屏广告

---
+++ src/apps/com.bbk.theme.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.bbk.theme',
+  name: 'i主题',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      activityIds: 'com.bbk.launcher2.Launcher',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: 'FrameLayout [text*="跳过"][text.length<=10]',
+          snapshotUrls: 'https://i.gkd.li/import/13438571',
+        },
+      ],
+    },
+  ],
+});
