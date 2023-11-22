35fcbf275ba647ae1f7a961f789706ab0966f751
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-11-22 13:16:53 +0800

    feat(淘票票):开屏广告

---
+++ src/apps/com.taobao.movie.android.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.taobao.movie.android',
+  name: '淘票票',
+  groups: [
+    {
+      enable: false,
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: 'https://i.gkd.li/import/13433880',
+    },
+  ],
+});
