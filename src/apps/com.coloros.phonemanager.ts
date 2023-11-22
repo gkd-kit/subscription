7c8daf7d77fa347d4abdb31c8399aebbb17f2a97
adproqwq <2757917031@qq.com>
2023-11-22 22:37:25 +0800

    feat(OPPO手机管家): 应用推荐

--- src/apps/com.coloros.phonemanager.ts
+++ src/apps/com.coloros.phonemanager.ts
@@ -5,7 +5,7 @@ export default defineAppConfig({
   name: 'OPPO手机管家',
   groups: [
     {
-      key: 3,
+      key: 0,
       name: '版本更新',
       quickFind: true,
       actionMaximum: 1,
@@ -15,6 +15,19 @@ export default defineAppConfig({
           matches:
             '@[text="取消"] < [id="com.coloros.phonemanager:id/buttonPanel"] > [text="更新"]',
           snapshotUrls: 'https://i.gkd.li/import/13194979',
+        },
+      ],
+    },
+    {
+      key: 1,
+      name: '应用推荐',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.coloros.phonemanager:id/ad_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13440085',
         },
       ],
     },
