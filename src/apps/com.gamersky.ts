842c91451735cdd418d97439d9b1c6cfbc86d65b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-23 22:51:30 +0800

    feat 卡片式广告

--- src/apps/com.gamersky.ts
+++ src/apps/com.gamersky.ts
@@ -17,5 +17,28 @@ export default defineAppConfig({
         'https://i.gkd.li/import/13038181',
       ],
     },
+    {
+      key: 1,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          name: '首页卡片广告',
+          activityIds: 'com.gamersky.main.activity.LibMainActivity',
+          quickFind: true,
+          matches:
+            '[id="com.gamersky:id/tap_video_ad_root"] > [id="com.gamersky:id/delete"]',
+          snapshotUrls: 'https://i.gkd.li/import/13451220',
+        },
+        {
+          key: 1,
+          name: '正文底部卡片广告',
+          activityIds:
+            'com.gamersky.common.activity.LibDetailContentDetailActivity',
+          matches: 'View[childCount=2] > [text="广告"] + View[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13451258',
+        },
+      ],
+    },
   ],
 });
