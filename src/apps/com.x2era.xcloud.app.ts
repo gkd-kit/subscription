c3dae2def2907610017e6a5aed817098f19ac944
二刺螈 <i@songe.li>
2023-11-24 00:11:32 +0800

    Update com.x2era.xcloud.app.ts

--- src/apps/com.x2era.xcloud.app.ts
+++ src/apps/com.x2era.xcloud.app.ts
@@ -4,6 +4,20 @@ export default defineAppConfig({
   name: 'Fa米家',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="com.x2era.xcloud.app:id/tv_jump"]',
+          snapshotUrls: 'https://i.gkd.li/import/13420730',
+        },
+      ],
+    },
+    {
       key: 1,
       name: 'Fa米家-升级提示',
       quickFind: true,
@@ -15,22 +29,6 @@ export default defineAppConfig({
           activityIds: 'com.x2era.xcloud.app.MainActivity',
           matches: '[id$=\'com.x2era.xcloud.app:id/iv_cancel\']',
           snapshotUrls: 'https://i.gkd.li/import/13420706',
-        },
-      ],
-    },
-    {
-      key: 2,
-      name: 'Fa米家-开屏广告',
-      desc: '跳过开屏广告',
-      quickFind: true,
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: [
-        {
-          activityIds: 'com.x2era.xcloud.app.ui.home.welcome.WelcomeActivity',
-          matches: '[id$=\'com.x2era.xcloud.app:id/tv_jump\']',
-          snapshotUrls: 'https://i.gkd.li/import/13420730',
         },
       ],
     },
