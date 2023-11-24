1ba4f06c902ee518e5273e1f6628d3f57db63b03
superHao <2217745768@qq.com>
2023-11-24 09:56:55 +0800

    fix: 医考帮

--- src/apps/com.yikaobang.yixue.ts
+++ src/apps/com.yikaobang.yixue.ts
@@ -11,6 +11,7 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
+      activityIds: 'com.psychiatrygarden.activity.HomePageNewActivity',
       rules: [
         {
           matches: '@ImageView[id="com.yikaobang.yixue:id/close"]',
