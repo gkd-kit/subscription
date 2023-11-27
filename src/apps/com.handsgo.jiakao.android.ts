845eb2f101d15a0488af8ca81633c8bae80ea5aa
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-27 22:05:47 +0800

    feat 弹窗广告

--- src/apps/com.handsgo.jiakao.android.ts
+++ src/apps/com.handsgo.jiakao.android.ts
@@ -26,5 +26,22 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '字节广告',
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches: [
+            '[desc^="logoad"] > [text="广告"]',
+            '[desc^="webview-close"] > View[clickable=true]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13476039',
+        },
+      ],
+    },
   ],
 });
