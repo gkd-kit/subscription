a37ac1653506ac4bcd0611a65e054991a6e07907
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-11-27 23:37:19 +0800

    feat 卡片式广告

--- src/apps/com.dewmobile.kuaiya.ts
+++ src/apps/com.dewmobile.kuaiya.ts
@@ -14,5 +14,19 @@ export default defineAppConfig({
       rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13477047',
     },
+    {
+      key: 1,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          name: '腾讯广告',
+          activityIds: 'com.dewmobile.kuaiya.act.MainActivity',
+          matches:
+            'ImageView - FrameLayout[childCount=1] > ImageView[childCount=0]',
+          snapshotUrls: 'https://i.gkd.li/import/13477048',
+        },
+      ],
+    },
   ],
 });
