ce4529f5a7c470c931110e81461618735ea1e291
lisonge <i@songe.li>
2023-11-28 20:16:37 +0800

    feat(财新): 开屏广告

--- src/apps/com.caing.news.ts
+++ src/apps/com.caing.news.ts
@@ -7,9 +7,11 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      rules: '@TextView[text*=\'跳过广告\']',
-      activityIds:
-        'com.caixin.android.component_launcher.LauncherContainerActivity',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13484755',
     },
   ],
