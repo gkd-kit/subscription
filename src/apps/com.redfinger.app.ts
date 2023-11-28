372f6b574fd4191321fe1ddeef90d922a3eb6558
lisonge <i@songe.li>
2023-11-28 20:14:13 +0800

    feat(红手指云手机): 开屏广告

--- src/apps/com.redfinger.app.ts
+++ src/apps/com.redfinger.app.ts
@@ -7,12 +7,12 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
+          quickFind: true,
           key: 0,
           matches: '[text*="跳过"][text.length<=10]',
           snapshotUrls: [
