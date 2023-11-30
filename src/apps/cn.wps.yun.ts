ee450fb6363d4f04c561959627c19589577c1431
lisonge <i@songe.li>
2023-11-30 21:52:48 +0800

    feat(金山文档): 列表广告

--- src/apps/cn.wps.yun.ts
+++ src/apps/cn.wps.yun.ts
@@ -6,13 +6,13 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '[ChangeMe]规则名称-2023-11-29 13:38:03',
-      desc: '[ChangeMe]本规则由GKD网页端审查工具生成',
+      name: '列表广告',
+      desc: '点击右侧x',
       rules: [
         {
           activityIds: 'cn.wps.yun.ui.IndexActivity',
           matches:
-            '[id="cn.wps.yun:id/rootGroup"] > TextView + [id="cn.wps.yun:id/moreIconRight"]',
+            '[id="cn.wps.yun:id/rootGroup"][childCount=5] >  [id="cn.wps.yun:id/moreIconRight"]',
           exampleUrls:
             'https://m.gkd.li/47232102/bc97cb52-aad0-4114-a548-5831edbe342d',
           snapshotUrls: 'https://i.gkd.li/import/13495062',
