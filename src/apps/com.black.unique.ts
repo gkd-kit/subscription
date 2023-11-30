335e077d9e1a01873a812ef857528d330f2998d0
lisonge <i@songe.li>
2023-11-30 21:34:02 +0800

    feat(全球购骑士特权): 开屏广告

--- src/apps/com.black.unique.ts
+++ src/apps/com.black.unique.ts
@@ -8,9 +8,12 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          matches: '@View[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
+          matches: '[id="com.byted.pangle.m:id/tt_splash_skip_btn"]',
           snapshotUrls: 'https://i.gkd.li/import/13499535',
         },
       ],
