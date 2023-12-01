f3d35f3e274a4afb2fcd25dd2ee16898f1f7d899
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-01 10:40:01 +0800

    feat 信息流广告（优化规则）

--- src/apps/com.ss.android.auto.ts
+++ src/apps/com.ss.android.auto.ts
@@ -12,22 +12,30 @@ export default defineAppConfig({
       snapshotUrls: 'https://i.gkd.li/import/12605327',
     },
     {
+      enable: false,
       key: 1,
-      name: '首页推荐卡片广告',
+      name: '信息流广告',
       activityIds: 'com.ss.android.auto.activity.SplashActivity',
       rules: [
         {
           key: 0,
+          name: '点击x按钮',
           matches:
             '[text="广告"] < LinearLayout + @FrameLayout[clickable=true][id!=null] > TextView[text.length=1][id=null][clickable=false]',
           // 貌似快照存在延迟导致屏幕与节点不对应
           snapshotUrls: 'https://i.gkd.li/import/12660816',
         },
         {
-          preKeys: 1,
-          matches:
-            '[text$="精准屏蔽"] + [text="不感兴趣"][id!=null][clickable=true]',
+          preKeys: 0,
+          key: 1,
+          matches: 'RelativeLayout > [text="不感兴趣"][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/import/12711589',
+        },
+        {
+          preKeys: 1,
+          key: 0,
+          matches: '@ViewGroup > LinearLayout > [text="不感兴趣"]',
+          snapshotUrls: 'https://i.gkd.li/import/13519924',
         },
       ],
     },
