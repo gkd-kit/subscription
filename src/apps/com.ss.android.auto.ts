215ca32afdc5bf7391be40fcc2bc593b603cb610
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-02 17:45:27 +0800

    feat(懂车帝):规则优化

--- src/apps/com.ss.android.auto.ts
+++ src/apps/com.ss.android.auto.ts
@@ -7,24 +7,31 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.ss.android.auto.activity.SplashActivity',
-      rules: '[desc="跳过广告"][id!=null]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@FrameLayout[clickable=true] > [text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12605327',
     },
     {
       key: 1,
       name: '首页推荐卡片广告',
+      desc: '需二次点击关闭原因',
+      quickFind: true,
       activityIds: 'com.ss.android.auto.activity.SplashActivity',
       rules: [
         {
           key: 0,
+          name: '点击【x】',
           matches:
-            '[text="广告"] < LinearLayout + @FrameLayout[clickable=true][id!=null] > TextView[text.length=1][id=null][clickable=false]',
+            'TextView[text.length=1][id=null][clickable=false] < @FrameLayout[clickable=true][id!=null] - LinearLayout > [text="广告"]',
           // 貌似快照存在延迟导致屏幕与节点不对应
           snapshotUrls: 'https://i.gkd.li/import/12660816',
         },
         {
           preKeys: 1,
+          name: '点击【不感兴趣】',
           matches:
             '[text$="精准屏蔽"] + [text="不感兴趣"][id!=null][clickable=true]',
           snapshotUrls: 'https://i.gkd.li/import/12711589',
@@ -34,6 +41,7 @@ export default defineAppConfig({
     {
       key: 2,
       name: '升级弹窗',
+      quickFind: true,
       activityIds: 'com.ss.android.auto.activity.SplashActivity',
       rules:
         '@TextView[text="以后再说"] - FrameLayout >2 TextView[text$="升级"]',
@@ -42,6 +50,7 @@ export default defineAppConfig({
     {
       key: 3,
       name: '首页直播推荐窗口',
+      quickFind: true,
       activityIds: 'com.ss.android.auto.activity.SplashActivity',
       rules:
         '@ImageView[clickable=true][id!=null] - RelativeLayout >4 [id="com.ss.android.auto:id/at1"][text="特价抢购"]',
@@ -50,6 +59,7 @@ export default defineAppConfig({
     {
       key: 4,
       name: '文章底部广告',
+      quickFind: true,
       activityIds:
         'com.ss.android.article.base.feature.detail2.view.NewDetailActivity',
       rules:
@@ -59,6 +69,7 @@ export default defineAppConfig({
     {
       key: 5,
       name: '评论区广告',
+      quickFind: true,
       activityIds: [
         'com.ss.android.article.base.feature.detail2.view.NewDetailActivity',
         'com.ss.android.auto.ugc.video.activity.UgcLongPostActivity',
@@ -75,6 +86,7 @@ export default defineAppConfig({
     {
       key: 6,
       name: '请求推送通知弹窗',
+      quickFind: true,
       activityIds: 'com.ss.android.auto.activity.SplashActivity',
       rules:
         '@TextView[clickable=true && text.length =1] + [text ^="打开推送通知"]',
