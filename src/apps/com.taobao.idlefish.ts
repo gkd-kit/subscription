ed9a4b4af2a948920a9af447ebb8a19049a54a18
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 15:57:16 +0800

    feat 请求开启通知权限弹窗

--- src/apps/com.taobao.idlefish.ts
+++ src/apps/com.taobao.idlefish.ts
@@ -16,5 +16,13 @@ export default defineAppConfig({
         'https://i.gkd.li/import/13049015',
       ],
     },
+    {
+      key: 1,
+      name: '请求开启通知权限弹窗',
+      activityIds:
+        'com.idlefish.flutterbridge.flutterboost.boost.FishFlutterBoostTransparencyActivity',
+      rules: '[desc^="开启系统通知"] > ImageView[clickable=true][desc=null]',
+      snapshotUrls: 'https://i.gkd.li/import/13538351',
+    },
   ],
 });
