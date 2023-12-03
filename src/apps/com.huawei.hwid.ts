3724c314a79ed0da766623d50919557c8aac1670
二刺螈 <i@songe.li>
2023-12-03 17:43:17 +0800

    feat(HMS Core): 开屏广告

--- src/apps/com.huawei.hwid.ts
+++ src/apps/com.huawei.hwid.ts
@@ -5,6 +5,20 @@ export default defineAppConfig({
   name: 'HMS Core',
   groups: [
     {
+      key: -1,
+      name: '开屏广告',
+      resetMatch: 'app',
+      matchTime: 10000,
+      actionMaximum: 1,
+      quickFind: true,
+      rules: [
+        {
+          matches: '[id="com.huawei.hwid:id/hiad_skip_text"]',
+          snapshotUrls: ['https://i.gkd.li/import/13520462'],
+        },
+      ],
+    },
+    {
       // 我不知道这个广告从哪里来的，它出乎意料地跳出并覆盖在Gspace上面
       key: 0,
       name: '页面中间跳出广告',
@@ -15,17 +29,6 @@ export default defineAppConfig({
         {
           matches: '[id="com.huawei.hwid:id/interstitial_close"]',
           snapshotUrls: ['https://i.gkd.li/import/12709068'],
-        },
-      ],
-    },
-    {
-      key: 1,
-      name: '开屏广告',
-      desc: '',
-      rules: [
-        {
-          matches: '[id="com.huawei.hwid:id/hiad_skip_text"]',
-          snapshotUrls: ['https://i.gkd.li/import/13520462'],
         },
       ],
     },
