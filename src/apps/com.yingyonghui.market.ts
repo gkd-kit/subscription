a59ca45eb15f1760eeb94bc41fdd4c01d08c51e5
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-03 15:42:56 +0800

    feat 弹窗广告

--- src/apps/com.yingyonghui.market.ts
+++ src/apps/com.yingyonghui.market.ts
@@ -24,5 +24,21 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '快手广告',
+          activityIds: 'com.yingyonghui.market.ui.MainActivity',
+          matches: [
+            'ViewGroup[childCount=2] > ImageView + [text="广告"]',
+            'ViewGroup[childCount=3] > TextView + TextView + ImageView',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13538316',
+        },
+      ],
+    },
   ],
 });
