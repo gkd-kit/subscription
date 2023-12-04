4331fa6a3172c86453e942b5dd78ed7b51fe54ea
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-04 21:20:38 +0800

    Revert "feat 弹窗广告"
    This reverts commit 99940583a6672d53c2a16215bee860ff22245a7b.

--- src/apps/com.huawei.health.ts
+++ src/apps/com.huawei.health.ts
@@ -35,18 +35,5 @@ export default defineAppConfig({
         },
       ],
     },
-    {
-      key: 2,
-      name: '弹窗广告',
-      rules: [
-        {
-          key: 0,
-          activityIds: 'com.huawei.watchface.api.WebViewActivity',
-          matches:
-            'View[childCount=2] > View + View[childCount=1] > Image[childCount=0]',
-          snapshotUrls: 'https://i.gkd.li/import/13546292',
-        },
-      ],
-    },
   ],
 });
