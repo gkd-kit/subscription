e04990fbec8e75ae69901a68279a0c93d93743b4
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-04 20:53:13 +0800

    feat 悬浮广告

--- src/apps/cn.ccspeed.ts
+++ src/apps/cn.ccspeed.ts
@@ -14,5 +14,19 @@ export default defineAppConfig({
       rules: '[text^="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13334790',
     },
+    {
+      key: 1,
+      name: '悬浮广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'cn.ccspeed.app.MainActivity',
+          quickFind: true,
+          matches:
+            '[id="cn.ccspeed:id/float_layout"] > @ImageView + [id="cn.ccspeed:id/iv_float"]',
+          snapshotUrls: 'https://i.gkd.li/import/13546075',
+        },
+      ],
+    },
   ],
 });
