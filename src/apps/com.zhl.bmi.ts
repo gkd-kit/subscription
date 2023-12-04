5628a3c32c03ef7bdd79a9081fb712b40da7f631
adproqwq <2757917031@qq.com>
2023-12-04 14:04:50 +0800

    fix(乐看视频): 广告弹窗补充规则

--- src/apps/com.zhl.bmi.ts
+++ src/apps/com.zhl.bmi.ts
@@ -10,11 +10,14 @@ export default defineAppConfig({
       desc: '点击右上角x',
       activityIds: 'com.zhl.bmi.MainActivity',
       quickFind: true,
-      rules:
+      rules: [
         '@ImageView < FrameLayout[childCount=1] <3 FrameLayout <2 FrameLayout < FrameLayout < [id="android:id/content"]',
+        '@ImageView < FrameLayout[childCount=1] <2 FrameLayout < FrameLayout < FrameLayout < [id="android:id/content"]'
+      ],
       snapshotUrls: [
         'https://i.gkd.li/import/13255787',
         'https://i.gkd.li/import/13255786',
+        'https://i.gkd.li/import/13542343', //第二条规则
       ],
     },
   ],
