61e9669c8634ef5fd335029fb0e6f7eb7243d6fd
lisonge <i@songe.li>
2023-12-05 21:13:45 +0800

    feat(抖音极速版): 青少年弹窗

--- src/apps/com.ss.android.ugc.aweme.lite.ts
+++ src/apps/com.ss.android.ugc.aweme.lite.ts
@@ -10,13 +10,21 @@ export default defineAppConfig({
       desc: '点击【我知道了】（旧版本）【关闭】（新版本）',
       quickFind: true,
       activityIds: 'com.ss.android.ugc.aweme.main.MainActivity',
+      actionMaximum: 1,
       rules: [
-        'Button[desc="开启青少年模式"] + Button[text="我知道了"]', //旧版本
-        'Button[text="开启青少年模式"] + LinearLayout > Button[text="关闭"]', //新版本
-      ],
-      snapshotUrls: [
-        'https://i.gkd.li/import/13111607', //旧版本
-        'https://i.gkd.li/import/13542867', //新版本
+        {
+          key: 0,
+          matches: 'Button[desc="开启青少年模式"] + Button[text="我知道了"]',
+          snapshotUrls: 'https://i.gkd.li/import/13111607',
+        },
+        {
+          actionMaximumKey: 0,
+          matches:
+            'Button[text="开启青少年模式"] + LinearLayout > Button[text="关闭"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13542867', //新版本
+          ],
+        },
       ],
     },
   ],
