b6bc0f2e835409dbef8fd80177a935037ecc0f41
lisonge <i@songe.li>
2023-12-05 21:08:52 +0800

    feat(360儿童卫士): 开屏广告

--- src/apps/com.qihoo360.antilostwatch.ts
+++ src/apps/com.qihoo360.antilostwatch.ts
@@ -7,14 +7,10 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      matchDelay: 300,
-      matchTime: 5000,
+      matchTime: 10000,
+      quickFind: true,
       actionMaximum: 1,
-      activityIds: [
-        'com.qihoo.kids.watch.im.boot.MainActivity',
-        'com.qihoo.kids.watch.im.home.HomeActivity',
-      ],
-      rules: '[name="android.widget.TextView"&&text="跳过"]',
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: [
         'https://i.gkd.li/import/13538012',
         'https://i.gkd.li/import/13538800',
