a7a28000d72aa1618426b2f9c69fa8d7b4bdb200
xcz1997 <cappuxcz@outlook.com>
2023-12-06 09:43:29 +0800

    Update com.samsung.android.packageinstaller.ts
    同时取消“是否打开反恶意”协议

--- src/apps/com.samsung.android.packageinstaller.ts
+++ src/apps/com.samsung.android.packageinstaller.ts
@@ -1,24 +1,40 @@
 import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
-  id: 'com.samsung.android.packageinstaller',
-  name: '软件包安装程序',
-  groups: [
-    {
-      enable: false,
-      key: 1,
-      name: '请求开启反恶意软件提示弹窗',
-      activityIds: [
-        'pl.solidexplorer.SolidExplorer',
-        'com.android.packageinstaller.activity.InstallerActivity',
-      ],
-      rules: [
+    "id": "com.samsung.android.packageinstaller",
+    "name": "软件包安装程序",
+    "groups": [
         {
-          key: 0,
-          matches: ['[text^="是否打开反恶意软件"]', '[text="取消"]'],
-          snapshotUrls: 'https://i.gkd.li/import/13233686',
-        },
-      ],
-    },
-  ],
+            "name": "跳过安全使用协议",
+            "key": 2,
+            "desc": "自动点击略过并关闭反恶意软件协议",
+            "enable": true,
+            "activityIds": [],
+            "rules": [
+                {
+                    "key": 1,
+                    "preKeys": [],
+                    "matches": [
+                        "[text *=\"略过\"]"
+                    ],
+                    "excludeMatches": [],
+                    "snapshotUrls": [
+                        "https://i.gkd.li/import/13233686"
+                    ]
+                },
+                {
+                    "key": 0,
+                    "preKeys": [],
+                    "matches": [
+                        "[text^=\"是否打开反恶意软件\"]",
+                        "[text=\"取消\"]"
+                    ],
+                    "excludeMatches": [],
+                    "snapshotUrls": [
+                        "https://i.gkd.li/import/13233686"
+                    ]
+                }
+            ]
+        }
+    ]
 });
