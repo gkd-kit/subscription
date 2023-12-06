83d728223bd6d0f25a9f7a249ca7d2f3b53f50a1
太上舞殇 <58279234+taishangwushang@users.noreply.github.com>
2023-12-06 13:14:48 +0800

    Create com.mxz.bbs.ts
    墨轩竹开屏广告

---
+++ src/apps/com.mxz.bbs.ts
@@ -0,0 +1,29 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+    "id":"com.mxz.bbs",
+    "name":"墨轩竹",
+    "groups":[
+        {
+            "name":"[ChangeMe]规则名称-2023-12-04 17:26:53",
+            "key":0,
+            "desc":"[ChangeMe]本规则由GKD网页端审查工具生成",
+            "rules":[
+                {
+                    "activityIds":[
+                        "io.dcloud.PandoraEntry"
+                    ],
+                    "matches":[
+                        "TextView[text*=\"跳过\"]"
+                    ],
+                    "snapshotUrls":[
+                        "https://i.gkd.li/import/13544162"
+                    ],
+                    "exampleUrls":[
+                        "https://m.gkd.li/58279234/f599a92e-0a65-40d7-9a5d-03815081c22b"
+                    ]
+                }
+            ]
+        },
+    ]
+});
