b4879654fee09d57aa36973ac829274fe418d5d9
太上舞殇 <58279234+taishangwushang@users.noreply.github.com>
2023-12-06 13:19:39 +0800

    Create com.ncili.jie.ts
    BT磁力下载器开屏广告

---
+++ src/apps/com.ncili.jie.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+    "id":"com.ncili.jie",
+    "name":"BT磁力下载器",
+    "groups":[
+        {
+            "name":"[ChangeMe]规则名称-2023-12-04 17:40:51",
+            "key":0,
+            "desc":"[ChangeMe]本规则由GKD网页端审查工具生成",
+            "rules":[
+                {
+                    "matches":[
+                        "[id=\"com.ncili.jie:id/skip_view2\"]"
+                    ],
+                    "snapshotUrls":[
+                        "https://i.gkd.li/import/13544299"
+                    ],
+                    "exampleUrls":[
+                        "https://m.gkd.li/58279234/fdda12a5-3a0a-450e-947e-73b48c1667da"
+                    ]
+                }
+            ]
+        },
+    ]
+});
