8dc5f455ad1c811c6d37b5c427f4c751b7d14678
xiaowonet <33050133+xiaowonet@users.noreply.github.com>
2023-12-07 18:18:40 +0800

    feat(淘宝): 将小组件添加到手机桌面

--- src/apps/com.taobao.taobao.ts
+++ src/apps/com.taobao.taobao.ts
@@ -150,5 +150,19 @@ export default defineAppConfig({
       rules: '@[text="关闭"] < * <3 * < * + * >3 [text$="小额免密支付"]',
       snapshotUrls: 'https://i.gkd.li/import/13438414',
     },
+    {
+      enable: false,
+      key: 14,
+      name: '将小组件添加到手机桌面',
+      desc: '点击取消',
+      activityIds: 'com.alibaba.triver.container.TriverMainActivity',
+      rules: [
+        {
+          action: 'clickCenter',
+          matches: 'View[text="立即添加"] + View[text="取消"]',
+          snapshotUrls: 'https://i.gkd.li/import/13598578',
+        },
+      ],
+    },
   ],
 });
