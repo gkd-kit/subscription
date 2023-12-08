f59d0b50b866b5b889728b2f15a67d2ff6283f9b
jackTpy <824945044@qq.com>
2023-12-08 09:42:06 +0800

    feat(Pods King): 开屏广告

--- src/apps/com.msnothing.airpodsking.ts
+++ src/apps/com.msnothing.airpodsking.ts
@@ -12,5 +12,18 @@ export default defineAppConfig({
       rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
       snapshotUrls: 'https://i.gkd.li/import/12908449',
     },
+    {
+      key: 1,
+      name: '开屏广告',
+      rules: [
+        {
+          activityIds: 'com.msnothing.ad.splash.HomeSplashActivity',
+          matches: '[text="跳过"]',
+          exampleUrls:
+            'https://m.gkd.li/48015717/1e2e3d8f-935f-431a-9bbd-27f217ad6912',
+          snapshotUrls: 'https://i.gkd.li/import/13607847',
+        },
+      ],
+    },
   ],
 });
