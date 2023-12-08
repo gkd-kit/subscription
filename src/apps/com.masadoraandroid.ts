77c12543a128c54b9ec113030587c93d0912001b
lisonge <i@songe.li>
2023-12-08 22:00:48 +0800

    feat(魔法集市 Global): 开屏广告

--- src/apps/com.masadoraandroid.ts
+++ src/apps/com.masadoraandroid.ts
@@ -2,12 +2,15 @@ import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
   id: 'com.masadoraandroid',
-  name: 'masadora',
+  name: '魔法集市 Global',
   groups: [
     {
       key: 1,
       name: '开屏广告',
-      activityIds: ['com.masadoraandroid.ui.start.StartActivity'],
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      matchTime: 10000,
       rules: [
         {
           matches: '[id="com.masadoraandroid:id/skip"]',
