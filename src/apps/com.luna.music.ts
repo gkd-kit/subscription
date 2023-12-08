0b71b0f322bf480f51f6274ed3dad6c34c7e7353
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-08 19:56:05 +0800

    feat(汽水音乐):VIP弹窗、看广告视频拿VIP

--- src/apps/com.luna.music.ts
+++ src/apps/com.luna.music.ts
@@ -17,18 +17,50 @@ export default defineAppConfig({
     {
       key: 2,
       name: 'VIP弹窗',
-      desc: '直接关闭所有该样式弹窗',
+      desc: '直接关闭所有底部半屏弹窗',
       quickFind: true,
       actionMaximum: 1,
       resetMatch: 'app',
+      activityIds: 'com.luna.biz.main.main.MainActivity',
       rules: [
         {
           action: 'back',
-          matches: '[id="com.luna.music:id/bullet_container"]',
+          matches: '[id="com.luna.music:id/design_bottom_sheet"]', 
           snapshotUrls: [
             'https://i.gkd.li/import/13533795',
             'https://i.gkd.li/import/13533797',
+            'https://i.gkd.li/import/13613296', // 旧规则matches: '[id="com.luna.music:id/bullet_container"]',在此误触
+            'https://i.gkd.li/import/13613498', // 旧规则matches: '[id="com.luna.music:id/bullet_container"]',在此误触
           ],
+        },
+      ],
+    },
+    {
+      enable: false,
+      key: 7,
+      name: '看广告视频拿VIP',
+      desc: '30s广告后点击【跳过】',
+      activityIds: [
+        'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+        'com.luna.biz.ad.AdActivity',
+      ],
+      rules: [
+        {
+          key: 0,
+          name: '等待30s点击【跳过】',
+          actionDelay: 30000, 
+          matches: 'WebView > WebView > View TextView[text="| 跳过"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13611006',
+            'https://i.gkd.li/import/13613085',
+          ],
+        },
+        {
+          preKeys: 0,
+          name: '点击【坚持退出】',
+          quickFind: true,
+          matches: '[id="com.byted.pangle:id/tt_negtive"][text="坚持退出"]',
+          snapshotUrls: 'https://i.gkd.li/import/13613184',
         },
       ],
     },
