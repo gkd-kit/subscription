cf3d3bd1208f23b09e9d6b9318e354d2505fed26
jacktpy <48015717+jacktpy@users.noreply.github.com>
2023-12-08 11:16:34 +0800

    Update com.msnothing.airpodsking.ts

--- src/apps/com.msnothing.airpodsking.ts
+++ src/apps/com.msnothing.airpodsking.ts
@@ -9,21 +9,14 @@ export default defineAppConfig({
       name: '开屏广告',
       activityIds: 'com.msnothing.ad.splash.HomeSplashActivity',
       matchLauncher: true,
-      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
-      snapshotUrls: 'https://i.gkd.li/import/12908449',
-    },
-    {
-      key: 1,
-      name: '开屏广告',
       rules: [
-        {
-          activityIds: 'com.msnothing.ad.splash.HomeSplashActivity',
-          matches: '[text="跳过"]',
-          exampleUrls:
-            'https://m.gkd.li/48015717/1e2e3d8f-935f-431a-9bbd-27f217ad6912',
-          snapshotUrls: 'https://i.gkd.li/import/13607847',
-        },
+        '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+        '[text="跳过"]'
       ],
-    },
+      snapshotUrls: [
+        'https://i.gkd.li/import/12908449',
+        'https://i.gkd.li/import/13607847'
+      ],
+    }
   ],
 });
