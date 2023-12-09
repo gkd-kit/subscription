77e5b0315f4c8d606100b83e5cb8c8e24291e542
heyue <1982389545@qq.com>
2023-12-09 23:33:15 +0800

    feat(微信读书): 已观看15秒广告自动关闭

--- src/apps/com.tencent.weread.ts
+++ src/apps/com.tencent.weread.ts
@@ -37,5 +37,37 @@ export default defineAppConfig({
       rules: '[text="设置青少年模式"] - LinearLayout >2 [text="我知道了"]',
       snapshotUrls: 'https://i.gkd.li/import/13233668',
     },
+    {
+      name: '观看15秒广告-X',
+      key: 3,
+      desc: '微信读书15秒后自动关闭广告，左上角为一个X',
+      quickFind: false,
+      rules: [
+        {
+          actionCd: 1000,
+          activityIds: ['com.qq.e.tg.RewardvideoPortraitADActivity'],
+          matches: [
+            'Image[name="android.widget.Image"][text="cross"] < @View[clickable=true] - TextView < View < View + TextView[text="已观看视频15秒，可获得奖励"]',
+          ],
+          snapshotUrls: ['https://i.gkd.li/import/13623891'],
+        },
+      ],
+    },
+    {
+      name: '观看15秒广告-跳过广告',
+      key: 4,
+      desc: '微信读书15秒后自动关闭广告，左上角按钮为“跳过广告”',
+      quickFind: false,
+      rules: [
+        {
+          actionCd: 1000,
+          activityIds: ['com.qq.e.tg.RewardvideoPortraitADActivity'],
+          matches: [
+            '@TextView[id="com.tencent.weread:style/AndroidThemeColorAccentYellow"] + TextView[text="已观看视频15秒，可获得奖励"]',
+          ],
+          snapshotUrls: ['https://i.gkd.li/import/13624414'],
+        },
+      ],
+    },
   ],
 });
