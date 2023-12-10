da869b78b3fd04f08ed83f5107a2a7f20305a304
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-10 21:29:37 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.lolaage.tbulu.tools.ts
+++ src/apps/com.lolaage.tbulu.tools.ts
@@ -7,11 +7,15 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.lolaage.tbulu.tools.ui.activity.WelcomeActivity',
-      matchLauncher: true,
       quickFind: true,
-      rules: '[id="com.lolaage.tbulu.tools:id/skipPart"]',
-      snapshotUrls: 'https://i.gkd.li/import/12882538',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text*="跳过"][text.length<=10]',
+      snapshotUrls: [
+        'https://i.gkd.li/import/12882538',
+        'https://i.gkd.li/import/13627860',
+      ],
     },
     {
       key: 1,
