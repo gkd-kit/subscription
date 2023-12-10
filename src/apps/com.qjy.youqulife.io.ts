0d8ab7f252702474c73e65262aca2cc65ff8649e
adproqwq <2757917031@qq.com>
2023-12-10 23:11:12 +0800

    feat(MINO): 弹窗广告新增规则

--- src/apps/com.qjy.youqulife.io.ts
+++ src/apps/com.qjy.youqulife.io.ts
@@ -2,7 +2,7 @@ import { defineAppConfig } from '../types';
 
 export default defineAppConfig({
   id: 'com.qjy.youqulife.io',
-  name: 'MINO 4K',
+  name: 'MINO',
   groups: [
     {
       key: 1,
@@ -16,6 +16,13 @@ export default defineAppConfig({
           matches:
             '[text="广告"] <2 ViewGroup -n ViewGroup > ViewGroup > [text="跳过"]',
           snapshotUrls: 'https://i.gkd.li/import/13378653',
+        },
+        {
+          key: 2,
+          name: '腾讯广告',
+          activityIds: 'com.qjy.youqulife.io.MainActivity',
+          matches: 'FrameLayout[index=1]',
+          snapshotUrls: 'https://i.gkd.li/import/13628208',
         },
       ],
     },
