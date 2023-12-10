c20ac5043a37bde47281d8b9d1ade9ad31ae8ec7
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-10 21:47:24 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.youdao.dict.ts
+++ src/apps/com.youdao.dict.ts
@@ -7,8 +7,11 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.youdao.dict.activity.DictSplashActivity',
-      rules: '[id="com.youdao.dict:id/skin_text"]',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[text$="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12668545',
     },
     {
