650ed762d2e6e2419f44424fd7b1db42531b26e4
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-10 21:44:45 +0800

    feat 弹窗广告

--- src/apps/cn.damai.ts
+++ src/apps/cn.damai.ts
@@ -13,5 +13,18 @@ export default defineAppConfig({
       rules: '[id="cn.damai:id/homepage_advert_pb"]',
       snapshotUrls: 'https://i.gkd.li/import/12472623',
     },
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'cn.damai.homepage.MainActivity',
+          matches:
+            '[id="cn.damai:id/homepage_popup_window_image"] + [id="cn.damai:id/homepage_popup_window_close_btn"]',
+          snapshotUrls: 'https://i.gkd.li/import/13627900',
+        },
+      ],
+    },
   ],
 });
