1a57c5dd7562a7713fc094c77440217dc8172d73
lisonge <i@songe.li>
2023-12-11 21:09:45 +0800

    feat(电影猎手): 开屏广告

--- src/apps/com.qiqi.hhvideo.ts
+++ src/apps/com.qiqi.hhvideo.ts
@@ -7,10 +7,12 @@ export default defineAppConfig({
     {
       key: 1,
       name: '开屏广告',
-      desc: '开屏广告',
+      actionMaximum: 1,
+      quickFind: true,
+      matchTime: 10000,
+      resetMatch: 'app',
       rules: [
         {
-          activityIds: 'com.qiqi.hhvideo.ui.SplashActivity2',
           matches: '[id="com.qiqi.hhvideo:id/skipad"]',
           exampleUrls:
             'https://m.gkd.li/48015717/236526b6-691f-4023-a3ac-64b4409ee759',
