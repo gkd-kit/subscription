f2d12c2dc599c28a596da63afe40101793bd3c78
Lucio-0408 <1026680384@qq.com>
2023-12-12 01:18:41 +0800

    fix(魔法集市 Global):开屏广告

--- src/apps/com.masadoraandroid.ts
+++ src/apps/com.masadoraandroid.ts
@@ -13,8 +13,8 @@ export default defineAppConfig({
       matchTime: 10000,
       rules: [
         {
-          matches: '[id="com.masadoraandroid:id/skip"]',
-          snapshotUrls: 'https://i.gkd.li/import/13578688',
+          matches: '[id="com.masadoraandroid:id/skip"][text="跳过 5"]',
+          snapshotUrls: 'https://i.gkd.li/import/13638359',
         },
       ],
     },
