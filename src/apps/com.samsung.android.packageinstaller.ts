f217fbea1b47cbcbaf81af6d42ef3a3c3ae0b06c
xcz1997 <cappuxcz@outlook.com>
2023-12-11 21:30:12 +0800

    Update com.samsung.android.packageinstaller.ts
    移除空数组字段

--- src/apps/com.samsung.android.packageinstaller.ts
+++ src/apps/com.samsung.android.packageinstaller.ts
@@ -9,20 +9,15 @@ export default defineAppConfig({
       key: 2,
       desc: '自动点击略过并关闭反恶意软件协议',
       enable: true,
-      activityIds: [],
       rules: [
         {
           key: 1,
-          preKeys: [],
           matches: ['[text *="略过"]'],
-          excludeMatches: [],
           snapshotUrls: ['https://i.gkd.li/import/13255641'],
         },
         {
           key: 0,
-          preKeys: [],
           matches: ['[text^="是否打开反恶意软件"]', '[text="取消"]'],
-          excludeMatches: [],
           snapshotUrls: ['https://i.gkd.li/import/13233686'],
         },
       ],
