fe68d55633b9989f04cd6f6e76d186118845eca4
lisonge <i@songe.li>
2023-12-11 21:07:41 +0800

    feat(天气通): 开屏广告

--- src/apps/sina.mobile.tianqitong.ts
+++ src/apps/sina.mobile.tianqitong.ts
@@ -10,7 +10,7 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: 'LinearLayout[childCount=2]',
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13628049',
     },
   ],
