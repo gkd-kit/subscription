a3753f7ae74c2a2e4d2207ab94493968a0f60c77
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-13 00:25:03 +0800

    Update com.smarlife.qingtong.ts

--- src/apps/com.smarlife.qingtong.ts
+++ src/apps/com.smarlife.qingtong.ts
@@ -12,6 +12,7 @@ export default defineAppConfig({
       resetMatch: 'app',
       rules: [
         {
+          key: 0,
           quickFind: true,
           matches: '[text*="跳过"][text.length<=10]',
           snapshotUrls: 'https://i.gkd.li/import/13636044',
