d6ce6b565404017849e8d95518ea8498e8e9a9ca
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2023-12-13 18:39:39 +0800

    Update com.example.raylink_flutter.ts

--- src/apps/com.example.raylink_flutter.ts
+++ src/apps/com.example.raylink_flutter.ts
@@ -11,7 +11,7 @@ export default defineAppConfig({
       actionMaximum: 1,
       resetMatch: 'app',
       activityIds: 'com.example.raylink_flutter.MainActivity', 
-      rules: ''[desc="立即升级"] + [desc="以后再说"]',
+      rules: '[desc="立即升级"] + [desc="以后再说"]',
       snapshotUrls: 'https://i.gkd.li/import/13659530',
     },
   ],
