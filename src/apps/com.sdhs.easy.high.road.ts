a7e31ec03e742482ab0d8e5316a36f744d0511b2
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-14 16:03:59 +0800

    feat 卡片式广告

--- src/apps/com.sdhs.easy.high.road.ts
+++ src/apps/com.sdhs.easy.high.road.ts
@@ -13,5 +13,21 @@ export default defineAppConfig({
       rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
       snapshotUrls: 'https://i.gkd.li/import/13670339',
     },
+    {
+      key: 1,
+      name: '卡片式广告',
+      rules: [
+        {
+          key: 0,
+          name: '腾讯广告',
+          activityIds: 'com.sdhs.easy.main.activity.MainActivity',
+          matches: [
+            '[desc^="logoad"]',
+            '[desc^="dislike"] > View[clickable=true]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/import/13670340',
+        },
+      ],
+    },
   ],
 });
