61b7edb523ed53128f49cdf99fd050f9de5f7496
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-14 16:10:11 +0800

    feat 弹窗广告

--- src/apps/com.showstartfans.activity.ts
+++ src/apps/com.showstartfans.activity.ts
@@ -14,5 +14,19 @@ export default defineAppConfig({
       rules: '[text$="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12894279',
     },
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          activityIds:
+            'com.showstartfans.activity.activitys.maintab.newmain.MainNewActivity',
+          quickFind: true,
+          matches: '[id="com.showstartfans.activity:id/img_close_coupon"]',
+          snapshotUrls: 'https://i.gkd.li/import/13670432',
+        },
+      ],
+    },
   ],
 });
