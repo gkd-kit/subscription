54c7f6a7dd2bc4fe6d3a7e67925c785058d33209
lisonge <i@songe.li>
2023-12-15 22:02:28 +0800

    feat(畅驾): 开屏广告

--- src/apps/cn.fszt.trafficapp.ts
+++ src/apps/cn.fszt.trafficapp.ts
@@ -7,9 +7,12 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      resetMatch: 'app',
+      actionMaximum: 1,
+      matchTime: 10000,
+      quickFind: true,
       rules: [
         {
-          activityIds: 'cn.yst.fscj.ui.main.SplashActivity',
           matches: '[text^="跳过"][text.length<=10]',
           snapshotUrls: 'https://i.gkd.li/import/13632400',
         },
