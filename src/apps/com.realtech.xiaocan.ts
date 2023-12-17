1b25938a4b6be12824b0af0d687cbdb0b11f5b7f
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-17 13:45:03 +0800

    feat 开屏广告（补充规则）

--- src/apps/com.realtech.xiaocan.ts
+++ src/apps/com.realtech.xiaocan.ts
@@ -13,7 +13,6 @@ export default defineAppConfig({
       rules: [
         {
           key: 1,
-          name: '开屏广告-1',
           quickFind: true,
           matches: '[text^="跳过"][text.length<=10]',
           snapshotUrls: [
@@ -24,9 +23,14 @@ export default defineAppConfig({
         },
         {
           key: 2,
-          name: '开屏广告-2',
           matches: '[id = "com.byted.pangle:id/tt_splash_skip_btn"]',
           snapshotUrls: 'https://i.gkd.li/import/12854728',
+        },
+        {
+          key: 3,
+          matches:
+            '[id="com.realtech.xiaocan:id/splash_ad_container"] >n FrameLayout[childCount=3] > FrameLayout + @View[clickable=true] + TextView',
+          snapshotUrls: 'https://i.gkd.li/import/13694858',
         },
       ],
     },
