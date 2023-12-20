2f75d5ec855ef047b88d92d768b7e091d0b46b89
MiracleLau <zhiminemail@163.com>
2023-12-20 23:11:01 +0800

    fix(智谱): 设置除开屏广告外,其他规则默认禁用

--- src/apps/com.crirp.zhipu.ts
+++ src/apps/com.crirp.zhipu.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   quickFind: true,
   groups: [
     {
-      name: '启动页-广告',
+      name: '开屏广告',
       key: 1,
       enable: true,
       activityIds: 'com.ruipeng.zipu.ui.bootpage.BootPageActivity',
@@ -21,7 +21,7 @@ export default defineAppConfig({
       name: '首页-今日要闻-不再提示',
       key: 2,
       desc: '【首页-今日要闻-不再提示】和【首页-今日要闻-关闭】只开启一个即可',
-      enable: true,
+      enable: false,
       activityIds: 'com.ruipeng.zipu.ui.main.uniauto.UniautoHomeActivity',
       rules: [
         {
