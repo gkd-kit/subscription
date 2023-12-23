028651ea03048d384d694b40adc930c97c9f3e13
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-23 22:52:31 +0800

    feat 开屏广告

--- src/apps/ltd.sky.okay.cn.ts
+++ src/apps/ltd.sky.okay.cn.ts
@@ -5,6 +5,15 @@ export default defineAppConfig({
   name: '漫天星',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="com.byted.pangle:id/tt_splash_skip_btn"]',
+      snapshotUrls: 'https://i.gkd.li/import/13759409',
+    },
+    {
       key: 1,
       name: '弹窗广告',
       rules: [
