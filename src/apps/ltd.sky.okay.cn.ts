00bc4b70ff326c27a2dc1bd4dab5609a0b96d6fc
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-23 22:27:12 +0800

    feat 弹窗广告（补充规则）

--- src/apps/ltd.sky.okay.cn.ts
+++ src/apps/ltd.sky.okay.cn.ts
@@ -16,6 +16,16 @@ export default defineAppConfig({
             'ImageView -2 FrameLayout > FrameLayout > FrameLayout[childCount=1] > ImageView[childCount=0]',
           snapshotUrls: 'https://i.gkd.li/import/13625479',
         },
+        {
+          key: 1,
+          name: '字节广告',
+          actionDelay: 1500,
+          activityIds:
+            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          matches:
+            '@Image[text=""] < View +n View > View > TextView[text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/13759331',
+        },
       ],
     },
   ],
