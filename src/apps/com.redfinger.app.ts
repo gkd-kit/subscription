6aa165aa2b5cc877557489ce9e62ec900adaa856
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 20:32:28 +0800

    feat 更新弹窗

--- src/apps/com.redfinger.app.ts
+++ src/apps/com.redfinger.app.ts
@@ -28,5 +28,15 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules:
+        '[id="com.redfinger.app:id/btn_update"] + [id="com.redfinger.app:id/iv_close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13761821',
+    },
   ],
 });
