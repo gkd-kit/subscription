6d9f5acd8b3d48b65ac9256ccb61be60fc78f2c9
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 15:47:59 +0800

    feat 青少年模式

--- src/apps/com.example.pptv.ts
+++ src/apps/com.example.pptv.ts
@@ -14,5 +14,14 @@ export default defineAppConfig({
       rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13669401',
     },
+    {
+      key: 1,
+      name: '青少年模式',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '@[text="我知道了"] - * > [text="未成年模式"]',
+      snapshotUrls: 'https://i.gkd.li/import/13761159',
+    },
   ],
 });
