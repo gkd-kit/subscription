1088b157005b71d654f071fa7bedec1c80b22489
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-24 20:34:38 +0800

    feat 弹窗广告

--- src/apps/com.redfinger.app.ts
+++ src/apps/com.redfinger.app.ts
@@ -38,5 +38,18 @@ export default defineAppConfig({
         '[id="com.redfinger.app:id/btn_update"] + [id="com.redfinger.app:id/iv_close"]',
       snapshotUrls: 'https://i.gkd.li/import/13761821',
     },
+    {
+      key: 2,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'com.redfinger.app.activity.MainActivity',
+          quickFind: true,
+          matches: '[id="com.redfinger.app:id/rl_screen_ad_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/13761825',
+        },
+      ],
+    },
   ],
 });
