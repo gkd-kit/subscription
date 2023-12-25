5edc5b882747343e684354d905fd2f6d444e527b
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-25 21:36:36 +0800

    feat 弹窗广告

--- src/apps/com.bz.yilianlife.sd.ts
+++ src/apps/com.bz.yilianlife.sd.ts
@@ -13,5 +13,19 @@ export default defineAppConfig({
       rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13766174',
     },
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          key: 0,
+          name: '快手广告',
+          activityIds: 'com.deep.search.MainActivity',
+          matches:
+            '@ImageView[clickable=true] <3 ViewGroup -2 ViewGroup >n [text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/13766176',
+        },
+      ],
+    },
   ],
 });
