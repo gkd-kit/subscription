50ee37d1456763003e9b9624ccb3826a7bf00571
MiracleLau <zhiminemail@163.com>
2023-12-25 15:31:11 +0800

    fix(财联社): 优化财联社规则属性

--- src/apps/com.lanjinger.choiassociatedpress.ts
+++ src/apps/com.lanjinger.choiassociatedpress.ts
@@ -3,15 +3,14 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.lanjinger.choiassociatedpress',
   name: '财联社',
-  quickFind: true,
   groups: [
     {
       key: 0,
       name: '开屏广告',
       matchTime: 10000,
       resetMatch: 'app',
+      quickFind: true,
       enable: true,
-      activityIds: 'com.lanjinger.choiassociatedpress.ad.AdActivity',
       actionMaximum: 1,
       rules: '[text="跳过"]',
       snapshotUrls: [
@@ -24,7 +23,9 @@ export default defineAppConfig({
       name: '首页-弹窗广告',
       matchTime: 10000,
       actionMaximum: 1,
+      resetMatch: 'app',
       enable: false,
+      quickFind: true,
       activityIds: 'com.lanjinger.choiassociatedpress.main.OperateActivity',
       rules: '@ImageView[id$="iv_back"][clickable=true]',
       snapshotUrls: 'https://i.gkd.li/import/13749206',
