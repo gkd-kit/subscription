4e402bce83957e141c0e29348cabbf344215ea72
Nicko <mayday.nicko@gmail.com>
2023-12-27 14:50:51 +0800

    人人视频新版本规则适配

--- src/apps/com.example.pptv.ts
+++ src/apps/com.example.pptv.ts
@@ -1,37 +1,45 @@
 import { defineAppConfig } from '../types';
 
-export default defineAppConfig({
+export default defineAppConfig([{
+	id: 'com.miui.systemAdSolution',
+	name: 'systemAd',
+	groups: [{
+		key: 1,
+		name: '系统广告',
+		activityIds: [],
+		rules: [{
+			matches: '[text="跳过"]',
+			snapshotUrls: 'https://i.gkd.li/snapshot/1703655729664'
+		}],
+	}, ],
+},
+{
   id: 'com.example.pptv',
   name: '人人视频',
   groups: [
     {
-      key: 0,
+      key: 1,
       name: '开屏广告',
-      quickFind: true,
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
+      activityIds: ['com.pptv.rrtvtrunk.main.presentation.MainActivity'],
       rules: [
         {
-          key: 0,
-          matches: '[text*="跳过"][text.length<=10]',
-          snapshotUrls: 'https://i.gkd.li/import/13669401',
+          matches: '[id="com.bykv.vk:id/tt_splash_skip_btn"]',
+          snapshotUrls: 'https://i.gkd.li/snapshot/1703655595011',
         },
         {
-          key: 1,
           matches: '[id="com.example.pptv:id/view_count_shake"]',
-          snapshotUrls: 'https://i.gkd.li/import/13761160',
+          snapshotUrls: [
+            'https://i.gkd.li/snapshot/1703656736790',
+          ],
         },
+        {
+          matches: '[id="com.example.pptv:id/iv_close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/snapshot/1703655711860',
+            ],
+        }
       ],
     },
-    {
-      key: 1,
-      name: '青少年模式',
-      actionMaximum: 1,
-      resetMatch: 'app',
-      quickFind: true,
-      rules: '@[text="我知道了"] - * > [text="未成年模式"]',
-      snapshotUrls: 'https://i.gkd.li/import/13761159',
-    },
-  ],
-});
+    ],
+}
+]);
