a3910d8b72fedf145412670de9c2e3e156f6a8bf
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-30 13:58:52 +0800

    feat 更新弹窗

--- src/apps/com.igpsport.igpsportandroid.ts
+++ src/apps/com.igpsport.igpsportandroid.ts
@@ -14,5 +14,13 @@ export default defineAppConfig({
       rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12818230',
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@[text="忽略本次"] + [text="立即升级"]',
+      snapshotUrls: 'https://i.gkd.li/import/13797203',
+    },
   ],
 });
