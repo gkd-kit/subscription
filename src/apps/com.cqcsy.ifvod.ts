b97efdf35e394ffb6ef55af314b0fd6e72bc4378
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-31 00:07:06 +0800

    feat 更新弹窗

--- src/apps/com.cqcsy.ifvod.ts
+++ src/apps/com.cqcsy.ifvod.ts
@@ -14,5 +14,14 @@ export default defineAppConfig({
       rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/12793228',
     },
+    {
+      key: 1,
+      name: '更新弹窗',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '@[text="以后再说"] +2 [text="立即更新"]',
+      snapshotUrls: 'https://i.gkd.li/import/13798537',
+    },
   ],
 });
