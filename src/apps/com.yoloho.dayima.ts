d0041c654a03f0239ea6949a317ec47bea0914c8
adproqwq <2757917031@qq.com>
2023-12-31 12:54:35 +0800

    feat(大姨妈): 广告弹窗-腾讯SDK

---
+++ src/apps/com.yoloho.dayima.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.yoloho.dayima',
+  name: '大姨妈',
+  groups: [
+    {
+      key: 1,
+      name: '广告弹窗',
+      matchTime: 10000,
+      rules: [
+        {
+          key: 0,
+          name: '腾讯SDK',
+          matches: '[id="android:id/content"] >n FrameLayout[index=2] > ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13800255',
+        },
+      ],
+    },
+  ],
+});
