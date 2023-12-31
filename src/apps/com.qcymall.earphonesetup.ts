9851ba899526866050cfb4b47959940f7fe32ed0
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2023-12-31 23:08:46 +0800

    feat 开屏广告（优化规则）

--- src/apps/com.qcymall.earphonesetup.ts
+++ src/apps/com.qcymall.earphonesetup.ts
@@ -7,14 +7,15 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: [
-        'com.qcymall.earphonesetup.v2ui.activity.AdvertiseActivity',
-        'com.qcymall.earphonesetup.ServiceLaunchActivity',
-      ],
-      rules: '[id="com.qcymall.earphonesetup:id/count_time"] + [text="跳过"]',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: [
         'https://i.gkd.li/import/12776001',
         'https://i.gkd.li/import/12776002',
+        'https://i.gkd.li/import/13801480',
       ],
     },
   ],
