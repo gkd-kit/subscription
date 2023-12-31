37a386de94889db4f46bfe1ec0416e4e41409d29
adproqwq <2757917031@qq.com>
2023-12-31 11:41:29 +0800

    feat(NGA玩家社区): 开屏广告（补充规则）

--- src/apps/gov.pianzong.androidnga.ts
+++ src/apps/gov.pianzong.androidnga.ts
@@ -11,15 +11,13 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules: [
-        '[id="gov.pianzong.androidnga:id/iv_tg_ad"]',
-        '[id="gov.pianzong.androidnga:id/ksad_splash_circle_skip_view"]',
-      ],
+      rules: '[id="gov.pianzong.androidnga:id/iv_tg_ad" || id="gov.pianzong.androidnga:id/ksad_splash_circle_skip_view" || id="gov.pianzong.androidnga:id/ksad_skip_view_skip"]',
       snapshotUrls: [
         'https://i.gkd.li/import/12476484',
         'https://i.gkd.li/import/12706127', // activityIds: 'com.miui.home.launcher.Launcher',
         'https://i.gkd.li/import/12864707', // activityIds: 'gov.pianzong.androidnga.activity.LoadingActivity',
         'https://i.gkd.li/import/12911882', // activityIds: 'gov.pianzong.androidnga.activity.WarmstartActivity'
+        'https://i.gkd.li/import/13798686', // id="gov.pianzong.androidnga:id/ksad_skip_view_skip"
       ],
     },
     {
