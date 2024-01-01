bd615c9a94263fc2ce0ec05741f7e2a5a3a28990
aisouler <zy43220@gmail.com>
2024-01-01 22:18:26 +0800

    Update com.cainiao.wireless.ts
    修改key值

--- src/apps/com.cainiao.wireless.ts
+++ src/apps/com.cainiao.wireless.ts
@@ -95,7 +95,7 @@ export default defineAppConfig({
       snapshotUrls: 'https://i.gkd.li/import/13692761',
     },
     {
-      key: 1,
+      key: 7,
       name: '红包弹窗',
       desc: '自动点击关闭',
       rules: [
