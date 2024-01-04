2c458c3d036bbdc9b93db18b850c8efb80e4c064
aisouler <zy43220@gmail.com>
2024-01-04 14:00:10 +0800

    Create moc.nauxuoyoaixoaix.www.ts

---
+++ src/apps/moc.nauxuoyoaixoaix.www.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+    id: 'moc.nauxuoyoaixoaix.www',
+    name: '晓晓优选',
+    groups: [
+      {
+        key: 1,
+        name: '开屏广告',
+        desc: '点击跳过',
+        rules: [
+          {
+            activityIds: 'moc.nauxuoyoaixoaix.www.ui.activity.SplashActivity',
+            matches: 
+              '[text^="扭动手机"] <<9 FrameLayout +2 View[clickable=true]',
+            snapshotUrls: 'https://i.gkd.li/import/13820450',
+          },
+        ],
+      },
+    ],
+  });
