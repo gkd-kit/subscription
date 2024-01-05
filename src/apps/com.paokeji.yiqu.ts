9e778e6bcee0d855e3c066a754271bb6cc8b39ed
adproqwq <adpro_qwq@outlook.com>
2024-01-05 22:21:01 +0800

    fix: 规则失效

--- src/apps/com.paokeji.yiqu.ts
+++ src/apps/com.paokeji.yiqu.ts
@@ -7,12 +7,12 @@ export default defineAppConfig({
     {
       key: 1,
       name: '弹窗广告',
-      activityIds: 'com.aster.comic.app.view.MainActivity',
       rules:
-        '[id="android:id/content"] >n FrameLayout[index=1] > ImageView[index=0]',
+        '[id="android:id/content"] >(-n+4) FrameLayout > FrameLayout[childCount=1] > ImageView[index=0]',
       snapshotUrls: [
         'https://i.gkd.li/import/13830354',
-        'https://i.gkd.li/import/13842716', //限定ImageView[index=0]防止误触
+        'https://i.gkd.li/import/13842716',
+        'https://i.gkd.li/import/13842966',
       ],
     },
   ],
