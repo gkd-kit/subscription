d484274a136279f2d5617d197f6b8c8694f11d62
aisouler <zy43220@gmail.com>
2024-01-06 11:33:19 +0800

    feat(埋堆堆):青少年弹窗

---
+++ src/apps/com.tvbc.maiduidui.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+    id: 'com.tvbc.maiduidui',
+    name: '埋堆堆',
+    groups: [
+      {
+        key: 1,
+        quickFind: true,
+        name: '青少年模式弹窗',
+        desc: '点击我知道了',
+        rules: [
+          {
+            activityIds: 'com.tvbc.maiduidui.MainHomeActivity',
+            matches: 
+                '[text="开启青少年模式"] + [text="我知道了"]',
+            snapshotUrls: 'https://i.gkd.li/import/13847804',
+          },
+        ],
+      },
+    ],
+  });
