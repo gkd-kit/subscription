d4313e464cf914aa29a3adc8022c8c992b13383d
aisouler <zy43220@gmail.com>
2024-01-08 13:22:18 +0800

    feat(Nico):开屏广告

---
+++ src/apps/com.superrhino.rarering.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.superrhino.rarering',
+  name: 'Nico',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      desc: '点击跳过',
+      rules: [
+        {
+          activityIds: 'com.superrhino.rare.LaunchActivity',
+          matches:
+            '@View[clickable=true] <3 * <2 * < FrameLayout[id="com.superrhino.rarering:id/fl_container"]',
+          snapshotUrls: 'https://i.gkd.li/import/13853858',
+        },
+      ],
+    },
+  ],
+});
