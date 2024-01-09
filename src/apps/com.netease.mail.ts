91ffdd359b8eb13150e7e9b833bba0e2d5e4eaef
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-01-10 01:55:46 +0800

    feat(网易邮箱大师):开屏广告、查看成就
    - 【开屏广告】- 开屏有个无效跳过按钮需排除
    - 【查看成就】- 新增
    
    Signed-off-by: jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>

--- src/apps/com.netease.mail.ts
+++ src/apps/com.netease.mail.ts
@@ -5,8 +5,35 @@ export default defineAppConfig({
   name: '网易邮箱大师',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 2,
+      resetMatch: 'app',
+      actionCdKey: 0,
+      actionMaximumKey: 0,
+      rules: [
+        {
+          key: 0,
+          name: '开屏-1',
+          quickFind: true,
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+          excludeMatches: '[vid="ad_skip"][text="跳过"][clickable=false]',
+          snapshotUrls: 'https://i.gkd.li/import/13874492', // v7.18.1版本中开屏有个隐藏无效跳过按钮需排除，说不定以后网易会删掉
+        },
+        {
+          key: -1,
+          name: '开屏-2',
+          matches:
+            '[childCount=0][visibleToUser=true][(text.length<10&&(text*="跳过"||text*="跳過"||text*="skip"||text*="Skip")) || id$="tt_splash_skip_btn" || vid*="skip" || vid*="Skip" || (vid*="count" && vid*="down" && vid!*="download") || desc*="跳过" || desc*="skip"]',
+          excludeMatches: '[vid="ad_skip"][text="跳过"][clickable=false]',
+          snapshotUrls: 'https://i.gkd.li/import/13874492', // v7.18.1版本中开屏有个隐藏无效跳过按钮需排除，说不定以后网易会删掉
+        },
+      ],
+    },
+    {
       key: 1,
-      name: '更新弹窗',
+      name: '更新提示',
       activityIds: [
         'com.netease.mobimail.module.flutter.CustomFlutterActivity',
       ],
@@ -16,7 +43,7 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '邮件列表广告',
+      name: '全屏广告-邮件列表广告',
       activityIds: ['com.netease.mail.biz.main.MainITabActivity'],
       quickFind: true,
       rules: [
@@ -33,6 +60,15 @@ export default defineAppConfig({
         },
       ],
       snapshotUrls: 'https://i.gkd.li/import/12664070',
+    },
+    {
+      key: 5,
+      name: '全屏广告-查看成就',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: 'TextView[text="恭喜您获得以下成就"] - TextView[text=""][clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/import/13876817',
     },
   ],
 });
