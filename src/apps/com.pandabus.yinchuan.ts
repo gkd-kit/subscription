fd0f6c7c43dac55620658726485d1d9d655d9001
Hazy-z <64131672+Hazy-z@users.noreply.github.com>
2024-01-10 15:55:45 +0800

    Create com.pandabus.yinchuan.ts
    银川公交APP“银川行”开屏摇一摇广告
    
    Signed-off-by: Hazy-z <64131672+Hazy-z@users.noreply.github.com>

---
+++ src/apps/com.pandabus.yinchuan.ts
@@ -0,0 +1,19 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.pandabus.yinchuan',
+  name: '银川行',
+  groups: [
+    {
+      key: 1,
+      name: '开屏摇一摇广告',
+      activityIds: ['com.pandabus.yinchuan.ui.activity.GuideActivity'],
+      rules: [
+        {
+          matches: '[id="com.pandabus.yinchuan:id/tv_skip"]',
+          snapshotUrls: 'https://i.gkd.li/import/13884598',
+        },
+      ],
+    },
+  ],
+});
