6a6caef924bdb96f99ec0ed5ee8943791023edf7
aisouler <zy43220@gmail.com>
2024-01-10 18:10:33 +0800

    feat(网速管家):开屏广告

--- src/apps/cn.lezhi.speedtest.ts
+++ src/apps/cn.lezhi.speedtest.ts
@@ -13,5 +13,14 @@ export default defineAppConfig({
         '[id="cn.lezhi.speedtest:id/rl_update_dialog_layout"] >n [id="cn.lezhi.speedtest:id/iv_close"]',
       snapshotUrls: 'https://i.gkd.li/import/12727619',
     },
+    {
+      key: 2,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '[id="cn.lezhi.speedtest:id/fl_splash_container"] >3 * +2 View[clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/import/13885906',
+    },
   ],
 });
