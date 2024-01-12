5349552f3b603c298dc80f346216c117993f6e73
PianCat <50688679+PianCat@users.noreply.github.com>
2024-01-12 13:31:34 +0800

    fix(软件包安装程序)：操作逻辑增强
    Signed-off-by: PianCat <50688679+PianCat@users.noreply.github.com>

--- src/apps/com.google.android.packageinstaller.ts
+++ src/apps/com.google.android.packageinstaller.ts
@@ -7,66 +7,60 @@ export default defineAppConfig({
     {
       enable: false,
       key: 1,
-      name: '功能类-自动安装应用',
-      activityIds: [
-        'com.android.packageinstaller.PackageInstallerActivity',
-        'com.android.packageinstaller.DeleteStagedFileOnResult',
-      ],
+      name: '自动安装应用',
+      quickFind: true,
       rules: [
         {
-          matches: '[text="安装"]',
-          snapshotUrls: 'https://i.gkd.li/import/13914644',
-        },
-        {
-          matches: '[id$="/button1"]',
+          key: 0,
+          activityIds: ['com.android.packageinstaller.PackageInstallerActivity', 'com.android.packageinstaller.DeleteStagedFileOnResult'],
+          matches: '@[clickable=true] > [text="安装"]',
           snapshotUrls: [
             'https://i.gkd.li/import/13914644',
             'https://i.gkd.li/import/13914647',
           ],
+        },
+        {
+          key: 1,
+          preKeys: 0,
+          activityIds: 'com.android.packageinstaller.InstallSuccess',
+          matches: '@[clickable=true] > [text="完成"]',
+          snapshotUrls: 'https://i.gkd.li/import/13914923',
         },
       ],
     },
     {
       enable: false,
       key: 2,
-      name: '功能类-自动更新应用',
-      activityIds: [
-        'com.android.packageinstaller.PackageInstallerActivity',
-        'com.android.packageinstaller.DeleteStagedFileOnResult',
-      ],
+      name: '自动更新应用',
+      quickFind: true,
       rules: [
         {
-          matches: '[text="更新"]',
-          snapshotUrls: 'https://i.gkd.li/import/13914647',
+          key: 0,
+          activityIds: ['com.android.packageinstaller.PackageInstallerActivity', 'com.android.packageinstaller.DeleteStagedFileOnResult'],
+          matches: '@[clickable=true] > [text="更新"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13914644',
+            'https://i.gkd.li/import/13914647',
+          ],
         },
         {
-          matches: '[id$="/button1"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/13914647',
-            'https://i.gkd.li/import/13914644',
-          ],
+          key: 1,
+          preKeys: 0,
+          activityIds: 'com.android.packageinstaller.InstallSuccess',
+          matches: '@[clickable=true] > [text="完成"]',
+          snapshotUrls: 'https://i.gkd.li/import/13914923',
         },
       ],
     },
     {
       enable: false,
       key: 3,
-      name: '功能类-自动取消安装应用',
-      activityIds: [
-        'com.android.packageinstaller.PackageInstallerActivity',
-        'com.android.packageinstaller.DeleteStagedFileOnResult',
-      ],
+      name: '自动取消安装应用',
+      activityIds: ['com.android.packageinstaller.PackageInstallerActivity', 'com.android.packageinstaller.DeleteStagedFileOnResult'],
       rules: [
         {
-          matches: '[text="取消"]',
+          matches: '@[clickable=true] > [text="取消"]',
           snapshotUrls: 'https://i.gkd.li/import/13914647',
-        },
-        {
-          matches: '[id$="/button2"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/13914644',
-            'https://i.gkd.li/import/13914647',
-          ],
         },
       ],
     },
