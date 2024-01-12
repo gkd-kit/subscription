c6ad66720aee245f0c47a542eca4ec71b20ac12d
PianCat <50688679+PianCat@users.noreply.github.com>
2024-01-12 14:00:29 +0800

    fix(软件包安装程序)：修改逻辑
    但是目前仍然存在问题，完成按钮不会自动点击
    
    Signed-off-by: PianCat <50688679+PianCat@users.noreply.github.com>

--- src/apps/com.google.android.packageinstaller.ts
+++ src/apps/com.google.android.packageinstaller.ts
@@ -1,19 +1,17 @@
-import { defineAppConfig } from '../types';
-
-export default defineAppConfig({
+{
   id: 'com.google.android.packageinstaller',
   name: '原生软件包安装程序（Google）',
   groups: [
     {
       enable: false,
       key: 1,
-      name: '自动安装应用',
+      name: '功能类-自动安装应用',
       quickFind: true,
       rules: [
         {
           key: 0,
           activityIds: ['com.android.packageinstaller.PackageInstallerActivity', 'com.android.packageinstaller.DeleteStagedFileOnResult'],
-          matches: '@[clickable=true] > [text="安装"]',
+          matches: '[text="安装"]',
           snapshotUrls: [
             'https://i.gkd.li/import/13914644',
             'https://i.gkd.li/import/13914647',
@@ -22,8 +20,8 @@ export default defineAppConfig({
         {
           key: 1,
           preKeys: 0,
-          activityIds: 'com.android.packageinstaller.InstallSuccess',
-          matches: '@[clickable=true] > [text="完成"]',
+          activityIds: ['com.android.packageinstaller.InstallSuccess'],
+          matches: '[text="完成"]',
           snapshotUrls: 'https://i.gkd.li/import/13914923',
         },
       ],
@@ -31,13 +29,13 @@ export default defineAppConfig({
     {
       enable: false,
       key: 2,
-      name: '自动更新应用',
+      name: '功能类-自动更新应用',
       quickFind: true,
       rules: [
         {
           key: 0,
           activityIds: ['com.android.packageinstaller.PackageInstallerActivity', 'com.android.packageinstaller.DeleteStagedFileOnResult'],
-          matches: '@[clickable=true] > [text="更新"]',
+          matches: '[text="更新"]',
           snapshotUrls: [
             'https://i.gkd.li/import/13914644',
             'https://i.gkd.li/import/13914647',
@@ -46,8 +44,8 @@ export default defineAppConfig({
         {
           key: 1,
           preKeys: 0,
-          activityIds: 'com.android.packageinstaller.InstallSuccess',
-          matches: '@[clickable=true] > [text="完成"]',
+          activityIds: ['com.android.packageinstaller.InstallSuccess'],
+          matches: '[text="完成"]',
           snapshotUrls: 'https://i.gkd.li/import/13914923',
         },
       ],
@@ -55,14 +53,15 @@ export default defineAppConfig({
     {
       enable: false,
       key: 3,
-      name: '自动取消安装应用',
+      quickFind: true,
+      name: '功能类-自动取消安装应用',
       activityIds: ['com.android.packageinstaller.PackageInstallerActivity', 'com.android.packageinstaller.DeleteStagedFileOnResult'],
       rules: [
         {
-          matches: '@[clickable=true] > [text="取消"]',
+          matches: '[text="取消"]',
           snapshotUrls: 'https://i.gkd.li/import/13914647',
         },
       ],
     },
   ],
-});
+}
