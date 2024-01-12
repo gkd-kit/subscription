7fc0244d29dbc5d43e4b24325998515a764cc33c
PianCat <50688679+PianCat@users.noreply.github.com>
2024-01-12 12:48:08 +0800

    添加软件包安装程序的自动点击
    当前对应安装程序为类原生的安装程序，且为Google版，删掉Google应该就可以对应AOSP版本，但是我不清楚ID是否相同，故此仅添加Google版
    
    Signed-off-by: PianCat <50688679+PianCat@users.noreply.github.com>

---
+++ src/apps/com.google.android.packageinstaller.ts
@@ -0,0 +1,62 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.google.android.packageinstaller',
+  name: '原生软件包安装程序（Google）',
+  groups: [
+    {
+      key: 1,
+      name: '自动安装应用',
+      activityIds: ['com.android.packageinstaller.PackageInstallerActivity', 'com.android.packageinstaller.DeleteStagedFileOnResult'],
+      rules: [
+        {
+          matches: '[text="安装"]',
+          snapshotUrls: 'https://i.gkd.li/import/13914644',
+        },
+        {
+          matches: '[id$="/button1"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13914644',
+            'https://i.gkd.li/import/13914647',
+          ],
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '自动更新应用',
+      activityIds: ['com.android.packageinstaller.PackageInstallerActivity', 'com.android.packageinstaller.DeleteStagedFileOnResult'],
+      rules: [
+        {
+          matches: '[text="更新"]',
+          snapshotUrls: 'https://i.gkd.li/import/13914647',
+        },
+        {
+          matches: '[id$="/button1"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13914647',
+            'https://i.gkd.li/import/13914644',
+          ],
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '自动取消安装应用',
+      activityIds: ['com.android.packageinstaller.PackageInstallerActivity', 'com.android.packageinstaller.DeleteStagedFileOnResult'],
+      rules: [
+        {
+          matches: '[text="取消"]',
+          snapshotUrls: 'https://i.gkd.li/import/13914647',
+        },
+        {
+          matches: '[id$="/button2"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13914644',
+            'https://i.gkd.li/import/13914647',
+          ],
+        },
+      ],
+    },
+  ],
+});
