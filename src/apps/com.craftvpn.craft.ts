80be7b64c0b6eab244e8c6a7085dd72b127872d9
aisouler <zy43220@gmail.com>
2024-01-12 13:35:06 +0800

    feat(小牛加速器):开屏广告

---
+++ src/apps/com.craftvpn.craft.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.craftvpn.craft',
+  name: '小牛加速器',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      desc: '点击X',
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '@View[index=0][clickable=true] <<n [id="android:id/content"]',
+          snapshotUrls: 'https://i.gkd.li/import/13914788',
+        },
+      ],
+    },
+  ],
+});
