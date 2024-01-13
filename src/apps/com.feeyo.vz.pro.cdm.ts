7616d71a42c51a5448041f119d84c7b7c668dedc
aisouler <zy43220@gmail.com>
2024-01-13 13:09:00 +0800

    feat(飞常准业内版):开屏广告

---
+++ src/apps/com.feeyo.vz.pro.cdm.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.feeyo.vz.pro.cdm',
+  name: '飞常准业内版',
+  groups: [
+    {
+      key: 1,
+      name: '开屏广告',
+      desc: '点击跳过',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: 'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView',
+          snapshotUrls: 'https://i.gkd.li/import/13926823',
+        },
+      ],
+    },
+  ],
+});
