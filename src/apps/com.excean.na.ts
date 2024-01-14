14742efae0bcb7ec55b1b04efd6af2c429e11f82
aisouler <zy43220@gmail.com>
2024-01-14 19:27:44 +0800

    feat(99手游加速器):局部广告

---
+++ src/apps/com.excean.na.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.excean.na',
+  name: '99手游加速器',
+  groups: [
+    {
+      key: 1,
+      quickFind: true,
+      name: '局部广告-卡片广告',
+      desc: '点击X',
+      rules: [
+        {
+          activityIds: 'com.excelliance.kxqp.ui.activity.GameLaunchActivity',
+          matches: '[id="com.excean.na:id/iv_close_ad"]',
+          snapshotUrls: 'https://i.gkd.li/import/13931051',
+        },
+      ],
+    },
+  ],
+});
