ea791836b91ae5cbcc38babc0e3046ab02f921ef
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-01-14 21:19:59 +0800

    Revert [id="com.tencent.mobileqq:id/iyx"]
    This reverts commit 72903417e2ed0cb10f729042d273f8bdeab51565.

--- src/apps/com.tencent.mobileqq.ts
+++ src/apps/com.tencent.mobileqq.ts
@@ -364,29 +364,23 @@ export default defineAppConfig({
       ],
     },
     {
+      enable: false,
       key: 18,
-      name: '更新提示',
+      name: '更新提示-消息页面-顶部',
       rules: [
         {
           key: 0,
           activityIds: 'com.tencent.mobileqq.activity.SplashActivity',
-          matches:
-            '[text="点击下载"] +2 [text="发现QQ版本更新"] + ImageView[clickable=true]',
+          quickFind: true,
+          matches: '[id="com.tencent.mobileqq:id/iyx"]',
           snapshotUrls: [
             'https://i.gkd.li/import/13188722',
-            'https://i.gkd.li/import/13255493',
+            'https://i.gkd.li/import/13255493', //desc值为null快照
+            'https://i.gkd.li/import/13843140', //关系选择器为-2快照
           ],
         },
         {
           key: 1,
-          activityIds: 'com.tencent.mobileqq.activity.SplashActivity',
-          quickFind: true,
-          matches:
-            '[text="点击下载"] + @ImageView[clickable=true] +2 [text="发现QQ版本更新"]',
-          snapshotUrls: 'https://i.gkd.li/import/13843140',
-        },
-        {
-          key: 2,
           activityIds: 'com.tencent.mobileqq.activity.SplashActivity',
           matches:
             '[text="发现QQ版本更新"] + [text="点击下载"] + ImageView[clickable=true]',
