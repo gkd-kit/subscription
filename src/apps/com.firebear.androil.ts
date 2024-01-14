d7181e976a93121383b0970b56a0faecfccfbd02
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-01-14 22:33:59 +0800

    feat(小熊油耗): 开屏广告
    Signed-off-by: jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>

--- src/apps/com.firebear.androil.ts
+++ src/apps/com.firebear.androil.ts
@@ -5,8 +5,35 @@ export default defineAppConfig({
   name: '小熊油耗',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      matchTime: 10000,
+      actionMaximum: 2,
+      resetMatch: 'app',
+      rules: [
+        {
+          key: 0,
+          name: '自定义规则[字节广告SDK]',
+          matches: 'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView',
+          snapshotUrls: 'https://i.gkd.li/import/13927561',
+        },
+        {
+          key: 1,
+          name: '全局默认规则1',
+          quickFind: true,
+          matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
+        },
+        {
+          key: 2,
+          name: '全局默认规则2',
+          matches:
+            '[childCount=0][visibleToUser=true][(text.length<10&&(text*="跳过"||text*="跳過"||text*="skip"||text*="Skip")) || id$="tt_splash_skip_btn" || vid*="skip" || vid*="Skip" || (vid*="count" && vid*="down" && vid!*="download") || desc*="跳过" || desc*="skip"]',
+        },
+      ],
+    },
+    {
       key: 1,
-      name: '广告弹窗',
+      name: '全屏广告',
       desc: '来自第三方SDK【穿山甲】',
       activityIds:
         'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
@@ -36,7 +63,7 @@ export default defineAppConfig({
     },
     {
       key: 2,
-      name: '升级弹窗',
+      name: '更新提示',
       activityIds: 'com.firebear.androil.app.home.MainActivity',
       rules:
         '@[id="com.firebear.androil:id/dismissNtfImgView"] < RelativeLayout - [id="com.firebear.androil:id/contentLay"] >3 TextView[text^="升级"]',
