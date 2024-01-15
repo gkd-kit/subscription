45cf962f77f45a33a008ba0c4456d63d555f1c1a
aisouler <zy43220@gmail.com>
2024-01-15 13:11:30 +0800

    feat(华为应用市场):青少年模式（儿童模式）

--- src/apps/com.huawei.appmarket.ts
+++ src/apps/com.huawei.appmarket.ts
@@ -14,5 +14,20 @@ export default defineAppConfig({
       rules: '@[text="以后再说"] + [text="立即更新"]',
       snapshotUrls: 'https://i.gkd.li/import/13228520',
     },
+    {
+      key: 2,
+      quickFind: true,
+      name: '青少年模式',
+      desc: '点击我知道了',
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          matches: '[id="android:id/buttonPanel"] >2 [text="知道了"]',
+          snapshotUrls: 'https://i.gkd.li/import/13934995',
+        },
+      ],
+    },
   ],
 });
