ff39fabff8c4b4287cd0dc2d1f527a6aaf6f8bf8
aisouler <zy43220@gmail.com>
2024-01-16 15:33:18 +0800

    feat(得力e+):开屏广告（补充规则）

--- src/apps/com.delicloud.app.smartoffice.ts
+++ src/apps/com.delicloud.app.smartoffice.ts
@@ -11,9 +11,18 @@ export default defineAppConfig({
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
-      rules:
-        '@View[clickable=true] <2 FrameLayout <2 FrameLayout <<2 FrameLayout[id="com.delicloud.app.smartoffice:id/splash_container"]',
-      snapshotUrls: ['https://i.gkd.li/import/13840775'],
+      rules: [
+        {
+          matches: 
+            '@View[clickable=true] <2 FrameLayout <2 FrameLayout <<2 FrameLayout[id="com.delicloud.app.smartoffice:id/splash_container"]',
+          snapshotUrls: ['https://i.gkd.li/import/13840775'],
+        },
+        {
+          matches:
+            '[id="com.delicloud.app.smartoffice:id/splash_container"] View[index=2]',
+          snapshotUrls: 'https://i.gkd.li/import/13944086',
+        },
+      ],
     },
   ],
 });
