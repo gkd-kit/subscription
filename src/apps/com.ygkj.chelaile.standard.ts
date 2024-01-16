a74f6f02009dba252c7bcc63779ea2f45fd3b1dc
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-01-16 18:30:20 +0800

    feat(车来了): 全屏广告-广告弹窗
    Signed-off-by: jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>

--- src/apps/com.ygkj.chelaile.standard.ts
+++ src/apps/com.ygkj.chelaile.standard.ts
@@ -6,13 +6,15 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '卡片式广告',
+      name: '局部广告-卡片式广告',
       rules: [
         {
           key: 0,
+          name: '字节广告SDK',
           activityIds: [
             'dev.xesam.chelaile.app.module.func.SplashActivity',
             'dev.xesam.chelaile.app.module.PanelHostActivity',
+            'dev.xesam.chelaile.app.module.line.gray.LineDetailActivity',
           ],
           matches:
             'ImageView[id^="com.ygkj.chelaile.standard:id/cll_all_pic_close"][visibleToUser=true]',
@@ -20,14 +22,35 @@ export default defineAppConfig({
             'https://i.gkd.li/import/13062991',
             'https://i.gkd.li/import/13062984',
             'https://i.gkd.li/import/13464325',
+            'https://i.gkd.li/import/13947845', // activityIds: 'dev.xesam.chelaile.app.module.line.gray.LineDetailActivity',
           ],
         },
         {
           key: 1,
-          activityIds: 'dev.xesam.chelaile.app.module.PanelHostActivity',
+          activityIds: [
+            'dev.xesam.chelaile.app.module.PanelHostActivity',
+            'dev.xesam.chelaile.app.module.line.TimeTableActivity',
+          ],
           matches:
             '[id="com.ygkj.chelaile.standard:id/cll_line_single_ad_close"][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/import/13625374',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13625374',
+            'https://i.gkd.li/import/13947849,
+          ],
+        },
+      ],
+    },
+    {
+      key: 5,
+      name: '全屏广告-广告弹窗',
+      rules: [
+        {
+          key: 0,
+          name: '字节广告SDK',
+          activityIds: 'dev.xesam.chelaile.app.module.line.gray.LineDetailActivity',
+          matches:
+            'ImageView[id^="com.ygkj.chelaile.standard:id/cll_all_pic_close"][visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13947871',
         },
       ],
     },
