83883a84e7ba2fae8ecb2f1721be0661ae1de77f
aisouler <zy43220@gmail.com>
2024-01-16 12:45:19 +0800

    feat(AppShare):卡片广告（修复）、分段广告

--- src/apps/info.muge.appshare.ts
+++ src/apps/info.muge.appshare.ts
@@ -39,28 +39,6 @@ export default defineAppConfig({
       ],
     },
     {
-      key: 1,
-      name: '局部广告-卡片式广告',
-      rules: [
-        {
-          key: 0,
-          name: '腾讯广告',
-          activityIds: 'info.muge.appshare.view.app.detail.AppDetailActivity',
-          matches: [
-            '[id="info.muge.appshare:id/adContainer"] >n FrameLayout > ImageView - FrameLayout[childCount=1] > ImageView[childCount=0]',
-            '[id="info.muge.appshare:id/adContainer"] >n FrameLayout[desc*="dislike"]', //新规则
-            '[id="info.muge.appshare:id/adContainer"] >5 FrameLayout > ImageView[visibleToUser=true]', //https://i.gkd.li/import/13937744
-          ],
-          snapshotUrls: [
-            'https://i.gkd.li/import/13761259',
-            'https://i.gkd.li/import/13712716',
-            'https://i.gkd.li/import/13758909', //新规则
-            'https://i.gkd.li/import/13937744',
-          ],
-        },
-      ],
-    },
-    {
       key: 2,
       quickFind: true,
       name: '功能类-自动签到',
@@ -79,6 +57,43 @@ export default defineAppConfig({
           activityIds: 'info.muge.appshare.view.main.MainActivity',
           matches: '[id="info.muge.appshare:id/tvClose"][text="确定"]',
           snapshotUrls: 'https://i.gkd.li/import/13931279',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '局部广告-卡片广告',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'info.muge.appshare.view.app.detail.AppDetailActivity',
+          matches: '[id="info.muge.appshare:id/adContainer"] >5 LinearLayout + FrameLayout > ImageView[visibleToUser=true]',
+          snapshotUrls: 'https://i.gkd.li/import/13937744',
+        },
+        {
+          key: 1,
+          activityIds: 'info.muge.appshare.view.app.detail.AppDetailActivity',
+          matches: '[id="info.muge.appshare:id/adContainer"] FrameLayout > ImageView - FrameLayout[childCount=1]',
+          snapshotUrls: 'https://i.gkd.li/import/13944807',
+        },     
+      ],
+    },
+    {
+      key: 4,
+      name: '分段广告-软件详情页广告',
+      desc: '有二次弹窗',
+      rules: [
+        {
+          key: 0,
+          activityIds: 'info.muge.appshare.view.app.detail.AppDetailActivity',
+          matches: '[id="info.muge.appshare:id/adContainer"] FrameLayout[desc*="dislike"]',
+          snapshotUrls: 'https://i.gkd.li/import/13758909',
+        },
+        {
+          preKeys: 0,
+          activityIds: 'info.muge.appshare.view.app.detail.AppDetailActivity',
+          matches: '[text="不感兴趣"]',
+          snapshotUrls: 'https://i.gkd.li/import/13944942',
         },
       ],
     },
