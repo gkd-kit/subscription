09aff09868536604448a7827417d20191c9c2217
adproqwq <adpro_qwq@outlook.com>
2024-01-17 20:48:41 +0800

    fix: 无法点击完成

--- src/apps/com.android.packageinstaller.ts
+++ src/apps/com.android.packageinstaller.ts
@@ -17,14 +17,13 @@ export default defineAppConfig({
         },
         {
           key: 1,
-          preKeys: 0,
           activityIds: 'com.android.packageinstaller.NewInstallInstalling',
           matches: '@[clickable=true][childCount=1] > [text="完成"]',
           snapshotUrls: 'https://i.gkd.li/import/13206476',
         },
         {
+          actionCdKey: 1,
           key: 2,
-          preKeys: 0,
           activityIds: 'com.android.packageinstaller.NewInstallInstalling',
           matches:
             '[id="com.android.packageinstaller:id/done_button"][clickable=true]',
