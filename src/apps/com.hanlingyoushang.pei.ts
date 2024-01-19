3b460b390e0b624a99dc8b679ca66762e2d91dc7
yuzh0502 <273666509@qq.com>
2024-01-19 14:25:47 +0800

    feat(佩奇影视): 添加佩奇影视首页通知和弹窗广告跳过

--- src/apps/com.hanlingyoushang.pei.ts
+++ src/apps/com.hanlingyoushang.pei.ts
@@ -7,16 +7,25 @@ export default defineAppConfig({
     {
       key: 1,
       quickFind: true,
-      name: '通知提示-首页',
+      enable: false,
+      name: '首页广告提示',
       rules: [
         {
+          name: '首页我知道了',
           matches: 'View[desc="我知道了"]',
           snapshotUrls: 'https://i.gkd.li/import/13950839',
         },
         {
+          name: '广告右上角x',
           matches:
             'FrameLayout[id="android:id/content"] >5 @ImageView[name="android.widget.ImageView"]',
           snapshotUrls: 'https://i.gkd.li/import/13952257',
+        },
+        {
+          name: '广告左上角x',
+          matches:
+            'ImageView[name="android.widget.ImageView"][depth=14][left=183]',
+          snapshotUrls: 'https://i.gkd.li/import/13984995',
         },
       ],
     },
