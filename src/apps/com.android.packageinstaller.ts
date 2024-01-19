480b93bc17ea684595d53f0a59f9efa8ad7c3493
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-01-19 20:58:45 +0800

    update: 合并规则

--- src/apps/com.android.packageinstaller.ts
+++ src/apps/com.android.packageinstaller.ts
@@ -18,16 +18,13 @@ export default defineAppConfig({
         {
           key: 1,
           activityIds: 'com.android.packageinstaller.NewInstallInstalling',
-          matches: '@[clickable=true][childCount=1] > [text="完成"]',
-          snapshotUrls: 'https://i.gkd.li/import/13206476',
-        },
-        {
-          actionCdKey: 1,
-          key: 2,
-          activityIds: 'com.android.packageinstaller.NewInstallInstalling',
-          matches:
+          matches: [
+            '@[clickable=true][childCount=1] > [text="完成"]',
             '[id="com.android.packageinstaller:id/done_button"][clickable=true]',
+          ],
           snapshotUrls: [
+            'https://i.gkd.li/import/13206476', //规则1
+            //规则2
             'https://i.gkd.li/import/13766420',
             'https://i.gkd.li/import/13962438', //text=null
           ],
