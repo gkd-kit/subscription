4d8d2ef160d891a72ebd8ba7a79430a92d1e87af
aisouler <zy43220@gmail.com>
2024-01-21 15:02:34 +0800

    feat(安易加速器):更新提示

---
+++ src/apps/com.giraffe.ts
@@ -0,0 +1,23 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.giraffe',
+  name: '安易加速器',
+  groups: [
+    {
+      key: 1,
+      name: '更新提示',
+      desc: '点击X',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      quickFind: true,
+      rules: [
+        {
+          activityIds: 'com.speed.shadowsocks.ui.main.MainActivity',
+          matches: '[id="com.giraffe:id/iv_close"]',
+          snapshotUrls: 'https://i.gkd.li/import/14000409',
+        },
+      ],
+    },
+  ],
+});
