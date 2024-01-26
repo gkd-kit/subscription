9fedc1fd3c3a80570bf16bb3deae706249e1436d
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-01-26 22:14:56 +0800

    feat(萤石云视频): 功能类-在线值守试用弹窗

--- src/apps/com.videogo.ts
+++ src/apps/com.videogo.ts
@@ -22,5 +22,14 @@ export default defineAppConfig({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-在线值守试用弹窗',
+      desc: '点击x',
+      activityIds: 'com.videogo.main.MainTabActivity',
+      rules:
+        '[text="了解并试用服务"] <<n ViewGroup[index=0][childCount=2] > ViewGroup[clickable=true]',
+      snapshotUrls: 'https://i.gkd.li/import/14065785',
+    },
   ],
 });
