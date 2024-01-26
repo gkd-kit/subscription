f98fe6c16f21a7d0baed88ac61a567b377e5e560
aisouler <zy43220@gmail.com>
2024-01-26 17:31:03 +0800

    feat:优化规则

--- src/apps/com.delicloud.app.smartoffice.ts
+++ src/apps/com.delicloud.app.smartoffice.ts
@@ -7,20 +7,17 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      quickFind: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
           matches:
-            '@View[clickable=true] <2 FrameLayout <2 FrameLayout <<2 FrameLayout[id="com.delicloud.app.smartoffice:id/splash_container"]',
-          snapshotUrls: ['https://i.gkd.li/import/13840775'],
-        },
-        {
-          matches:
-            '[id="com.delicloud.app.smartoffice:id/splash_container"] View[index=2]',
-          snapshotUrls: 'https://i.gkd.li/import/13944086',
+            'FrameLayout > FrameLayout[childCount>2] > @View[clickable=true] + TextView',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13840775',
+            'https://i.gkd.li/import/13944086',
+          ],
         },
       ],
     },
