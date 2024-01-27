639959aa619c76912420db58868594675cba6501
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-01-27 21:27:20 +0800

    feat: 功能类-免费会员弹窗

--- src/apps/world.letsgo.booster.android.pro.ts
+++ src/apps/world.letsgo.booster.android.pro.ts
@@ -13,5 +13,14 @@ export default defineAppConfig({
       rules: '[vid="close_update_tip"]',
       snapshotUrls: 'https://i.gkd.li/import/14066056',
     },
+    {
+      key: 2,
+      name: '功能类-免费会员弹窗',
+      desc: '点击【快快收下】',
+      quickFind: true,
+      activityIds: 'world.letsgo.booster.android.pages.home.HomeActivity',
+      rules: '[text="快快收下"]',
+      snapshotUrls: 'https://i.gkd.li/import/14072381',
+    },
   ],
 });
