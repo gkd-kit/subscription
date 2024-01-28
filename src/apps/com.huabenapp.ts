30e23175a5cf46282d3cae687ddde924bbc1b70a
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-01-28 21:30:38 +0800

    feat(话本小说): 功能类-阅读界面-底部社区问答

--- src/apps/com.huabenapp.ts
+++ src/apps/com.huabenapp.ts
@@ -14,5 +14,12 @@ export default defineAppConfig({
         '[text="发现新版本"] <n ViewGroup < ScrollView +n ViewGroup[clickable=true]',
       snapshotUrls: 'https://i.gkd.li/import/14072723',
     },
+    {
+      key: 2,
+      name: '功能类-阅读界面-底部社区问答',
+      activityIds: 'com.huabenapp.MainActivity',
+      rules: '[text*="社区问答"] +2 ViewGroup',
+      snapshotUrls: 'https://i.gkd.li/import/14076166',
+    },
   ],
 });
