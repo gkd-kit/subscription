7ab4861fb26d606dfb410e563d41b5ccbbe35baa
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-01-31 00:01:22 +0800

    feat(一个): 全屏广告

---
+++ src/apps/com.hihanhan.one.rt45.ts
@@ -0,0 +1,15 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+   id: 'com.hihanhan.one.rt45',
+  name: '一个',
+  groups: [
+    {
+      key: 1,
+      name: '全屏广告',
+      quickFind: true,
+      rules: '[vid="ivClose"]',
+      snapshotUrls: 'https://i.gkd.li/import/14099393',
+    },
+  ]
+});
