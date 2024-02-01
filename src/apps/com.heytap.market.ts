bed0ee90522f3751017f781902f3135aa5e5eeea
Adpro <101449500+adproqwq@users.noreply.github.com>
2024-02-01 12:39:13 +0800

    feat(OPPO软件商店): 开屏广告

--- src/apps/com.heytap.market.ts
+++ src/apps/com.heytap.market.ts
@@ -5,6 +5,19 @@ export default defineAppConfig({
   name: 'OPPO软件商店',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      desc: '全局规则不生效',
+      actionMaximum: 1,
+      resetMatch: 'app',
+      matchTime: 10000,
+      quickFind: true,
+      rules: '[text*="跳过"][text.length<=5]',
+      exampleUrls:
+        'https://m.gkd.li/101449500/4c1e3df4-e0e8-49ec-a260-ed406b46168e',
+      snapshotUrls: 'https://i.gkd.li/import/14120100',
+    },
+    {
       key: 1,
       name: '版本更新',
       quickFind: true,
